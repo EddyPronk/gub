9ad5246986f0af44e9786df9d30f26cb8dc1343d
hanwen <hanwen@xs4all.nl>
2005-12-02 10:38:36 +0100

    expat 1.95.8-1 for darwin.

--- specs/framework.py
+++ specs/framework.py
@@ -411,7 +411,8 @@ def get_packages (settings, platform):
 	'darwin': (
 		Gettext__darwin (settings).with (version='0.10.40'),
 		Freetype (settings).with (version='2.1.9', mirror=download.freetype),
-		Expat (settings).with (version='1.95.8', mirror=download.sourceforge, format='gz'),
+		Expat (settings).with (version='1.95.8-1', mirror=download.lp, format='bz2'),
+#		Expat (settings).with (version='1.95.8', mirror=download.sourceforge, format='gz'),
 		Glib__darwin (settings).with (version='2.8.4', mirror=download.gtk),
 		Fontconfig__darwin (settings).with (version='2.3.2', mirror=download.fontconfig),
 		Pango__darwin (settings).with (version='1.10.1', mirror=download.gtk),
