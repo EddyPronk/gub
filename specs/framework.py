ffc19ea2675475869ad3ded1175f2a76b2265900
hanwen <hanwen@xs4all.nl>
2005-12-08 20:58:14 +0100

    use lilywinbuilddir key.

--- specs/framework.py
+++ specs/framework.py
@@ -117,8 +117,8 @@ libltdl_cv_sys_search_path=${libltdl_cv_sys_search_path="%(system_root)s/usr/lib
 		self.file_sub ([('^\(allow_undefined_flag=.*\)unsupported',
 			       '\\1')],
 			       '%(builddir)s/guile-readline/libtool')
-		self.system ('''cp $HOME/installers/windows/bin/%(target_architecture)s-libtool %(builddir)s/libtool''')
-		self.system ('''cp $HOME/installers/windows/bin/%(target_architecture)s-libtool %(builddir)s/guile-readline/libtool''')
+		self.system ('''cp %(lilywinbuilddir)s/bin/%(target_architecture)s-libtool %(builddir)s/libtool''')
+		self.system ('''cp %(lilywinbuilddir)s/bin/%(target_architecture)s-libtool %(builddir)s/guile-readline/libtool''')
 
 class Guile__linux (Guile):
 	def compile_command (self):
