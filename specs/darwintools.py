68271b5970e0e65f1f48fcd50a4bc79621b07fcb
hanwen <hanwen@xs4all.nl>
2005-12-11 23:11:54 +0100

    override sysinstall / package stages to ensure handling of /System/Library

--- specs/darwintools.py
+++ specs/darwintools.py
@@ -11,7 +11,7 @@ class Darwin_sdk (gub.Binary_package):
 	def patch (self):
 		pat = self.settings.system_root + '/usr/lib/*.la'
 		for a in glob.glob (pat):
-			self.file_sub ([(r' (/usr/lib/.*\.la)', '%(system_root)s\1')], a)
+			self.file_sub ([(r' (/usr/lib/.*\.la)', r'%(system_root)s\1')], a)
 
 	def install (self):
 
@@ -19,6 +19,17 @@ class Darwin_sdk (gub.Binary_package):
 		self.system ('mkdir -p %(install_root)s/')
 		self.system ('tar -C %(srcdir)s/root -cf- . | tar -C %(install_root)s/ -xvf-')
 
+	
+	def sysinstall (self):
+		self._install_gub (self.settings.system_root)
+
+	def package (self):
+		# naive tarball packages for now
+		self.system ('''
+tar -C %(install_root)s -zcf %(gub_uploads)s/%(gub_name)s .
+''')
+
+		
 	
 class Odcctools (gub.Cross_package):
 	def install_prefix (self):
