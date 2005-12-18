5e9f9f2a1cfbbaa61626c55c50cb393ea8418ae3
hanwen <hanwen@xs4all.nl>
2005-12-18 15:48:06 +0100

    remove Darwin_sdk from darwintools

--- specs/darwintools.py
+++ specs/darwintools.py
@@ -4,17 +4,6 @@ import glob
 import gub
 import re
 
-class Darwin_sdk (gub.Binary_package, gub.Cross_package):
-	def file_name (self):
-		return 'darwin-sdk.tar.gz'
-
-	def patch (self):
-		pat = self.settings.system_root + '/usr/lib/*.la'
-		for a in glob.glob (pat):
-			self.file_sub ([(r' (/usr/lib/.*\.la)', r'%(system_root)s\1')], a)
-	def install_prefix (self):
-		return self.install_root ()
-		
 class Odcctools (gub.Cross_package):
 	def install_prefix (self):
 		return self.settings.tooldir
@@ -48,9 +37,6 @@ class Gcc (framework.Gcc):
 
 def get_packages (settings):
 	return (
-		##  download.hw
-		Darwin_sdk (settings).with (version='', mirror=lambda: 'http://lilypond.org/~hanwen/darwin-sdk.tar.gz'),
-		
 #		Odcctools (settings).with (version='20051031', mirror=download.opendarwin, format='bz2'),
 		Odcctools (settings).with (version='20051122', mirror=download.opendarwin, format='bz2'),		
 #		Gcc (settings).with (version='4.0.2', mirror = download.gcc, format='bz2'),
