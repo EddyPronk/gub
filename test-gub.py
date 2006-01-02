41ec10a6e046014a9a085b9406bcaceccc7891d8
hanwen <hanwen@xs4all.nl>
2006-01-02 17:33:52 +0100

    test-gub.py fixes

--- test-gub.py
+++ test-gub.py
@@ -4,13 +4,14 @@ import time
 
 server = "smtp.xs4all.nl"
 sender = "hanwen@xs4all.nl"
-dest = ['janneke@gnu.org',
+addresses = [
+	'janneke@gnu.org',
 	'hanwen@xs4all.nl']
 
 def tag_name ():
 	(year, month, day, hours,
 	 minutes, seconds, weekday,
-	 day_of_year, dst) = localtime()
+	 day_of_year, dst) = time.localtime()
 	
 	return "success-%(year)d-%(month)d-%(day)d" % locals()
 
@@ -19,21 +20,25 @@ def system (cmd):
 	return os.system (cmd)
 
 stat = os.system ("make distclean")
-stat = os.system ("make all 2>&1 | tee test-gub.log")
+#stat = os.system ("nice make all 2>&1 | tee test-gub.log")
+
+stat = os.system ("nice make all >& test-gub.log")
 if stat:
 	f = open ('test-gub.log')
-	f.seek (2, -10240)
+	f.seek (0, 2)
+	length = f.tell()
+	f.seek (- min (length, 10240), 1)
 	body = f.read ()
 
 	connection = smtplib.SMTP (server)
+	subject = 'GUB Autobuild: FAIL'
 	body = 'Subject: %s\n\n' % subject +  body
 	connection.sendmail (sender, addresses, body)
 
-
 	## TODO: include diff from last known working
 else:
-	system ('darcs tag %s' % tag_name ()) 
-
+	#	system ('darcs tag %s' % tag_name ()) 
+	pass
 	## TODO: push the tag
 	
 	
