cd46e4efb04724a8dc52245c00b7583ab369a0c1
hanwen <hanwen@xs4all.nl>
2006-01-05 19:13:28 +0100

    add nsis, scons.

--- specs/tools.py
+++ specs/tools.py
@@ -2,6 +2,7 @@ import re
 import gub
 import download
 import misc
+import os
 
 class Tool_package (gub.Package):
 	def configure_command (self):
@@ -36,22 +37,63 @@ class Flex (Tool_package):
 		
 	def patch (self):
 		self.system ("cd %(srcdir)s && patch -p1 < %(patchdir)s/flex-2.5.4a-FC4.patch")
+
+
+class Nsis (Tool_package):
+	def compile (self): 
+		env = os.environ.copy()
+		env['PATH'] = '%(topdir)s/i686-mingw32/system/usr/cross/bin:' + env['PATH']
+		self.system ('cd %(builddir)s/ && make -C Source POSSIBLE_CROSS_PREFIXES=i686-mingw32- ')
+			     
+	def patch (self):
+		self.system ('rm -rf %(builddir)s && ln -s %(srcdir)s %(builddir)s')
 		
+	def srcdir (self):
+		d = Tool_package.srcdir (self).replace ('_','-')
+		return d
+
+	def configure (self):
+		pass
+	
+	def install (self):
+		self.system('''
+cd %(builddir)s && ./install.sh %(install_root)s
+cd %(install_root)s/usr/ && mkdir bin && cd bin && ln -s ../share/NSIS/makensis .
+''')
+
+	def package (self):
+		self.system ('tar -C %(install_root)s/usr -zcf %(gub_uploads)s/%(gub_name)s .')
+
+class Scons (Tool_package):
+	def compile (self):
+		pass
+	def patch (self):
+		pass
+	
+	def configure (self):
+		pass
+	def install_command (self):
+		return 'python %(srcdir)s/setup.py install --prefix=%(tooldir)s --root=%(install_root)s'
+	def package (self):
+		self.system ('tar -C %(install_root)s/%(tooldir)s/ -zcf %(gub_uploads)s/%(gub_name)s .')
+	
 def get_packages (settings):
-	packages_dict = {
-		'linux': [],
-		'darwin': [
+	return [
+		Nsis (settings).with (version='2.06',
+				      mirror="http://ftp.debian.org/debian/pool/main/n/nsis/nsis_%(version)s.orig.tar.%(format)s",
+				      
+				      format="gz"),
+
+		Scons (settings).with (version='0.96.91',
+				       format = 'gz',
+				       mirror=download.sf),
 		Pkg_config (settings).with (version="0.20",
 					    mirror=download.freedesktop),
 		Guile (settings).with (version='1.6.7',
 				       mirror=download.gnu, format='gz',
- 
 				       ),
 		Flex (settings).with (version="2.5.4a",
 				      mirror=download.nongnu, format='gz')
-		]}
-
-
-	return packages_dict[settings.build_platform]
+		]
 
 
