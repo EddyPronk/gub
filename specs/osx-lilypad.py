e45749357380610404c9dab4cbf4ae52f6053644
hanwen <hanwen@xs4all.nl>
2006-02-25 01:51:27 +0100

    rename class to Osx_lilypad.

--- specs/osx-lilypad.py
+++ specs/osx-lilypad.py
@@ -1,9 +1,7 @@
 import download
 import gub
-import targetpackage
 
-class OSX_Lilypad (gub.Null_package):
+class Osx_lilypad (gub.Null_package):
 	def __init__ (self, settings):
-		targetpackage.Target_package.__init__ (self, settings)
-		self.with.with (version="0.0", mirror=download.hw, depends=[])
-
+		gub.Null_package.__init__ (self, settings)
+		self.with (version="0.0", mirror=download.hw, depends=[])
