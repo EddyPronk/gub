ec22eee4d1b9a1b3ab0588227ed193b9a3a40919
hanwen <hanwen@xs4all.nl>
2006-03-01 20:49:03 +0100

    gcc 4.1 for linux.

--- specs/linux.py
+++ specs/linux.py
@@ -46,8 +46,8 @@ def get_packages (settings, names):
 		Libc6_dev (settings).with (version='2.2.5-11.8', mirror=download.glibc_deb, format='deb'),
 		Linux_kernel_headers (settings).with (version='2.6.13+0rc3-2', mirror=download.lkh_deb, format='deb'),
 		cross.Binutils (settings).with (version='2.16.1', format='bz2'),
-		cross.Gcc (settings).with (version='4.0.2',
-					   mirror=download.gcc, format='bz2',
+		cross.Gcc (settings).with (version='4.1.0',
+					   mirror=download.gcc_41, format='bz2',
 					   depends=['binutils']),
 		]
 	return packages
