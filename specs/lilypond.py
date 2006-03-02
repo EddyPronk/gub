13018b72a24c70faa80a8ad7ce1438423c5444eb
janneke <janneke@gnu.org>
2006-03-02 13:24:49 +0100

    LilyPond__cygwin depends on glib2-*; cygwin ships glib-1.x as default.

--- specs/lilypond.py
+++ specs/lilypond.py
@@ -123,8 +123,8 @@ class LilyPond__cygwin (LilyPond):
 	def __init__ (self, settings):
 		LilyPond.__init__ (self, settings)
 		self.with (version=settings.lilypond_branch, mirror=cvs.gnu,
-			   depends=['fontconfig', 'freetype2', 'guile', 'pango', 'python'],
-			   builddeps=['gettext-devel', 'glib-devel', 'guile', 'libfreetype2-devel', 'pango-devel', 'python'],
+			   depends=['fontconfig', 'freetype2', 'glib2', 'guile', 'pango', 'python'],
+			   builddeps=['gettext-devel', 'glib2-devel', 'guile', 'libfreetype2-devel', 'pango-devel', 'python'],
 			   track_development=True)
 
         def patch (self):
