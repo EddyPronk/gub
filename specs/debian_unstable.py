a25330d3fd84f2b28702e5c3bced426e707b3b34
hanwen <hanwen@lilypond.org>
2006-03-10 15:24:57 +0100

    move debian package generation to debian_unstable.py

--- specs/debian_unstable.py
+++ specs/debian_unstable.py
@@ -3,9 +3,14 @@ import os
 import cross
 import xpm
 
+from new import classobj
+
+mirror = 'http://ftp.de.debian.org/debian'
+
 def get_packages (settings, names):
 	p = xpm.Debian_package_manager (settings)
-        url = p.mirror + '/dists/unstable/main/binary-i386/Packages.gz'
+        url = mirror + '/dists/unstable/main/binary-i386/Packages.gz'
+	
 	# FIXME: download/offline
 	downloaddir = settings.downloaddir
 	file = settings.downloaddir + '/Packages'
@@ -16,3 +21,37 @@ def get_packages (settings, names):
 
 def change_target_packages (packages):
 	cross.change_target_packages (packages)
+
+
+def get_debian_packages (settings, package_file):
+	return map (lambda j: get_debian_package (settings, j),
+		    open (package_file).read ().split ('\n\n')[:-1])
+
+def get_debian_package (settings, description):
+	s = description[:description.find ('\nDescription')]
+	d = dict (map (lambda line: line.split (': ', 1),
+		       map (string.strip, s.split ('\n'))))
+	Package = classobj (d['Package'], (gub.Binary_package,), {})
+	package = Package (settings)
+	package.name_dependencies = []
+	if d.has_key ('Depends'):
+		deps = map (string.strip,
+			    re.sub ('\([^\)]*\)', '',
+				    d['Depends']).split (', '))
+		# FIXME: BARF, ignore choices
+		deps = filter (lambda x: x.find ('|') == -1, deps)
+		# FIXME: how to handle Provides: ?
+		# FIXME: BARF, fixup libc Provides
+		deps = map (lambda x: re.sub ('libc($|-)', 'libc6\\1',
+					      x), deps)
+		# FIXME: ugh, skip some
+		blacklist = ('perl', 'perl-modules', 'perl-base')
+		deps = filter (lambda x: x not in blacklist, deps)
+		package.name_dependencies = deps
+
+	package.name_build_dependencies = []
+	package.ball_version = d['Version']
+	package.url = mirror + '/' + d['Filename']
+	package.format = 'deb'
+
+	return package
