58aa8589937a7a40a7bfc5a61bfb6ea6b5efc5e8
Han-Wen Nienhuys <hanwen@xs4all.nl>
2006-03-12 19:18:11 +0100

    dump scons hdr too.

--- specs/scons.py
+++ specs/scons.py
@@ -13,9 +13,10 @@ class Scons (Tool_package):
 	
 	def install_command (self):
 		return 'python %(srcdir)s/setup.py install --prefix=%(buildtools)s --root=%(install_root)s'
-	def package (self):
-		self.system ('tar -C %(install_root)s/%(buildtools)s/ -zcf %(gub_uploads)s/%(gub_name)s .')
 
+	def package (self):
+		self.system ('tar -C %(install_root)s/%(buildtools)s/../ -zcf %(gub_uploads)s/%(gub_name)s .')
+		self.dump_header_file()
 	
 	def __init__ (self, settings):
 		Tool_package.__init__ (self, settings)
