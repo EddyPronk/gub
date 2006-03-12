da4a11e6051fd244163c1af730e9cd123d8551a1
hanwen <hanwen@lilypond.org>
2006-03-12 19:05:43 +0100

    icoutils tweak

--- specs/icoutils.py
+++ specs/icoutils.py
@@ -2,6 +2,6 @@ from toolpackage import Tool_package
 
 class Icoutils (Tool_package):
 	def __init__ (self, settings):
-		Tool_package.__init__ (settings)
+		Tool_package.__init__ (self, settings)
 		self.with (version='0.26.0',
 			   mirror='http://savannah.nongnu.org/download/icoutils/icoutils-%(version)s.tar.gz'),
