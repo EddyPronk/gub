2b03cc658c4eb8c5adc7f5553aeb1a7049c2ca22
hanwen <hanwen@lilypond.org>
2006-03-13 09:09:13 +0100

    libpng depends for icoutil.

--- specs/icoutils.py
+++ specs/icoutils.py
@@ -4,4 +4,5 @@ class Icoutils (Tool_package):
 	def __init__ (self, settings):
 		Tool_package.__init__ (self, settings)
 		self.with (version='0.26.0',
-			   mirror='http://savannah.nongnu.org/download/icoutils/icoutils-%(version)s.tar.gz'),
+			   mirror='http://savannah.nongnu.org/download/icoutils/icoutils-%(version)s.tar.gz',
+			   depends=['libpng']),
