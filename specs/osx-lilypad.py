52ee3b3e3a17cf65ad36d006c535b0e147a321fd
hanwen <hanwen@lilypond.org>
2006-03-23 09:13:07 +0100

    osx lilypad 0.2

--- specs/osx-lilypad.py
+++ specs/osx-lilypad.py
@@ -4,4 +4,4 @@ import gub
 class Osx_lilypad (gub.Null_package):
 	def __init__ (self, settings):
 		gub.Null_package.__init__ (self, settings)
-		self.with (version="0.1", mirror=download.hw, depends=[])
+		self.with (version="0.2", mirror=download.hw, depends=[])
