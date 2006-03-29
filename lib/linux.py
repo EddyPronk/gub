bce9bd05eca584338bd82240a45769a346c3c55c
hanwen <hanwen@lilypond.org>
2006-03-29 08:13:59 +0100

    lkh at rc3-2.1 (thanks pedro.)

--- lib/linux.py
+++ lib/linux.py
@@ -49,7 +49,7 @@ def get_cross_packages (settings):
 	packages = [
 		Libc6 (settings).with (version='2.2.5-11.8', mirror=download.glibc_deb, format='deb'),
 		Libc6_dev (settings).with (version='2.2.5-11.8', mirror=download.glibc_deb, format='deb'),
-		Linux_kernel_headers (settings).with (version='2.6.13+0rc3-2', mirror=download.lkh_deb, format='deb'),
+		Linux_kernel_headers (settings).with (version='2.6.13+0rc3-2.1', mirror=download.lkh_deb, format='deb'),
 		cross.Binutils (settings).with (version='2.16.1', format='bz2'),
 		cross.Gcc (settings).with (version='4.0.2',
 					   mirror=download.gcc, format='bz2',
