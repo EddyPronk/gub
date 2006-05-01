e2c9e539f1bd5e185507b01bcf22c4dc85884966
hanwen <hanwen@lilypond.org>
2006-05-01 19:13:05 +0100

    bump mftrace version.

--- specs/mftrace.py
+++ specs/mftrace.py
@@ -3,5 +3,5 @@ from toolpackage import Tool_package
 class Mftrace (Tool_package):
     def __init__ (self, settings):
         Tool_package.__init__ (self, settings)
-        self.with (version='1.1.19',
-             mirror="http://www.xs4all.nl/~hanwen/mftrace/mftrace-1.1.19.tar.gz")
+        self.with (version='1.2.3',
+                   mirror="http://www.xs4all.nl/~hanwen/mftrace/mftrace-1.2.3.tar.gz")
