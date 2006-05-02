cbeb81a7a66cc49e9a0b5695b80ea3586f1b4263
Han-Wen Nienhuys <hanwen@xs4all.nl>
2006-05-02 11:33:19 +0100

    ff 20060501

--- specs/fontforge.py
+++ specs/fontforge.py
@@ -10,9 +10,12 @@ class Fontforge (Tool_package):
 
 	def configure_command (self):
 		return Tool_package.configure_command (self) + " --without-freetype-src "
-
+	def patch (self):
+		Tool_package.patch (self)
+		self.system ("cd %(srcdir)s && patch -p0 < %(patchdir)s/fontforge-20060501-srcdir.patch")
+		
 	def __init__ (self, settings):
 		Tool_package.__init__ (self, settings)
 		self.with (mirror="http://fontforge.sourceforge.net/fontforge_full-%(version)s.tar.bz2",
-					 version="20060125"),
+			   version="20060501"),
 		
