ff6036c7014c5fcfb4fe333ffe499832ef33f615
hanwen <hanwen@lilypond.org>
2006-05-04 10:45:14 +0100

    bump urw release.

--- specs/urw-fonts.py
+++ specs/urw-fonts.py
@@ -3,7 +3,7 @@ import gub
 class Urw_fonts (gub.Binary_package):
     def __init__ (self, settings):
         gub.Binary_package.__init__ (self, settings)
-        self.with(version="1.0.7pre40",
+        self.with(version="1.0.7pre41",
                   mirror="ftp://ftp.gnome.ru/fonts/urw/release/urw-fonts-%(version)s.tar.bz2")
     def install (self):
         self.system ('mkdir -p %(install_root)s/usr/share/fonts/default/Type1')
