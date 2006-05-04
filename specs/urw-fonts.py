04ba0f8aa3317ac3b1cf5ba61d8c428664bfe461
hanwen <hanwen@lilypond.org>
2006-05-04 10:43:15 +0100

    add specs/urw-fonts.py

---
+++ specs/urw-fonts.py
@@ -0,0 +1,14 @@
+import gub
+
+class Urw_fonts (gub.Binary_package):
+    def __init__ (self, settings):
+        gub.Binary_package.__init__ (self, settings)
+        self.with(version="1.0.7pre40",
+                  mirror="ftp://ftp.gnome.ru/fonts/urw/release/urw-fonts-%(version)s.tar.bz2")
+    def install (self):
+        self.system ('mkdir -p %(install_root)s/usr/share/fonts/default/Type1')
+        self.system ('cp %(srcdir)s/root/* %(install_root)s/usr/share/fonts/default/Type1/')
+    def package (self):
+        gub.Package.package (self)
+    
+
