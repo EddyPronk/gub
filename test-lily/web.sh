047c1632725434204f72636cf4e138000462c766
hanwen <hanwen@lilypond.org>
2006-05-17 12:09:18 +0100

    proper quoting.

--- test-lily/web.sh
+++ test-lily/web.sh
@@ -10,8 +10,8 @@ set -x
 make download \
 && python test-gub.py --to hanwen@xs4all.nl --to janneke@gnu.org \
    --from builddaemon@lilypond.org --repository downloads/lilypond-$BRANCH/  \
-    --smtp $SMTPSERVER --quiet make $BUILD_PLATFORM \
+    --smtp $SMTPSERVER --quiet "make $BUILD_PLATFORM" \
 && python test-gub.py --to hanwen@xs4all.nl --to janneke@gnu.org \
    --from builddaemon@lilypond.org --repository downloads/lilypond-$BRANCH/  \
-    --smtp $SMTPSERVER --quiet make doc \
+    --smtp $SMTPSERVER --quiet "make doc" \
 && find target/$BUILD_PLATFORM/build/lilypond-$BRANCH/ -name 'lily-[0-9]*' -mtime +1   -exec rm '{}' ';'
