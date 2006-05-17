20e70014c21f2b6e162ecf97e03e012835558fd1
hanwen <hanwen@lilypond.org>
2006-05-17 11:52:57 +0100

    add test-lily/web.sh

---
+++ test-lily/web.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+BUILD_PLATFORM="$1"
+BRANCH="$2"
+SMTPSERVER="$3"
+
+make download \
+&& python test-gub.py --to hanwen@xs4all.nl --to janneke@gnu.org \
+   --from builddaemon@lilypond.org --repository downloads/lilypond-$BRANCH/  \
+    --smtp $SMTPSERVER --quiet make $BUILD_PLATFORM \
+&& python test-gub.py --to hanwen@xs4all.nl --to janneke@gnu.org \
+   --from builddaemon@lilypond.org --repository downloads/lilypond-$BRANCH/  \
+    --smtp $SMTPSERVER --quiet make doc \
+&& find target/$BUILD_PLATFORM/build/lilypond-$BRANCH/ -name 'lily-[0-9]*' -mtime +1   -exec rm '{}' ';'
