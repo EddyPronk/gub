5efa1f63bec7ef4237a7154fbede4476a3f4e953
Han-Wen Nienhuys <hanwen@lilypond.org>
2006-05-22 21:02:52 +0100

    doc-build iso doc

--- test-lily/web.sh
+++ test-lily/web.sh
@@ -16,7 +16,7 @@ make BRANCH=$BRANCH download \
 && python test-gub.py --to hanwen@xs4all.nl --to janneke@gnu.org \
    --from builddaemon@lilypond.org --repository downloads/lilypond-$BRANCH/  \
     --posthook "find target/$BUILD_PLATFORM/build/lilypond-$BRANCH/ -name 'lily-[0-9]*' -mtime +1   -exec rm '{}' ';' -print" \
-    --smtp $SMTPSERVER --quiet "make BRANCH=$BRANCH doc" \
+    --smtp $SMTPSERVER --quiet "make BRANCH=$BRANCH doc-build" \
 
 
  
