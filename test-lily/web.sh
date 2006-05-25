f35461b93b332f086bc042fcd4d8be58c334b068
Han-Wen Nienhuys <hanwen@lilypond.org>
2006-05-25 09:40:08 +0100

    send results to lilypond-cvs@gnu.org

--- test-lily/web.sh
+++ test-lily/web.sh
@@ -10,10 +10,10 @@ set -x
 echo Build at `date`
 
 make BRANCH=$BRANCH download \
-&& python test-gub.py --to hanwen@xs4all.nl --to janneke@gnu.org \
+&& python test-gub.py --to hanwen@xs4all.nl --to lilypond-cvs@gnu.org \
    --from builddaemon@lilypond.org --repository downloads/lilypond-$BRANCH/  \
     --smtp $SMTPSERVER --quiet "make BRANCH=$BRANCH $BUILD_PLATFORM" \
-&& python test-gub.py --to hanwen@xs4all.nl --to janneke@gnu.org \
+&& python test-gub.py --to hanwen@xs4all.nl --to lilypond-cvs@gnu.org \
    --from builddaemon@lilypond.org --repository downloads/lilypond-$BRANCH/  \
     --posthook "find target/$BUILD_PLATFORM/build/lilypond-$BRANCH/ -name 'lily-[0-9]*' -mtime +1   -exec rm '{}' ';' -print" \
     --smtp $SMTPSERVER --quiet "make BRANCH=$BRANCH doc-build" \
