12b5bc8541b1028be13243d8cb6c0f139ccd40a9
hanwen <hanwen@lilypond.org>
2006-05-28 20:44:31 +0100

    nsis 2.17

--- specs/nsis.py
+++ specs/nsis.py
@@ -4,7 +4,7 @@ import os
 class Nsis (ToolBuildSpec):
     def __init__ (self, settings):
         ToolBuildSpec.__init__(self, settings)
-        self.with (version='2.16',
+        self.with (version='2.17',
                    mirror="http://surfnet.dl.sourceforge.net/sourceforge/%(name)s/%(name)s-%(version)s-src.tar.%(format)s",
                    
                    format="bz2")
@@ -14,11 +14,11 @@ class Nsis (ToolBuildSpec):
 
     def patch (self):
   
-        if 1: # 2.16
+        if 0: # 2.16
             self.system ("cd %(srcdir)s && patch -p1 < %(patchdir)s/nsis-2.16-macos.patch")
             self.system ("cd %(srcdir)s && patch -p0 < %(patchdir)s/nsis-2.16-platform-h.patch")
-            self.system ('mkdir -p %(allbuilddir)s', ignore_error=True)
-            self.system ('ln -s %(srcdir)s %(builddir)s') 
+        self.system ('mkdir -p %(allbuilddir)s', ignore_error=True)
+        self.system ('ln -s %(srcdir)s %(builddir)s') 
 		
     def configure (self):
         pass
@@ -27,15 +27,13 @@ class Nsis (ToolBuildSpec):
         ## no trailing / in paths!
         return (' scons PREFIX=%(system_root)s/usr PREFIX_DEST=%(install_root)s '
                 ' DEBUG=yes '
-
-                ## /s switch doesn't work anymore?!
                 ' NSIS_CONFIG_LOG=yes '
                 ' SKIPPLUGINS=System')
     
     def compile (self): 
         env = {'PATH': '%(topdir)s/target/mingw/system/usr/cross/bin:' + os.environ['PATH']}
         self.system ('cd %(builddir)s/ && %(compile_command)s',
-              env)
+                     env)
 
     def install (self):
         env = {'PATH': '%(topdir)s/target/mingw/system/usr/cross/bin:' + os.environ['PATH']}
