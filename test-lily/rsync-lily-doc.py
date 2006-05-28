877b8adcd4e18026df24733550c6e4ac01fafeb9
hanwen <hanwen@lilypond.org>
2006-05-28 22:37:06 +0100

    cleanup of rsync-lily-doc.py

--- test-lily/rsync-lily-doc.py
+++ test-lily/rsync-lily-doc.py
@@ -4,89 +4,98 @@ import re
 import sys
 import string
 
-host = 'hanwen@ssh.webdev.nl'
-home = os.environ['HOME']
-unpack_dir = home + '/nobackup/lilypond-local-web/doc'
-MAJOR_VERSION=None
-branch = 'HEAD'
+import optparse
 
-py_platform_mapping = {
-	'darwin': 'darwin-ppc',
-	'linux2': 'linux',
-}
-platform = py_platform_mapping[sys.platform]
+################
+# settings
+
+def parse_options ():
+    p = optparse.OptionParser ()
+    p.add_option ('--upload',
+		  dest='destination',
+		  help='where to upload the result',
+		  default='hanwen@ssh.webdev.nl:/var/www/lilypond/doc')
+
+    home = os.environ['HOME']
+    p.add_option ('--unpack-dir',
+		  dest='unpack_dir',
+		  default=home + '/nobackup/lilypond-local-web/doc'
+		  help="Where to put local versions of the docs")
+
+    (opts, args) = p.parse_args ()
+    return (opts, args)
 
 def do_urchin (filename):
-	s = open (filename).read()
-	if re.search ('UA-68969', s):
-		return
-	
-	print 'instrumenting ', filename
-	urchin_track = """<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
-	</script>
-	<script type="text/javascript">
+    s = open (filename).read()
+    if re.search ('UA-68969', s):
+    	return
+    
+    print 'instrumenting ', filename
+    urchin_track = """<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+</script>
+<script type="text/javascript">
 _uacct = "UA-68969-1";
 urchinTracker();
 </script>"""
-	
-	s = re.sub ("(?i)</head>", urchin_track + '\n</head>', s)
-	open (filename, 'w').write (s)
+    
+    s = re.sub ("(?i)</head>", urchin_track + '\n</head>', s)
+    open (filename, 'w').write (s)
 
 def system (cmd):
-	print cmd
-	stat = os.system (cmd)
-	if stat:
-		raise 'fail'
-def read_version (vf):
-	print 'reading', vf 
-	for l in open (vf).readlines():
-		def f(m):
-			globals()[m.group(1)] = string.atoi (m.group(2))
-			return ''
+    print cmd
+    stat = os.system (cmd)
+    if stat:
+    	raise 'fail'
 
-		l = re.sub ('^(MAJOR_VERSION|MINOR_VERSION|PATCH_LEVEL) *= *(.*)', f, l)
+def read_version (source):
+    return tuple (open (source + '/VERSION').read ().split ('.'))
+    
+def create_local_web_dir (options, source):
+    os.chdir (options.unpack_dir)
 
-if sys.argv[1:]:
-	branch = sys.argv[1]
-	
-cwd = os.getcwd ()
-for dir in [cwd, cwd + '/downloads/lilypond-%s/' % branch]:
-	if os.path.exists (dir + '/VERSION'):
-		read_version (dir + '/VERSION')
-		break
-	
-if not MAJOR_VERSION:
-	raise 'not in lilydir'
+    version = read_version (source)
+    dir = 'v%s' % '.'.join (version)
+    
+    system ('rm -rf %s' % dir)
+    system ('mkdir -p %s' % dir)
+    os.chdir (dir)
+    
+    system ('rsync -a %s/ . ' % webroot)
+    system ('chmod -R g+w . ' )
+    system ('chgrp -R lilypond . ' )
+    system ('chmod 2755 `find -type d ` . ')
 
-webroot = None
-for dir in [cwd, cwd +'/target/%s/build/lilypond-%s/' % (platform, branch)]:
-	if os.path.exists (dir + '/out-www/web-root/'):
-		webroot = dir +  '/out-www/web-root/'
-		break
-	
+    for f in ['Documentation/index.html',
+	      'Documentation/topdocs/NEWS.html',
+	      'Documentation/user/lilypond/index.html',
+	      'Documentation/user/lilypond-internals/index.html',
+	      'examples.html',
+	      'Documentation/user/music-glossary/index.html',
+	      'Documentation/topdocs/NEWS.html',
+	      'Documentation/user/lilypond/Tutorial.html',
+	      'input/test/collated-files.html',
+	      'input/regression/collated-files.html']:
+        do_urchin (f)
 
-print 'dir=', unpack_dir
-os.chdir (unpack_dir)
-dir = 'v%s.%s' % (MAJOR_VERSION, MINOR_VERSION)
-system ('rm -rf %s' % dir)
-system ('mkdir  -p %s' % dir)
-os.chdir (unpack_dir + '/' + dir)
-system ('rsync -a %s/ . ' % webroot) # --link
-system ('chmod -R g+w . ' )
-system ('chgrp -R lilypond . ' )
-system ('chmod 2755 `find -type d ` . ')
 
-for f in ['Documentation/index.html',
-	  'Documentation/topdocs/NEWS.html',
-	  'Documentation/user/lilypond/index.html',
-	  'Documentation/user/lilypond-internals/index.html',
-	  'examples.html',
-	  'Documentation/user/music-glossary/index.html',
-	  'Documentation/topdocs/NEWS.html',
-	  'Documentation/user/lilypond/Tutorial.html',
-	  'input/test/collated-files.html',
-	  'input/regression/collated-files.html']:
-	do_urchin (f)
+def upload (options, source):
+    os.chdir (options.unpack_dir)
+    version = read_version (source)
 
-system ('rsync --delete --stats --progress -pgorltvu -e ssh . %s:/var/www/lilypond/doc/%s/' % (host, dir))
+    dir = 'v%s' % '.'.join (version)
+    os.chdir (dir)
+    branch_dir = 'v%s.%s' % '.'.join (version[:2])
+    system ('rsync --delete --stats --progress -pgorltvu -e ssh . %s/%s/' % (options.destination, branch_dir))
+    
+    
+def main ():
+    (opts, args) = parse_options ()
 
+    for a in args:
+	create_local_web_dir (opts, a)
+	if opts.destination:
+	    upload (opts, a)
+
+
+if __name__ == '__main__':
+    main ()
