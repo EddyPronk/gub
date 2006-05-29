b6e71dd1f2e5ce8e2a7c8a994f5b239f0edfc743
hanwen <hanwen@lilypond.org>
2006-05-29 11:34:38 +0100

    run output-distance.py script on a couple of previous versions
    automatically.

--- test-lily/rsync-lily-doc.py
+++ test-lily/rsync-lily-doc.py
@@ -1,28 +1,38 @@
 #!/usr/bin/python
+
 import os
 import re
 import sys
 import string
-
+import glob
 import optparse
 
 ################
 # settings
-
+# 'hanwen@ssh.webdev.nl:/var/www/lilypond/doc'
 def parse_options ():
     p = optparse.OptionParser ()
     p.add_option ('--upload',
 		  dest='destination',
 		  help='where to upload the result',
-		  default='hanwen@ssh.webdev.nl:/var/www/lilypond/doc')
+		  default='')
+    p.add_option ('--output-distance',
+                  dest="output_distance_script",
+                  help="compute signature distances using script") 
 
     home = os.environ['HOME']
     p.add_option ('--unpack-dir',
 		  dest='unpack_dir',
-		  default=home + '/nobackup/lilypond-local-web/doc'
+		  default='uploads/webdoc/',
 		  help="Where to put local versions of the docs")
 
     (opts, args) = p.parse_args ()
+
+    opts.unpack_dir = os.path.abspath (opts.unpack_dir)
+
+    if not args:
+        p.print_help()
+    
     return (opts, args)
 
 def do_urchin (filename):
@@ -30,7 +40,7 @@ def do_urchin (filename):
     if re.search ('UA-68969', s):
     	return
     
-    print 'instrumenting ', filename
+    print filename
     urchin_track = """<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
 </script>
 <script type="text/javascript">
@@ -48,9 +58,16 @@ def system (cmd):
     	raise 'fail'
 
 def read_version (source):
-    return tuple (open (source + '/VERSION').read ().split ('.'))
+    s = open (source + '/VERSION').read ()
+    s = s.strip()
+    return tuple (s.split ('.'))
     
 def create_local_web_dir (options, source):
+
+    if not os.path.isdir (options.unpack_dir):
+        system ('mkdir -p '  + options.unpack_dir)
+
+    print 'creating web root in',  options.unpack_dir 
     os.chdir (options.unpack_dir)
 
     version = read_version (source)
@@ -60,11 +77,12 @@ def create_local_web_dir (options, source):
     system ('mkdir -p %s' % dir)
     os.chdir (dir)
     
-    system ('rsync -a %s/ . ' % webroot)
+    system ('rsync -a %s/ . ' % source)
     system ('chmod -R g+w . ' )
     system ('chgrp -R lilypond . ' )
     system ('chmod 2755 `find -type d ` . ')
 
+    print 'Instrumenting for Google Analytics' 
     for f in ['Documentation/index.html',
 	      'Documentation/topdocs/NEWS.html',
 	      'Documentation/user/lilypond/index.html',
@@ -77,14 +95,65 @@ def create_local_web_dir (options, source):
 	      'input/regression/collated-files.html']:
         do_urchin (f)
 
+def compute_distances (options, source):
+    os.chdir (options.unpack_dir)
+    
+    cur_version = tuple (map (int, read_version (source)))
+    region = 3
+
+    versions = [(cur_version[0], cur_version[1], cur_version[2] - i) for i in range (1, region + 1)]
+    version_dirs = ['v%d.%d.%d' % v for v in versions]
+    print cur_version
+    version_str = '%d.%d.%d'  % cur_version
+
+    if cur_version[1] % 2 == 1:
+        stable_major = cur_version[1] - 1
+
+        stable_dirs = glob.glob ('v%d.%d.*' % (cur_version[0], stable_major))
+
+        if stable_dirs:
+            stable_dirs.sort ()
+            stable_dirs.reverse ()
+            version_dirs.append (stable_dirs[0])
+
+#    version_dirs = [d for d in version_dirs if
+# os.path.isdir (os.path.join (options.unpack_dir, d))]
+
+    cur_dir = 'v' + version_str
+
+    html = ''
+    for d in version_dirs:
+        cmd = 'python %s %s %s ' % (options.output_distance_script,
+                                    d, cur_dir)
+
+        base = os.path.split (d)[1]
+
+        html += '<li><a href="%(base)s.html">results for %(base)s</a>' % locals()
+        
+        print cmd
+
+    html = '''<html>
+<head>
+<title>
+Regression test results for %(version_str)s
+</title>
+</head>
+<body>
+<h1>Regression test results</h1>
+<ul>%(html)s</ul>
+</body>
+</html>
+''' % locals() 
+    
+    open (cur_dir + '/test-results.html', 'w').write (html)
 
 def upload (options, source):
     os.chdir (options.unpack_dir)
-    version = read_version (source)
 
+    version = read_version (source)
     dir = 'v%s' % '.'.join (version)
     os.chdir (dir)
-    branch_dir = 'v%s.%s' % '.'.join (version[:2])
+    branch_dir = 'v%s.%s' % (version[:2])
     system ('rsync --delete --stats --progress -pgorltvu -e ssh . %s/%s/' % (options.destination, branch_dir))
     
     
@@ -93,6 +162,8 @@ def main ():
 
     for a in args:
 	create_local_web_dir (opts, a)
+        if opts.output_distance_script:
+            compute_distances (opts, a)
 	if opts.destination:
 	    upload (opts, a)
 
