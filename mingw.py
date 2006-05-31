ee826b09555ea0a1086c2bdcd8411ef932eae2c7
lilytest <hanwen@xs4all.nl>
2006-05-31 23:32:06 +0100

    remove mingw.py cygwin.py

--- mingw.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# debug helper for python-mode's jumping to exception file:line errors
-
-#import "gub-builder" as gub_builder
-import gub_builder
-import sys
-
-sys.argv = ['bar', '--target-platform=mingw', '--keep', 'build', 'lilypond']
-gub_builder.main ()
