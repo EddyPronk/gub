a017a257cd4c8d95347ec2890dfe8a01a7256cd0
janneke <janneke@gnu.org>
2006-06-19 14:27:45 +0100

    Add verbosity to install tarring.

--- lib/gub.py
+++ lib/gub.py
@@ -570,7 +570,10 @@ rm -rf %(srcdir)s %(builddir)s %(install_root)s
         follow this command, since it will erase the old install_root first."""
         
         self.system ('mkdir -p %(install_root)s')
-        self.system ('tar -C %(srcdir)s/root -cf- . | tar -C %(install_root)s -xf-')
+        _verbose = ''
+        if self.verbose:
+            _verbose = ' -v'
+        self.system ('tar -C %(srcdir)s/root -cf- . | tar -C %(install_root)s%(_verbose)s -xf-', env=locals ())
         self.libtool_installed_la_fixups ()
 
 class NullBuildSpec (BuildSpec):
