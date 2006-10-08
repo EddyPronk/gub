d81b85810fb787e4f85afd7df98276ccb73b3cb3
Jan Nieuwenhuizen <janneke@gnu.org>
2006-10-08 18:34:34 +0100

    Fix icotutils url.

--- specs/icoutils.py
+++ specs/icoutils.py
@@ -3,9 +3,8 @@ from toolpackage import ToolBuildSpec
 class Icoutils (ToolBuildSpec):
     def __init__ (self, settings):
         ToolBuildSpec.__init__ (self, settings)
-        self.with (version='0.26.0',
-                   mirror='http://savannah.nongnu.org/download/icoutils/icoutils-%(version)s.tar.gz',
-                   ),
+	import download
+        self.with (version='0.26.0', mirror=download.nongnu_savannah)
     def get_build_dependencies (self):
         return ['libpng-devel']
     def get_dependency_dict (self):
