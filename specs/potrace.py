b5738de6664996f883243de4893185c66cf275fa
Jan Nieuwenhuizen <janneke@gnu.org>
2006-10-08 18:28:17 +0100

    Fix potrace url.

--- specs/potrace.py
+++ specs/potrace.py
@@ -3,5 +3,5 @@ from toolpackage import ToolBuildSpec
 class Potrace (ToolBuildSpec):
     def __init__ (self, settings):
         ToolBuildSpec.__init__ (self, settings)
-        self.with (mirror="http://potrace.sourceforge.net/download/potrace-%(version)s.tar.gz",
-             version="1.7"),
+	import download
+        self.with (mirror=download.sf, version="1.7"),
