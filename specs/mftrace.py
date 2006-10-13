60e5f6b5b3e6e94d63c515d2d6d149104c792717
Han-Wen Nienhuys <hanwen@lilypond.org>
2006-10-13 15:32:49 +0100

    bump mftrace to 1.2.5 ; remove license_file() kludge.

--- specs/mftrace.py
+++ specs/mftrace.py
@@ -3,10 +3,6 @@ from toolpackage import ToolBuildSpec
 class Mftrace (ToolBuildSpec):
     def __init__ (self, settings):
         ToolBuildSpec.__init__ (self, settings)
-        self.with (version='1.2.4',
+        self.with (version='1.2.5',
                    mirror="http://www.xs4all.nl/~hanwen/mftrace/mftrace-%(version)s.tar.gz")
 
-    def license_file (self):
-        # FIXME: not a license file, build fix.
-        return '%(srcdir)s/README.txt'
-		   
