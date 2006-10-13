c42f95bb19f1c84c6ba1a6b3e11cc1fa319ea28d
Jan Nieuwenhuizen <janneke@gnu.org>
2006-10-13 14:20:22 +0100

    Build fix: return README.txt as license file for mftrace.

--- specs/mftrace.py
+++ specs/mftrace.py
@@ -5,3 +5,8 @@ class Mftrace (ToolBuildSpec):
         ToolBuildSpec.__init__ (self, settings)
         self.with (version='1.2.4',
                    mirror="http://www.xs4all.nl/~hanwen/mftrace/mftrace-%(version)s.tar.gz")
+
+    def license_file (self):
+        # FIXME: not a license file, build fix.
+        return '%(srcdir)s/README.txt'
+		   
