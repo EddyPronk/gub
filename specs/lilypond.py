b89a07c7e62174a8e884d304fd0f22fcf321b74b
Han-Wen Nienhuys <hanwen@lilypond.org>
2006-10-16 08:42:29 +0100

    robustness: allow rm out/lilypond to fail.

--- specs/lilypond.py
+++ specs/lilypond.py
@@ -312,7 +312,7 @@ LDFLAGS="%(LDFLAGS)s %(python_lib)s"
                '%(builddir)s/config.make')
 
     def compile (self):
-        self.system ('cd %(builddir)s/lily && rm out/lilypond')
+        self.system ('cd %(builddir)s/lily && rm -f out/lilypond', ignore_error=True)
         LilyPond.compile (self)
         self.system ('cd %(builddir)s/lily && mv out/lilypond out/lilypond-console')
         self.system ('cd %(builddir)s/lily && make MODULE_LDFLAGS="-mwindows" && mv out/lilypond out/lilypond-windows')
