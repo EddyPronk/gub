0ef028a7963ec422fe61cc217d83fcee3fd155bd
Han-Wen Nienhuys <hanwen@lilypond.org>
2006-10-17 14:35:18 +0100

    libiconv 1.11

--- specs/libiconv.py
+++ specs/libiconv.py
@@ -3,7 +3,7 @@ import targetpackage
 class Libiconv (targetpackage.TargetBuildSpec):
     def __init__ (self, settings):
         targetpackage.TargetBuildSpec.__init__ (self, settings)
-        self.with (version='1.9.2')
+        self.with (version='1.11')
     
     def broken_for_distcc (self):
         return True
@@ -15,6 +15,7 @@ class Libiconv (targetpackage.TargetBuildSpec):
         targetpackage.TargetBuildSpec.configure (self)
         # # FIXME: libtool too old for cross compile
         self.update_libtool ()
+        
     def install (self):
         targetpackage.TargetBuildSpec.install (self)
         self.system ('rm %(install_root)s/usr/lib/charset.alias')
