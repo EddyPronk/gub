d5f2ac77c8b8f2e58dc09a6bda5ea0b991115268
Jan Nieuwenhuizen <janneke@gnu.org>
2006-10-19 16:53:00 +0100

    Texlive .exe smurfing.

--- specs/texlive.py
+++ specs/texlive.py
@@ -177,15 +177,27 @@ CFLAGS="-O2 -g -DKPSE_DLL"
         return (Texlive.compile_command (self) + self.makeflags ())
 
     # move to gub?
-    def libtool_broken_exe_install (self):
+    def pre_install_smurf_exe (self):
         import os
         for i in self.locate_files ('%(builddir)s', '*.exe'):
             base = os.path.splitext (i)[0]
-            self.system ('''
-mv %(i)s %(base)s
-''',
-                         locals ())
+            self.system ('''mv %(i)s %(base)s''', locals ())
+
+    # move to gub?
+    def post_install_smurf_exe (self):
+        import os
+        for i in (self.locate_files ('%(install_root)s/bin', '*')
+                  + self.locate_files ('%(install_root)s/usr/bin', '*')):
+            if (not os.path.islink (i)
+                and not os.path.splitext (i)[1]
+                and not self.read_pipe ('file -b %(i)s', locals ()).startswith ('MS-DOS executable PE')):
+                self.system ('''mv %(i)s %(i)s.exe''', locals ())
 
     def install (self):
-    	self.libtool_broken_exe_install ()
+    	self.pre_install_smurf_exe ()
         Texlive.install (self)
+    	self.post_install_smurf_exe ()
+
+        # FIXME: we do this for all cygwin packages
+        self.install_readmes ()
+        
