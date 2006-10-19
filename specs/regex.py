e10e1d06216d21367f2e8993c5eb6cca7bb5af6c
Han-Wen Nienhuys <hanwen@lilypond.org>
2006-10-19 10:10:29 +0100

    regex license.

--- specs/regex.py
+++ specs/regex.py
@@ -5,3 +5,6 @@ class Regex (targetpackage.TargetBuildSpec):
     def __init__ (self, settings):
         targetpackage.TargetBuildSpec.__init__ (self, settings)
         self.with (version='2.3.90-1', mirror=download.hw, format='bz2')
+
+    def license_file (self):
+        return '%(srcdir)s/COPYING.LIB'
