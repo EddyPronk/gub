385be767216985014fad7223deb710473291efb0
hanwen <hanwen@lilypond.org>
2006-10-27 21:12:58 +0100

    test-binary.py robustness

--- test-lily/test-binary.py
+++ test-lily/test-binary.py
@@ -22,7 +22,7 @@ def system (c, ignore_error=False):
 test_settings = eval (open ('test-settings.py').read ())
     
 
-platform_test_aliases = {
+platform_test_script_types = {
     'linux-x86': 'shar',
     'linux-64': 'shar',
     'darwin-ppc' : 'darwin',
@@ -35,12 +35,7 @@ def test_build (bin):
         bin = os.path.abspath (bin)
 
     base = os.path.split (bin)[1]
-
-    platform = ''
-    for p in ('linux-x86', 'freebsd', 'darwin-ppc', 'darwin-x86', 'mingw'):
-        if re.search (p, base):
-            platform = p
-            break
+    platform = re.search ('lilypond-[0-9.]+-[0-9]+.([a-z0-9-]+).*', bin).group (1)
 
     if not platform:
         print 'unknown platform for', bin
@@ -48,13 +43,11 @@ def test_build (bin):
 
     ending_found = 0 
     for e in ['.sh', '.zip', '.exe', 'tar.bz2']:
-        ending_found = ending_found or base.endswith  (e)
+        ending_found = ending_found or bin.endswith  (e)
 
     if not ending_found:
         print 'unknown extension for', base
         return
-    
-    
     
     try:
         os.unlink ('typography-demo.pdf')
@@ -65,33 +58,32 @@ def test_build (bin):
     logdir = "log/"
     try:
         (uid, host, dir, test_file) = test_settings[platform]
-        if test_file == None:
-            test_file = 'test-lily/typography-demo.ly' 
-            
-        base_test_file = os.path.split (test_file)[1]
-        base_test_file_stem = os.path.splitext (base_test_file)[0]
-
-        system ('ssh %(uid)s@%(host)s mkdir  %(dir)s' % locals (), ignore_error=True)
-        system ('ssh %(uid)s@%(host)s rm  %(dir)s/%(base_test_file_stem)s.*' % locals (), ignore_error=True)
-        
-
-        test_platform = platform_test_aliases.get (platform, platform)
-        system ('scp %(test_file)s test-lily/test-%(test_platform)s-gub.sh '
-                ' %(bin)s '
-                ' %(uid)s@%(host)s:%(dir)s/'
-                % locals())
-
-
-        system ('ssh %(uid)s@%(host)s sh %(dir)s/test-%(test_platform)s-gub.sh %(dir)s %(base)s %(base_test_file)s'
-                % locals())
-        system ('scp %(uid)s@%(host)s:%(dir)s/%(base_test_file_stem)s.pdf %(logdir)s/%(base)s.test.pdf'
-                % locals ())
-        system ('xpdf %(logdir)s/%(base)s.test.pdf'
-                % locals ())
-        
     except KeyError:
         system ('touch %(logdir)s/%(base)s.test.pdf' % locals ())
+        return
     
+    if test_file == None:
+        test_file = 'test-lily/typography-demo.ly' 
+
+    base_test_file = os.path.split (test_file)[1]
+    base_test_file_stem = os.path.splitext (base_test_file)[0]
+
+    system ('ssh %(uid)s@%(host)s mkdir  %(dir)s' % locals (), ignore_error=True)
+    system ('ssh %(uid)s@%(host)s rm  %(dir)s/%(base_test_file_stem)s.*' % locals (), ignore_error=True)
+
+    test_platform_script = 'test-%s-gub.sh' % platform_test_script_types.get (platform, platform)
+    system ('scp %(test_file)s test-lily/%(test_platform_script)s '
+            ' %(bin)s '
+            ' %(uid)s@%(host)s:%(dir)s/'
+            % locals())
+
+    system ('ssh %(uid)s@%(host)s sh %(dir)s/%(test_platform_script)s %(dir)s %(base)s %(base_test_file)s'
+            % locals())
+    system ('scp %(uid)s@%(host)s:%(dir)s/%(base_test_file_stem)s.pdf %(logdir)s/%(base)s.test.pdf'
+            % locals ())
+    system ('xpdf %(logdir)s/%(base)s.test.pdf'
+            % locals ())
+
 
 pids = []
 for a in sys.argv[1:]:
