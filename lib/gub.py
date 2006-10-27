f373117b6896110b4573baf8e1e95c961aacd23b
Jan Nieuwenhuizen <janneke@gnu.org>
2006-10-27 17:14:29 +0100

    Bugfix for hdr name expand.  Fixes installability of gup packages.

--- lib/gub.py
+++ lib/gub.py
@@ -42,7 +42,7 @@ class PackageSpec:
            suffix = '-' + dict['vc_branch_suffix']
             
         self._dict['split_ball'] = ('%(gub_uploads)s/%(split_name)s' + suffix +'.%(platform)s.gup') % self._dict
-        self._dict['split_hdr'] = '%(gub_uploads)s/%(split_name)s' + suffix + '%(vc_branch_suffix)s.%(platform)s.hdr' % self._dict
+        self._dict['split_hdr'] = ('%(gub_uploads)s/%(split_name)s' + suffix + '%(vc_branch_suffix)s.%(platform)s.hdr') % self._dict
 
         deps =  ';'.join (self._dependencies)
         self._dict['dependencies_string'] = deps
