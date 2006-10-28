59dbdf25a62331bbc35695e2ebcc025ff09570d4
Jan Nieuwenhuizen <janneke@gnu.org>
2006-10-28 13:16:32 +0100

    Nitpicks.

--- specs/freetype.py
+++ specs/freetype.py
@@ -9,17 +9,17 @@ class Freetype (targetpackage.TargetBuildSpec):
         self.with (version='2.1.10', mirror=download.nongnu_savannah)
 
     def license_file (self):
-        return '%(srcdir)s/docs/LICENSE.TXT' 
+        return '%(srcdir)s/docs/LICENSE.TXT'
 
     def get_build_dependencies (self):
         return ['libtool-devel', 'zlib-devel']
 
     def get_subpackage_names (self):
         return ['devel', '']
-    
+
     def get_dependency_dict (self):
         return {'': ['zlib']}
-        
+
     def configure (self):
 #                self.autoupdate (autodir=os.path.join (self.srcdir (),
 #                                                       'builds/unix'))
@@ -37,13 +37,13 @@ class Freetype (targetpackage.TargetBuildSpec):
     def munge_ft_config (self, file):
         self.file_sub ([('\nprefix=[^\n]+\n', '\nlocal_prefix=yes\nprefix=%(system_root)s/usr\n')],
                        file, must_succeed=True)
-        
+
     def install (self):
         targetpackage.TargetBuildSpec.install (self)
         self.system ('mkdir -p %(install_root)s/usr/cross/bin/')
         self.system ('mv %(install_root)s/usr/bin/freetype-config %(install_root)s/usr/cross/bin/freetype-config')
         self.munge_ft_config ('%(install_root)s/usr/cross/bin/freetype-config')
-                       
+
 class Freetype__mingw (Freetype):
     def configure (self):
         Freetype.configure (self)
@@ -67,7 +67,6 @@ class Freetype__local (toolpackage.ToolBuildSpec, Freetype):
     # FIXME, mi-urg?
     def license_file (self):
         return Freetype.license_file (self)
-
 
     def install (self):
         toolpackage.ToolBuildSpec.install (self)
