1e9866866090acfc614d7add793531310cf1d1ca
Jan Nieuwenhuizen <janneke@gnu.org>
2006-11-06 23:33:07 +0100

    No wrappers for git, that does not work.

--- specs/git.py
+++ specs/git.py
@@ -9,3 +9,9 @@ class Git (toolpackage.ToolBuildSpec):
         self.shadow_tree ("%(srcdir)s", '%(builddir)s')
     def configure (self):
         self.dump ('prefix=%(system_root)s/usr', '%(builddir)s/config.mak') 
+    def wrap_executables (self):
+        # GIT executables use ancient unix style smart name-based
+        # functionality switching.  Did Linus not read or understand
+        # Standards.texi?
+        pass
+        
