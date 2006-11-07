cfa11fae2d130ad8b6120ad34a5a781def076fb1
janneke <janneke@gnu.org>
2006-11-07 10:10:44 +0100

    Refactor build-platform.py

--- build-platform.py
+++ build-platform.py
@@ -1,20 +1,23 @@
 #! /usr/bin/python
 
-platforms = {
-    'i686-pc-cygwin': 'cygwin',
-    'x86_64-linux-gnu': 'linux-64',
-    'i486-linux-gnu': 'linux-x86',
-    }
+class Gcc_machine:
+    _platforms = {
+	'i686-pc-cygwin': 'cygwin',
+	'x86_64-linux-gnu': 'linux-64',
+	'i486-linux-gnu': 'linux-x86',
+	}
+    def __init__ (self):
+	import os
+	machine = os.popen ('gcc -dumpmachine 2>/dev/null').read ().strip ()
+	self._platform = self._platforms[machine]
+    def __str__ (self):
+	return self._platform
 
 def main ():
-    import os
-    m = os.popen ('gcc -dumpmachine 2>/dev/null').read ()
-    if m.endswith ('\n'):
-	m = m[:-1]
-    if m in platforms:
-	print platforms[m]
-    else:
-	print ''
+    try:
+	print Gcc_machine ()
+    except:
+	print
 
 if __name__ == '__main__':    
     main ()
