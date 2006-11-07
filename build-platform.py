0d97d7cea7c3ac63d37b04704ddcfdecfd928117
hanwen <hanwen@lilypond.org>
2006-11-07 12:49:58 +0100

    robustness: analyse gcc machine string, iso. dicting it.

--- build-platform.py
+++ build-platform.py
@@ -1,23 +1,34 @@
 #! /usr/bin/python
-
-class Gcc_machine:
+import re
+import os
+        
+def platform ():
     _platforms = {
 	'i686-pc-cygwin': 'cygwin',
 	'x86_64-linux-gnu': 'linux-64',
 	'i486-linux-gnu': 'linux-x86',
 	}
-    def __init__ (self):
-	import os
-	machine = os.popen ('gcc -dumpmachine 2>/dev/null').read ().strip ()
-	self._platform = self._platforms[machine]
-    def __str__ (self):
-	return self._platform
 
-def main ():
+    machine = os.popen ('gcc -dumpmachine 2>/dev/null').read ().strip ()
+    
     try:
-	print Gcc_machine ()
-    except:
-	print
+        return _platforms[machine]
+    except KeyError:
+        cpu = machine.split ('-')[0]
+        op_sys = ''
+        if re.search ('i[0-9]86', cpu):
+            cpu = 'x86'
+        elif re.search ('x86_64', cpu):
+            cpu = '64'
+            
+        for a in ['linux', 'freebsd', 'darwin']:
+            if a in machine:
+                op_sys = a
+                
+        return '%s-%s' % (op_sys, cpu) 
+    
+def main ():
+    print platform ()
 
 if __name__ == '__main__':    
     main ()
