31c34774c5fda3cf8ba9e2017fcb38db8cb11d41
hanwen <hanwen@lilypond.org>
2006-12-03 16:53:00 +0100

    zlib toolpackage. mingw cleanup

--- specs/zlib.py
+++ specs/zlib.py
@@ -1,6 +1,7 @@
 import download
 import targetpackage
 import gub
+import toolpackage
 
 class Zlib (targetpackage.TargetBuildSpec):
     def __init__ (self, settings):
@@ -21,10 +22,6 @@ class Zlib (targetpackage.TargetBuildSpec):
     def configure_command (self):
         zlib_is_broken = 'SHAREDTARGET=libz.so.1.2.2'
 
-        ### UGH.
-        if self.settings.platform.startswith ('mingw'):
-            zlib_is_broken = 'target=mingw'
-
         ## doesn't use autoconf configure.
         return zlib_is_broken + ' %(srcdir)s/configure --shared '
 
@@ -40,3 +37,33 @@ class Zlib__mingw (Zlib):
                         ('mgwz','libz'),
                         ],
                        '%(srcdir)s/configure')
+
+    def configure_command (self):
+        zlib_is_broken = 'target=mingw'
+        return zlib_is_broken + ' %(srcdir)s/configure --shared '
+
+class Zlib__local (toolpackage.ToolBuildSpec, Zlib):
+    def __init__ (self, settings):
+        toolpackage.ToolBuildSpec.__init__ (self, settings)
+        self.with (version='1.2.2',
+                   mirror='http://heanet.dl.sourceforge.net/sourceforge/libpng/zlib-1.2.2.tar.gz')
+
+        
+    def install_command (self):
+        return toolpackage.ToolBuildSpec.broken_install_command (self)
+        
+    def install (self):
+        toolpackage.ToolBuildSpec.install (self)
+        self.system ('cd %(install_root)s && mkdir -p ./%(local_prefix)s && cp -av usr/* ./%(local_prefix)s && rm -rf usr')
+
+    def configure_command (self):
+        return Zlib.configure_command (self)
+        
+    def patch (self):
+        ## ugh : C&P
+        toolpackage.ToolBuildSpec.patch (self)
+        
+        self.system ('cp %(sourcefiledir)s/zlib.license %(license_file)s')
+        self.system ('cd %(srcdir)s && patch -p1 < %(patchdir)s/zlib-1.2.2-windows.patch')
+        self.shadow_tree ('%(srcdir)s', '%(builddir)s')
+      
