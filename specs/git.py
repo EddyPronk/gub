d5445fcc0c7c09494dceea3bb192eeec977a1f0a
hanwen <hanwen@lilypond.org>
2006-12-12 19:15:15 +0100

    bump GIT version

--- specs/git.py
+++ specs/git.py
@@ -3,8 +3,8 @@ import toolpackage
 class Git (toolpackage.ToolBuildSpec):
     def __init__ (self, settings):
         toolpackage.ToolBuildSpec.__init__ (self, settings)
-        self.with (mirror="http://kernel.org/pub/software/scm/git/git-1.4.3.tar.bz2",
-                   version="1.4.3")
+        self.with (mirror="http://kernel.org/pub/software/scm/git/git-%(version)s.tar.bz2",
+                   version="1.4.4.2")
     def patch (self):
         self.shadow_tree ("%(srcdir)s", '%(builddir)s')
     def configure (self):
