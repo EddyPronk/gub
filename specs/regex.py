54749a2bdf7fa9807b581588302d6c173f384abd
hanwen <hanwen@lilypond.org>
2006-12-31 14:06:11 +0100

    another download.hw fix

--- specs/regex.py
+++ specs/regex.py
@@ -4,7 +4,8 @@ import targetpackage
 class Regex (targetpackage.TargetBuildSpec):
     def __init__ (self, settings):
         targetpackage.TargetBuildSpec.__init__ (self, settings)
-        self.with (version='2.3.90-1', mirror=download.hw, format='bz2')
+        self.with (version='2.3.90-1',
+                   mirror=download.lilypondorg, format='bz2')
 
     def license_file (self):
         return '%(srcdir)s/COPYING.LIB'
