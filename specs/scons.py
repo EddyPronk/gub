14ad1f2572eb8f5aefdb4c80b368c9b170c92b05
hanwen <hanwen@lilypond.org>
2007-01-08 10:29:31 +0100

    bump scons to .94

--- specs/scons.py
+++ specs/scons.py
@@ -16,7 +16,7 @@ class Scons (ToolBuildSpec):
 
     def __init__ (self, settings):
         ToolBuildSpec.__init__ (self, settings)
-        self.with (version='0.96.92',
+        self.with (version='0.96.94',
                    format='gz',
                    mirror=download.sf),
         
