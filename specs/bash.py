81986fc0d22f4c1ddc9d72f01f8a2919b32d1d4e
hanwen <hanwen@lilypond.org>
2007-02-07 15:53:37 +0100

    add specs/bash.py

---
+++ specs/bash.py
@@ -0,0 +1,14 @@
+import download
+import targetpackage
+
+class Bash  (targetpackage.TargetBuildSpec):
+    def __init__ (self, settings):
+        targetpackage.TargetBuildSpec.__init__ (self, settings)
+        self.with (version='3.2',
+                   mirror='ftp://ftp.cwru.edu/pub/bash/bash-3.2.tar.gz', format='bz2')
+
+    def get_build_dependencies (self):
+        return ['libtool', 'gettext-devel']
+
+    def configure_command (self):
+        return (targetpackage.TargetBuildSpec.configure_command (self))
