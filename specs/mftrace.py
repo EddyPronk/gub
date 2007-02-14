72f7d7685499158e923ada71e72d524f9e780b5c
Han-Wen Nienhuys <hanwen@lilypond.org>
2007-02-14 01:10:27 +0100

    bump mftrace version

--- specs/mftrace.py
+++ specs/mftrace.py
@@ -3,6 +3,6 @@ from toolpackage import ToolBuildSpec
 class Mftrace (ToolBuildSpec):
     def __init__ (self, settings):
         ToolBuildSpec.__init__ (self, settings)
-        self.with (version='1.2.5',
-                   mirror="http://lilypond.org/mftrace/mftrace-%(version)s.tar.gz")
+        self.with (version='1.2.14',
+                   mirror="http://lilypond.org/download/sources/mftrace/mftrace-%(version)s.tar.gz")
 
