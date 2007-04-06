161f6f7de6991e77119c7700ef1e12ae93088b0b
Han-Wen Nienhuys <hanwen@lilypond.org>
2007-04-06 20:18:03 -0300

    automake 1.10

--- specs/automake.py
+++ specs/automake.py
@@ -7,4 +7,4 @@ class Automake(toolpackage.ToolBuildSpec):
     def __init__ (self, settings):
 	toolpackage.ToolBuildSpec.__init__ (self, settings)
 	self.with (mirror=download.gnu,
-		   version="1.9.6", format='bz2')
+		   version="1.10", format='gz')
