c27380cd499fa60b642cbc7a970f8d9993aee5f0
Jan Nieuwenhuizen <janneke@gnu.org>
2007-04-11 15:27:24 +0200

    Nitpicks.

--- lib/distcc.py
+++ lib/distcc.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 
-import sys
 import os
 import re
-import telnetlib
 import socket
+import sys
+import telnetlib
 
 def live_hosts (hosts, port = 3633):
     live = []
     for h in hosts:
         try:
-            t = telnetlib.Telnet(h, port)
+            t = telnetlib.Telnet (h, port)
             t.close ()
         except socket.error:
             continue
@@ -22,14 +22,14 @@ def live_hosts (hosts, port = 3633):
     return live
 
 
-
 def main ():
     exe_name = os.path.split (sys.argv[0])[1]
-    path_comps = [c for c in os.environ['PATH'].split (':') if not re.search ('distcc', c)]
+    path_comps = [c for c in os.environ['PATH'].split (':')
+                  if not re.search ('distcc', c)]
     os.environ['PATH'] = ':'.join (path_comps)
     argv = ['distcc', exe_name] + sys.argv[1:]
     os.execvp ('distcc', argv)
 
 if __name__ == '__main__':
-    main()
+    main ()
 
