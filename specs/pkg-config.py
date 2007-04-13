5ef385a672f52ad3b8d418d0b81b454c0766ba01
Jan Nieuwenhuizen <janneke@gnu.org>
2007-04-13 14:54:30 +0200

    Build fix.

--- specs/pkg-config.py
+++ specs/pkg-config.py
@@ -1,8 +1,12 @@
-from toolpackage import ToolBuildSpec
 import download
+import toolpackage
 
-class Pkg_config (ToolBuildSpec):
+class Pkg_config (toolpackage.ToolBuildSpec):
     def __init__ (self, settings):
-        ToolBuildSpec.__init__ (self, settings)
+        toolpackage.ToolBuildSpec.__init__ (self, settings)
         self.with (version="0.20",
                    mirror=download.freedesktop),
+
+    def configure (self):
+        toolpackage.ToolBuildSpec.configure (self)
+        self.update_libtool ()
