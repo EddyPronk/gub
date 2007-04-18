e425eaa34e2e137f659789ad6547d37e5c4f2f0b
Jan Nieuwenhuizen <janneke@gnu.org>
2007-04-18 22:38:10 +0200

    Make with_tarball friendlier to use.

--- specs/hello.py
+++ specs/hello.py
@@ -1,11 +1,7 @@
 import download
-import repository
 import targetpackage
 
 class Hello (targetpackage.TargetBuildSpec):
     def __init__ (self, settings):
         targetpackage.TargetBuildSpec.__init__ (self, settings)
-        self.with_vc (repository.NewTarBall (self.settings.downloads,
-                                             mirror=download.lilypondorg,
-                                             name=self.name (),
-                                             ball_version='1.0'))
+        self.with_tarball (mirror=download.lilypondorg, version='1.0')
