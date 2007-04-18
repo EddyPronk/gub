775d753d157acfd8f49501c09543c2b012005fea
Jan Nieuwenhuizen <janneke@gnu.org>
2007-04-18 22:31:36 +0200

    Use NewTarBall.

--- specs/hello.py
+++ specs/hello.py
@@ -1,12 +1,11 @@
+import download
 import repository
 import targetpackage
 
 class Hello (targetpackage.TargetBuildSpec):
     def __init__ (self, settings):
         targetpackage.TargetBuildSpec.__init__ (self, settings)
-        self.with_vc (repository.TarBall (self.settings.downloads,
-                                          url='http://lilypond.org/download/gub-sources/hello-1.0.tar.gz',
-                                          version='1.0',
-                                          strip_components=True))
-
-Hello__debian_arm = Hello
+        self.with_vc (repository.NewTarBall (self.settings.downloads,
+                                             mirror=download.lilypondorg,
+                                             name=self.name (),
+                                             ball_version='1.0'))
