07512944fac2edde7f81a2cb1bc0124c438803ab
Jan Nieuwenhuizen <janneke@gnu.org>
2007-04-23 22:10:51 +0200

    Add symlinks for static libraries.

--- specs/boost.py
+++ specs/boost.py
@@ -1,30 +1,34 @@
 import download
+import gub
 import misc
 import targetpackage
+#
+import os
 
 # TODO: AutoToolSpec
 class BjamBuildSpec (targetpackage.TargetBuildSpec):
+    def __init__ (self, settings):
+        targetpackage.TargetBuildSpec.__init__ (self, settings)
+        gub.append_target_dict (self, {'CFLAGS': ''})
+    def get_substitution_dict (self, env={}):
+        # FIXME: how to add settings to dict?
+        dict = targetpackage.TargetBuildSpec.get_substitution_dict (self, env)
+        dict['CFLAGS'] = ''
+        return dict
     def patch (self):
         self.shadow_tree ('%(srcdir)s', '%(builddir)s')
-    def get_substitution_dict (self, env={}):
-        dict = targetpackage.TargetBuildSpec.get_substitution_dict (self, env)
-        # When using GCC, boost ignores standard CC,CXX
-        # settings, but looks at GCC,GXX.
-        dict['GCC'] = dict['CC']
-        dict['GXX'] = dict['CXX']
-        dict['CFLAGS'] = 'boe'
-        return dict
     def configure_command (self):
         return 'true'
     def compile_command (self):
-# FIXME: get_substitution_dict is broken?
-#'-sGCC=%(tool_prefix)s-gcc %(CFLAGS)s'
-#'-sGXX=%(tool_prefix)s-g++ %(CFLAGS)s'
 # FIXME: WTF, where has python_version gone?
-#'-sPYTHON_VERSION=%(python_version)s'
 # only static because dynamic libs fail on linux-64..?
 #<runtime-link>static/dynamic
 # --without-test because is only available as shared lib
+
+# FIXME: how to add settings to dict?
+#'-sGCC=%(tool_prefix)sgcc %(CFLAGS)s
+#'-sGXX=%(tool_prefix)sg++ %(CFLAGS)s
+
         return misc.join_lines ('''
 bjam
 '-sTOOLS=gcc'
@@ -54,18 +58,22 @@ class Boost (BjamBuildSpec):
     def __init__ (self,settings):
         BjamBuildSpec.__init__ (self, settings)
         self.with (version='1.33.1', mirror=download.boost_1_33_1, format='bz2')
+        gub.change_target_dict (self, {'CFLAGS': '-DBOOST_PLATFORM_CONFIG=\\"boost/config/platform/linux.hpp\\"'})
     def get_substitution_dict (self, env={}):
         dict = BjamBuildSpec.get_substitution_dict (self, env)
-        # When using GCC, boost ignores standard CC,CXX
-        # settings, but looks at GCC,GXX.
-        dict['GCC'] = dict['CC']
-        dict['GXX'] = dict['CXX']
-        # WT-Bloody-F?
         dict['CFLAGS'] = '-DBOOST_PLATFORM_CONFIG=\\"boost/config/platform/linux.hpp\\"'
         return dict
     def license_file (self):
         return '%(srcdir)s/LICENSE_1_0.txt'
-
+    def install (self):
+        BjamBuildSpec.install (self)
+        cwd = os.getcwd ()
+        os.chdir (self.expand ('%(install_root)s/usr/lib'))
+        for i in self.locate_files ('%(install_root)s/usr/lib', 'libboost_*-s.a'):
+            f = os.path.basename (i)
+            os.symlink (f, f.replace ('-s.a', '.a'))
+        os.chdir (cwd)
+        
 class Boost__linux_arm_softfloat (BjamBuildSpec):
     def configure_command (self):
         self.system ('''
