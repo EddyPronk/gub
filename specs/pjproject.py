379d9719ec36e034f7c47e6fa29cb494b40b3fcc
Jan Nieuwenhuizen <janneke@gnu.org>
2007-04-23 21:58:54 +0200

    Broken for multiple make jobs, set settings.cpu_count_str=1, as broken_for_distcc still allows -j2?

--- specs/pjproject.py
+++ specs/pjproject.py
@@ -8,21 +8,19 @@ class Pjproject (targetpackage.TargetBuildSpec):
                                           url='http://www.pjsip.org/release/0.5.10.1/pjproject-0.5.10.1.tar.gz',
                                           version='0.5.10.1',
                                           strip_components=True))
-
+        # broken for multiple make jobs make -j2
+        self.settings.cpu_count_str = '1'
+    def broken_for_distcc (self):
+        return True
     def patch (self):
         self.system ('cd %(srcdir)s && patch -p0 < %(patchdir)s/pjproject-install.patch')
-
     def configure_command (self):
 #        return targetpackage.TargetBuildSpec.configure_command (self).replace ('/configure', '/aconfigure')
         return (targetpackage.TargetBuildSpec.configure_command (self).replace ('%(srcdir)s/configure', './aconfigure')
                 + ' --disable-sound')
-
     def configure (self):
         self.shadow_tree ('%(srcdir)s', '%(builddir)s')
         targetpackage.TargetBuildSpec.configure (self)
-
     def install_command (self):
         return (targetpackage.TargetBuildSpec.install_command (self)
                 + ' prefix=/usr')
-
-Pjproject_arm = Pjproject
