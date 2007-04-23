0e16c8ceb3edd458eb47ef065180f11c1f1ba999
Jan Nieuwenhuizen <janneke@gnu.org>
2007-04-23 08:16:44 +0200

    New package.

---
+++ specs/libdbi-drivers-sqlite3.py
@@ -0,0 +1,47 @@
+import download
+import misc
+import repository
+import targetpackage
+
+class Libdbi_drivers_sqlite3 (targetpackage.TargetBuildSpec):
+    def __init__ (self, settings):
+        targetpackage.TargetBuildSpec.__init__ (self, settings)
+        #self.with (version='0.8.1', mirror=download.sf, format='gz')
+        self.with_vc (repository.NewTarBall (self.settings.downloads, download.sf, 'libdbi-drivers', '0.8.1'))
+
+    def get_build_dependencies (self):
+        #return ['sqlite3', 'libdbi', 'libtool']
+        return ['libdbi', 'libtool']
+
+    def configure_command (self):
+        return (targetpackage.TargetBuildSpec.configure_command (self)
+                + misc.join_lines ('''
+--with-dbi-incdir=%(system_root)s/usr/include
+--with-sqlite3
+--with-sqlite3-libdir=%(system_root)s/usr/include
+--with-sqlite3-incdir=%(system_root)s/usr/include
+'''))
+
+    def configure (self):
+        targetpackage.TargetBuildSpec.configure (self)
+        self.update_libtool ()
+
+    def makeflags (self):
+        return ' doc_DATA= html_DATA='
+
+    def compile_command (self):
+        return (targetpackage.TargetBuildSpec.compile_command (self)
+                + self.makeflags ())
+
+    def install_command (self):
+        return (targetpackage.TargetBuildSpec.install_command (self)
+                + self.makeflags ())
+
+    def install (self):
+        targetpackage.TargetBuildSpec.install (self)
+        self.system ('''mv %(install_root)s/usr/lib/dbd/libsqlite3.so %(install_root)s/usr/lib/dbd/libdbdsqlite3.so''')
+
+class Libdbi_drivers_sqlite3__debian__arm (Libdbi_drivers_sqlite3):
+    def get_build_dependencies (self):
+        return ['sqlite3-dev', 'libdbi', 'libtool']
+
