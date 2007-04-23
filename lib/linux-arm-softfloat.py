d0eba3a2e1b535ad267b0c3564835b4f6c55012b
Jan Nieuwenhuizen <janneke@gnu.org>
2007-04-23 09:57:45 +0200

    And glibc_core too.

--- lib/linux-arm-softfloat.py
+++ lib/linux-arm-softfloat.py
@@ -66,6 +66,7 @@ class Glibc_core (glibc.Glibc_core):
         glibc.Glibc_core.patch (self)
         self.system ('''
 cd %(srcdir)s && patch -p1 < %(patchdir)s/glibc-2.3-wordexp-inline.patch
+cd %(srcdir)s && patch -p1 < %(patchdir)s/glibc-2.3-linux-2.4.23-arm-bus-isa.patch
 ''')
     def configure_command (self):
         return (glibc.Glibc_core.configure_command (self)
