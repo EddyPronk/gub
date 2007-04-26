63f47d8d8cdff2b68ed0a44b2f6f30246c329699
Jan Nieuwenhuizen <janneke@gnu.org>
2007-04-26 14:09:50 +0200

    libxml2 without python.

--- specs/libxml2.py
+++ specs/libxml2.py
@@ -5,3 +5,6 @@ class Libxml2 (targetpackage.TargetBuildSpec):
     def __init__ (self, settings):
         targetpackage.TargetBuildSpec.__init__ (self, settings)
         self.with_tarball (mirror=mirrors.gnome_218, version='2.6.27')
+    def configure_command (self):
+        return (targetpackage.TargetBuildSpec.configure_command (self)
+                + ' --without-python')
