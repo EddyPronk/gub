472692e6bb1bfdd4d67c14edb1ba14565fd47063
Jan Nieuwenhuizen <janneke@gnu.org>
2007-05-03 16:12:10 +0200

    Also for gcc-core.

--- gub/specs/linux-arm-softfloat/cross/gcc-core.py
+++ gub/specs/linux-arm-softfloat/cross/gcc-core.py
@@ -1,7 +1,11 @@
+from gub import mirrors
 from gub import misc
 gcc = misc.load_spec ('cross/gcc-core')
 
 class Gcc_core (gcc.Gcc_core):
+    def __init__ (self, settings):
+        gcc.Gcc_core.__init__ (self, settings)
+        self.with_tarball (mirror=mirrors.gnu, version='3.4.6', format='bz2')
     def patch (self):
         gcc.Gcc_core.patch (self)
         self.system ('''
