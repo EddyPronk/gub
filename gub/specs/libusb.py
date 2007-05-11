efd40f75c72d03fdd9270827bd6bc3fe8980d933
Jan Nieuwenhuizen <janneke@gnu.org>
2007-05-11 17:14:49 +0200

    Libtool update for libusb.

--- gub/specs/libusb.py
+++ gub/specs/libusb.py
@@ -5,3 +5,7 @@ class Libusb (targetpackage.TargetBuildSpec):
     def __init__ (self, settings):
         targetpackage.TargetBuildSpec.__init__ (self, settings)
         self.with (version="0.1.12", mirror=mirrors.sf)
+    def configure (self):
+        targetpackage.TargetBuildSpec.configure (self)
+        # # FIXME: libtool too old for cross compile
+        self.update_libtool ()
