792a9f92872800a69723672936577c2698b63fc4
Jan Nieuwenhuizen <janneke@gnu.org>
2007-05-11 17:44:43 +0200

    Libgphoto2 build fix.

--- gub/specs/libgphoto2.py
+++ gub/specs/libgphoto2.py
@@ -6,6 +6,22 @@ class Libgphoto2 (targetpackage.TargetBuildSpec):
     def __init__ (self, settings):
         targetpackage.TargetBuildSpec.__init__ (self, settings)
         self.with (version='2.1.6', mirror=sf_gphoto)
+    def wrap_pkg_config (self):
+        self.dump ('''#! /bin/sh
+/usr/bin/pkg-config\
+  --define-variable prefix=%(system_root)s/usr\
+  --define-variable includedir=%(system_root)s/usr/include\
+  --define-variable libdir=%(system_root)s/usr/lib\
+"$@"
+''',
+                   '%(srcdir)s/pkg-config')
+        import os
+        os.chmod (self.expand ('%(srcdir)s/pkg-config'), 0755)
+    def patch (self):
+        self.wrap_pkg_config ()
+    def configure_command (self):
+        return ('PATH=%(srcdir)s:$PATH '
+                + targetpackage.TargetBuildSpec.configure_command (self))
     def get_build_dependencies (self):
         return ['libexif', 'libjpeg', 'libusb']
     
