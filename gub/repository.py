176341c717b337d6f20fe38b1d60f3f35f80dee1
Han-Wen Nienhuys <hanwen@lilypond.org>
2007-05-26 16:51:02 -0300

    reset workdir before pulling

--- gub/repository.py
+++ gub/repository.py
@@ -432,6 +432,7 @@ class Git (Repository):
         revision = self.revision
         
         if os.path.isdir (os.path.join (destdir, self.vcs)):
+            self.git ('reset --hard HEAD' % locals (), dir=destdir)
             self.git ('pull %(repo_dir)s %(branch)s' % locals (), dir=destdir)
         else:
             self.system ('git-clone -l -s %(repo_dir)s %(destdir)s' % locals ())
