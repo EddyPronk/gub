24a31736b9090eff6cf7e8bb661b6b139714b6f6
Han-Wen Nienhuys <hanwen@lilypond.org>
2007-05-27 14:51:47 -0300

    tcl fixes:
    - broken_for_distcc()
    - tclConfig install

--- gub/specs/tcl.py
+++ gub/specs/tcl.py
@@ -7,12 +7,19 @@ class Tcl (targetpackage.TargetBuildSpec):
     def __init__ (self, settings):
         targetpackage.TargetBuildSpec.__init__ (self, settings)
         self.with_tarball (mirror='http://prdownloads.sourceforge.net/tcl/tcl8.4.14-src.tar.gz', version='8.4.14')
+
     def license_file (self):
         return "%(srcdir)s/license.terms"
+    def broken_for_distcc (self):
+        return True
     def configure_command(self):
         return "%(srcdir)s/unix/configure --prefix=%(install_prefix)s "
+    
     def patch (self):
         self.system ("cd %(srcdir)s && patch -p1 < %(patchdir)s/tcl-8.4.14-dde.patch")
+    def install(self):
+        targetpackage.TargetBuildSpec.install (self)
+        self.system ('mv %(install_root)s/usr/lib/tclConfig.sh  %(install_root)s/usr/bin')
 
 class Tcl__mingw (Tcl):
     @context.subst_method
