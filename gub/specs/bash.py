5a288f707a143e35a37c23c7951d2e1e091ed68b
Han-Wen Nienhuys <hanwen@lilypond.org>
2007-05-27 17:41:39 -0300

    initial attempt at msys bash.

--- gub/specs/bash.py
+++ gub/specs/bash.py
@@ -9,3 +9,21 @@ class Bash (targetpackage.TargetBuildSpec):
 
     def get_build_dependencies (self):
         return ['libtool', 'gettext-devel']
+
+
+class Bash__mingw (Bash):
+    def __init__ (self, settings):
+        Bash.__init__ (self, settings)
+        self.with_template (version='2.05b-MSYS',
+                            mirror='http://ufpr.dl.sourceforge.net/sourceforge/mingw/bash-2.05b-MSYS-src.tar.bz2',
+                            format='bz2', strip_components=2)
+
+    def patch (self):
+        self.file_sub ([(r'test \$ac_cv_sys_tiocgwinsz_in_termios_h != yes',
+                         r'test "$ac_cv_sys_tiocgwinsz_in_termios_h" != yes'),
+                        ], '%(srcdir)s/configure')
+    
+    def config_cache_overrides (self, str):
+        str += 'bash_cv_have_mbstate_t=yes\n'
+        return str
+ 
