cc088c4599893377f5a229c63fcacb20e63dc24f
Jan Nieuwenhuizen <janneke@gnu.org>
2007-05-29 16:00:32 +0200

    Update libexif in order to build libgphoto2.

--- gub/specs/libexif.py
+++ gub/specs/libexif.py
@@ -8,4 +8,6 @@ class Libexif (targetpackage.TargetBuildSpec):
 #        self.with_template (version="0.6.9", mirror=mirrors.sf)
 # does not install
 #        self.with_template (version="0.6.14", mirror=mirrors.sf)
-        self.with_template (version="0.6.12", mirror=mirrors.sf)
+# too old for libgphoto-2.3.1
+#        self.with_template (version="0.6.12", mirror=mirrors.sf)
+        self.with_template (version="0.6.15", mirror=mirrors.sf)
