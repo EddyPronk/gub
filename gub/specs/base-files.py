c6e8b0badba8eae518c8d6076fb8ca8d94804751
Jan Nieuwenhuizen <janneke@gnu.org>
2007-06-01 16:00:01 +0200

    New package.

---
+++ gub/specs/base-files.py
@@ -0,0 +1,32 @@
+from gub import gubb
+
+url='ftp://ftp.nl.debian.org/debian/pool/main/b/base-files/base-files_4.0.0.tar.gz'
+
+class Base_files (gubb.BinarySpec):
+    def __init__ (self, settings):
+        gubb.BinarySpec.__init__ (self, settings)
+        from gub import repository
+        # FIXME: cannot parse debian balls
+        self.ball_version = '4.0.0'
+        self.with_tarball (mirror=url, version='')
+    def get_subpackage_names (self):
+        return ['']
+    def patch (self):
+        self.system ('''
+cd %(srcdir)s && mkdir -p `cat debian/directory-list`
+cd %(srcdir)s && mkdir -p usr/share/base-files
+cd %(srcdir)s && mv share usr/share/base-files
+cd %(srcdir)s && mv licenses usr/share/common-licenses
+# from debian/rules
+cd %(srcdir)s && fakeroot chown root:src     usr/src
+cd %(srcdir)s && fakeroot chown root:staff   var/local
+cd %(srcdir)s && chmod 755  `find . -type d`
+cd %(srcdir)s && chmod 1777 `cat debian/1777-dirs`
+cd %(srcdir)s && chmod 2775 `cat debian/2775-dirs`
+cd %(srcdir)s && rm -rf debian
+''')
+    def xinstall (self):
+        self.system ('''
+cd %(install-root)s && fakeroot chown root:src     usr/src
+cd %(install_root)s && fakeroot chown root:staff   var/local
+''')
