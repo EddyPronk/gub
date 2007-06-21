7029e31155e6aca509632e1a0ffff6b3cd9c55ef
Jan Nieuwenhuizen <janneke@gnu.org>
2007-06-21 14:46:56 +0200

    Bump version for cygwin.

--- gub/specs/freetype-config.py
+++ gub/specs/freetype-config.py
@@ -29,3 +29,9 @@ class Freetype_config (gubb.SdkBuildSpec):
         file = self.expand ('%(install_prefix)s/cross/bin/freetype-config')
         self.dump (s, file)
         os.chmod (file, 0755)
+
+class Freetype_config__cygwin (Freetype_config):
+    def __init__ (self, settings):
+        Freetype_config.__init__ (self, settings)
+        self.with_template (version='2.3.4')
+        
