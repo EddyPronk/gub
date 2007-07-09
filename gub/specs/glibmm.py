690f5bb8ece2013402d5344ad9bc995e9059bf95
Jan Nieuwenhuizen <janneke@gnu.org>
2007-07-09 11:49:00 +0200

    New package.

---
+++ gub/specs/glibmm.py
@@ -0,0 +1,11 @@
+from gub import repository
+from gub import targetpackage
+
+url='http://ftp.gnome.org/pub/GNOME/sources/glibmm/2.12/glibmm-2.12.10.tar.gz'
+
+class Glibmm (targetpackage.TargetBuildSpec):
+    def __init__ (self, settings):
+        targetpackage.TargetBuildSpec.__init__ (self, settings)
+        self.with_vc (repository.TarBall (self.settings.downloads, url))
+    def get_build_dependencies (self):
+        return ['glib', 'libsig++']
