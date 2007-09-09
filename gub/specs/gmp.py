eab660d316ae3749dbf971593a6f6dbc75f3a0d7
Jan Nieuwenhuizen <janneke@gnu.org>
2007-09-09 12:26:04 +0200

    Bugfix for new gmp libtool script: also subst bash.  Fixes gmp build.

--- gub/specs/gmp.py
+++ gub/specs/gmp.py
@@ -33,7 +33,8 @@ class Gmp (targetpackage.TargetBuildSpec):
         # automake's Makefile.in's too old for new libtool,
         # but autoupdating breaks even more.  This nice
         # hack seems to work.
-        self.file_sub ([('#! /bin/sh', '#! /bin/sh\ntagname=CXX')],
+        self.file_sub ([('#! /bin/sh', '#! /bin/sh\ntagname=CXX'),
+                        ('#! /bin/bash', '#! /bin/bash\ntagname=CXX')],
                '%(builddir)s/libtool')
         
 class Gmp__darwin (Gmp):
