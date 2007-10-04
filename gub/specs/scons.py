47f508458780ccaf40fc7371788b581417b6db2e
Jan Nieuwenhuizen <janneke@gnu.org>
2007-10-04 20:49:35 +0200

    Removing comma fixes source to be a string iso tuple.
    Fixes scons package.

--- gub/specs/scons.py
+++ gub/specs/scons.py
@@ -4,19 +4,16 @@ from gub import mirrors
 class Scons (toolsbuild.ToolsBuild):
     source = mirrors.with_template (name='scons', version='0.96.93',
                    format='gz',
-                   mirror=mirrors.sf),
-
-    def compile (self):
-        pass
-
+                   mirror=mirrors.sf)
+    def stages (self):
+        return misc.list_remove (toolsbuild.ToolsBuild.stages (self),
+                       ['compile'])
     def patch (self):
+        # FIXME: no autotools
         pass
-    
     def configure (self):
         self.system ('mkdir %(builddir)s')
-    
     def install_command (self):
         return 'python %(srcdir)s/setup.py install --prefix=%(tools_prefix)s --root=%(install_root)s'
-
     def license_file (self):
         return '%(srcdir)s/LICENSE.txt' 
