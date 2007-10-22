12c53204ad78eb10540be4f54ac92c5ff25e9da5
Han-Wen Nienhuys <hanwen@lilypond.org>
2007-10-22 01:35:27 -0200

    Indentation fixes.

--- gub/specs/texinfo.py
+++ gub/specs/texinfo.py
@@ -3,5 +3,5 @@ from gub import toolsbuild
 
 class Texinfo (toolsbuild.ToolsBuild):
     source = mirrors.with_template (name='texinfo', version="4.8",
-                   mirror=mirrors.gnu, format="bz2")
+                                    mirror=mirrors.gnu, format="bz2")
     patches = 'texinfo-4.8.patch'
--- gub/specs/urw-fonts.py
+++ gub/specs/urw-fonts.py
@@ -3,7 +3,7 @@ from gub import mirrors
 
 class Urw_fonts (build.BinaryBuild):
     source = mirrors.with_template (name='urw-fonts', version='1.0.7pre41',
-                   mirror='ftp://ftp.gnome.ru/fonts/urw/release/urw-fonts-%(version)s.tar.bz2&strip_components=0')
+                                    mirror='ftp://ftp.gnome.ru/fonts/urw/release/urw-fonts-%(version)s.tar.bz2&strip_components=0')
     def __init__ (self, settings, source):
         build.BinaryBuild.__init__ (self, settings, source)
         # FIXME: TODO: find nice way to pass strip_components
