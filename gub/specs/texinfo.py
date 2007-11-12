0f6345cf5a28d9fe8e2c8105535536b73f5a7b2c
Jan Nieuwenhuizen <janneke@gnu.org>
2007-11-12 09:54:00 +0100

    Disable patch for texinfo 4.8.

--- gub/specs/texinfo.py
+++ gub/specs/texinfo.py
@@ -6,7 +6,7 @@ class Texinfo(toolpackage.ToolBuildSpec):
         toolpackage.ToolBuildSpec.__init__ (self, settings)
         self.with_template (version="4.11",
                    mirror=mirrors.gnu, format="bz2")
-    def patch (self):
+    def patch_for_48_when_are_we_going_to_versionize_patches (self):
         toolpackage.ToolBuildSpec.patch (self)
         self.system ('cd %(srcdir)s && patch -p1 <  %(patchdir)s/texinfo-4.8.patch')
 
