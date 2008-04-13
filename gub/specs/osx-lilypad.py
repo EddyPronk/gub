43d1b944b74d6bf22a6b7b58f750b8ed9a85efaf
Han-Wen Nienhuys <hanwen@lilypond.org>
2008-04-13 19:37:45 -0300

    Use Nicolas' universal binary for Mac OS lilypad.

--- gub/specs/osx-lilypad.py
+++ gub/specs/osx-lilypad.py
@@ -7,11 +7,13 @@ class Osx_lilypad (build.NullBuild):
 class Osx_lilypad__darwin__ppc (build.NullBuild):
     def __init__ (self, settings, source):
         build.NullBuild.__init__ (self, settings, source)
-    source = mirrors.with_template (name='osx-lilypad', version="0.2",
-                   mirror='http://lilypond.org/download/gub-sources/osx-lilypad-ppc-0.2.tar.gz')
+    source = mirrors.with_template (
+        name='osx-lilypad', version="0.2",
+        mirror='http://lilypond.org/download/gub-sources/osx-lilypad-universal-0.3.tar.gz')
 
 class Osx_lilypad__darwin__x86 (build.NullBuild):
     def __init__ (self, settings, source):
         build.NullBuild.__init__ (self, settings, source)
-    source = mirrors.with_template (name='osx-lilypad', version="0.2",
-                   mirror='http://lilypond.org/download/gub-sources/osx-lilypad-x86-0.2.tar.gz')
+    source = mirrors.with_template (
+        name='osx-lilypad', version="0.2",
+        mirror='http://lilypond.org/download/gub-sources/osx-lilypad-universal-0.3.tar.gz')
