ae7b6510e9f0bbce11bbe8a353229e72e2e8343d
Han-Wen Nienhuys <hanwen@lilypond.org>
2008-09-08 10:40:23 -0300

    Add texi2html spec.

---
+++ gub/specs/texi2html.py
@@ -0,0 +1,6 @@
+from gub import toolsbuild
+from gub import misc
+from gub import mirrors
+
+class Texi2html (toolsbuild.ToolsBuild):
+    source = 'http://lilypond.org/vc/texi2html.git&branch=master'
