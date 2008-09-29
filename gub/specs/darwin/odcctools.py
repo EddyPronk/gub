312e08ee0b541f9bcf456e9b9a7af38d8f204461
Han-Wen Nienhuys <hanwen@lilypond.org>
2008-09-29 01:24:17 -0300

    odcctools from IPhone-dev, r278.
    Built an installer successfully.

--- gub/specs/darwin/odcctools.py
+++ gub/specs/darwin/odcctools.py
@@ -3,18 +3,18 @@ import os
 from gub import cross
 from gub import mirrors
 
-# http://dev.gentoo.org/~grobian/distfiles/odcctools-20071104.tar.bz2
 class Odcctools (cross.CrossToolsBuild):
-    source = mirrors.with_template (name='odcctools', version='20060413',
-                   ####version='20060608',
-                   #version='20061117',
-                   mirror=mirrors.opendarwin,
-                   format='bz2')
+    source = ('svn:http://iphone-dev.googlecode.com/svn/trunk/odcctools'
+              '&revision=278'
     def __init__ (self, settings, source):
         cross.CrossToolsBuild.__init__ (self, settings, source)
         if 'x86_64-linux' in self.settings.build_architecture:
             # odcctools does not build with 64 bit compiler
             cross.setup_linux_x86 (self)
+
+    def patch(self):
+        cross.CrossToolsBuild.patch(self)
+        self.apply_patch('odcctools-r211-word.patch')
     def get_build_dependencies (self):
         return ['darwin-sdk']
     def configure (self):
