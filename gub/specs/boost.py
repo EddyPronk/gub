5063b5df76621f7ebf0ee304a36310229d75535a
Jan Nieuwenhuizen <janneke@gnu.org>
2008-10-15 20:50:54 +0200

    Update boost.py to new serialization framework, add Boost__mingw fixlet.

--- gub/specs/boost.py
+++ gub/specs/boost.py
@@ -1,5 +1,4 @@
 from gub import mirrors
-from gub import build
 from gub import misc
 from gub import targetbuild
 #
@@ -9,7 +8,7 @@ import os
 class BjamBuild (targetbuild.TargetBuild):
     def __init__ (self, settings, source):
         targetbuild.TargetBuild.__init__ (self, settings, source)
-        build.append_target_dict (self, {'CFLAGS': ''})
+        targetbuild.append_target_dict (self, {'CFLAGS': ''})
     def get_substitution_dict (self, env={}):
         # FIXME: how to add settings to dict?
         dict = targetbuild.TargetBuild.get_substitution_dict (self, env)
@@ -58,7 +57,7 @@ class Boost (BjamBuild):
     source = mirrors.with_template (name='boost', version='1.33.1', mirror=mirrors.boost_1_33_1, format='bz2')
     def __init__ (self, settings, source):
         BjamBuild.__init__ (self, settings, source)
-        build.change_target_dict (self, {'CFLAGS': '-DBOOST_PLATFORM_CONFIG=\\"boost/config/platform/linux.hpp\\"'})
+        targetbuild.change_target_dict (self, {'CFLAGS': '-DBOOST_PLATFORM_CONFIG=\\"boost/config/platform/linux.hpp\\"'})
     def get_substitution_dict (self, env={}):
         dict = BjamBuild.get_substitution_dict (self, env)
         dict['CFLAGS'] = '-DBOOST_PLATFORM_CONFIG=\\"boost/config/platform/linux.hpp\\"'
@@ -68,24 +67,27 @@ class Boost (BjamBuild):
     def install (self):
         BjamBuild.install (self)
         # Bjam `installs' header files by using symlinks to the source dir?
-        for i in self.locate_files ('%(install_prefix)s/include/boost',
-                                    '*'):
-            if os.path.islink (i):
-                s = os.readlink (i)
-                self.system ('''
-rm %(i)s
-cp %(s)s %(i)s
-''',
-                             locals ())
-        cwd = os.getcwd ()
-        os.chdir (self.expand ('%(install_prefix)s/lib'))
-        for i in self.locate_files ('%(install_prefix)s/lib', 'libboost_*-s.a'):
-            f = os.path.basename (i)
-            os.symlink (f, f.replace ('-s.a', '.a'))
-        os.chdir (cwd)
+
+        def replace_links (logger, file):
+            if os.path.islink (file):
+                link = os.readlink (file)
+                loggedos.system (logger, '''
+rm %(file)s
+cp %(link)s %(file)s
+''' % locals ())
+        self.map_locate (replace_links, '%(install_prefix)s/include/boost', '*')
+
+        def add_plain_lib_names (logger, file):
+            os.symlink (file, file.replace ('-s.a', '.a'))
+        self.map_locate (add_plain_lib_names, '%(install_prefix)s/lib', 'libboost_*-s.a')
         
 class Boost__linux_arm_softfloat (BjamBuild):
     def configure_command (self):
         self.system ('''
 cp -f boost/config/platform/linux.hpp boost/config/platform/linux-gnueabi.hpp
 ''')
+
+class Boost__mingw (Boost):
+    def compile_command (self):
+        return (Boost.configure_command (self)
+                .replace ('linux.hpp', 'win32.hpp'))
