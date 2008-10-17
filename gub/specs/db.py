9a644d28bf6cdc5cdf0cb465a70a5a9e77c74558
Jan Nieuwenhuizen <janneke@gnu.org>
2008-10-17 20:38:23 +0200

    Install also Berkeley db libs without version.

--- gub/specs/db.py
+++ gub/specs/db.py
@@ -8,6 +8,22 @@ class Db (targetbuild.TargetBuild):
         return '../dist/configure'
     def builddir (self):
         return targetbuild.TargetBuild.builddir (self) + '/build_unix'
+    def clean (self):
+        targetbuild.TargetBuild.clean (self)
+        if self.source.is_tracking ():
+            # URG
+            return
+        self.system ('rm -rf ' + targetbuild.TargetBuild.builddir (self))
+    def configure (self):
+        targetbuild.TargetBuild.configure (self)
+        self.system ('sed -i -e "s@/(prefix)docs@/(prefix)/share/doc/gb@" %(builddir)s/Makefile')
+        self.system ('sed -i -e "s/^	@/	/" %(builddir)s/Makefile')
+    def install (self):
+        targetbuild.TargetBuild.install (self)
+        self.system ('rm -f %(install_prefix)s/lib/libdb.{a,so{,.a},la}')
+        self.system ('cd %(install_prefix)s/lib && ln -s libdb-*.a libdb.a')
+        self.system ('cd %(install_prefix)s/lib && ln -s libdb-*.la libdb.la')
+        self.system ('cd %(install_prefix)s/lib && ln -s libdb-*.so libdb.so')
 
 class Db__mingw (Db):
     def patch (self):
@@ -19,13 +35,20 @@ touch %(builddir)s/netinet/in.h
 touch %(builddir)s/netdb.h
 touch %(builddir)s/arpa/inet.h
 ''')
-        self.system ('sed -ie s@HAVE_VXWORKS@__MINGW32__@ %(builddir)s/../os/os_mkdir.c')
-        self.system ('sed -ie s@dbenv@env@ %(builddir)s/../os/os_yield.c')
+        self.system ('sed -i -e s@HAVE_VXWORKS@__MINGW32__@ %(builddir)s/../os/os_mkdir.c')
+        self.system ('sed -i -e s@dbenv@env@ %(builddir)s/../os/os_yield.c')
     def configure (self):
         Db.configure (self)
         self.system ('echo "#undef fsync" >> %(builddir)s/db_config.h')
         self.system ('echo "#define fsync _commit" >> %(builddir)s/db_config.h')
-        self.system ('sed -ie "s@[.]exe@@g" %(builddir)s/Makefile')
+        self.system ('sed -i -e "s@[.]exe@@g" %(builddir)s/Makefile')
     def configure_command (self):
         return (Db.configure_command (self)
                 + ' LDFLAGS=-lwsock32')
+    def install (self):
+        targetbuild.TargetBuild.install (self)
+        self.system ('rm -f %(install_prefix)s/{bin,lib}/libdb.{{,so,dll}{,.a},la}')
+        self.system ('cd %(install_prefix)s/bin && cp libdb-*.dll libdb.dll')
+        self.system ('cd %(install_prefix)s/lib && cp libdb-*.a libdb.a')
+        self.system ('cd %(install_prefix)s/lib && cp libdb-*.dll.a libdb.dll.a')
+        self.system ('cd %(install_prefix)s/lib && cp libdb-*.la libdb.la')
