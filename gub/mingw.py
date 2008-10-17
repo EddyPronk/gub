22c4b65aee7e2821ef57b271ef65fb37b0dd50fc
Jan Nieuwenhuizen <janneke@gnu.org>
2008-10-17 20:42:25 +0200

    Preliminary mingw post-install exe smurfing.

--- gub/mingw.py
+++ gub/mingw.py
@@ -1,3 +1,5 @@
+from gub import misc
+
 def get_cross_packages (settings):
     # obsolete
     return []
@@ -5,13 +7,18 @@ def get_cross_packages (settings):
 def get_cross_build_dependencies (settings):
     return ['cross/gcc']
     
-def change_target_package (p):
+def change_target_package (package):
     from gub import cross
     from gub import build
     from gub import targetbuild
-    cross.change_target_package (p)
-    targetbuild.change_target_dict (p,
+    cross.change_target_package (package)
+    targetbuild.change_target_dict (package,
                     {
             'DLLTOOL': '%(toolchain_prefix)sdlltool',
             'DLLWRAP': '%(toolchain_prefix)sdllwrap',
             })
+    return
+    def install (whatsthis):
+        package.post_install_smurf_exe ()
+
+    package.install = misc.MethodOverrider (package.install, install)
