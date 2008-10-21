52b389e76c34cd23ec0606a762c794ed0797c736
Jan Nieuwenhuizen <janneke@gnu.org>
2008-10-21 17:12:40 +0200

    openoffice.py: Add cross tool checks and usage, several cross and mingw fixes.
    This gets openoffice mingw cross build past xml2cmp.

--- gub/specs/openoffice.py
+++ gub/specs/openoffice.py
@@ -1,12 +1,24 @@
+import os
+#
 from gub import context
 from gub import misc
 from gub import targetbuild
+
+'''
+7 matches for "delivered successfully" in buffer: *shell*
+   4642:Module 'solenv' delivered successfully. 0 files copied, 1 files unchanged
+   4655:Module 'stlport' delivered successfully. 0 files copied, 8 files unchanged
+   4995:Module 'soltools' delivered successfully. 7 files copied, 7 files unchanged
+   5069:Module 'external' delivered successfully. 3 files copied, 27 files unchanged
+   6288:Module 'libwpd' delivered successfully. 12 files copied, 0 files unchanged
+   6397:Module 'xml2cmp' delivered successfully. 3 files copied, 2 files unchanged
+'''
 
 class Openoffice (targetbuild.TargetBuild):
 #    source = 'svn://gsvn.gnome.org/svn/ooo-build&branch=trunk&revision=14327'
     source = 'svn://svn.gnome.org/svn/ooo-build&branch=trunk'
     patches = ['openoffice-srcdir-build.patch']
-    upstream_patches = ['openoffice-config_office-cross.patch', 'openoffice-config_office-gnu-make.patch', 'openoffice-config_office-mingw.patch']
+    upstream_patches = ['openoffice-config_office-cross.patch', 'openoffice-config_office-gnu-make.patch', 'openoffice-config_office-mingw.patch', 'openoffice-solenv-cross.patch', 'openoffice-solenv.patch', 'openoffice-sal-cross.patch', 'openoffice-soltools-cross.patch', 'openoffice-soltools-mingw.patch']
 #    upstream_patches = ['openoffice-config_office-cross.patch', 'openoffice-config_office-mingw.patch']
     def get_build_dependencies (self):
         return ['boost-devel', 'curl', 'db-devel', 'expat', 'fontconfig-devel', 'libjpeg-devel', 'libpng-devel', 'python', 'saxon-java', 'xerces-c', 'zlib-devel']
@@ -22,6 +34,15 @@ class Openoffice (targetbuild.TargetBuild):
     @context.subst_method
     def cvs_tag (self):
         return 'ooo300-m9'
+    @context.subst_method
+    def upstream_dir (self):
+        return '%(builddir)s/build/%(cvs_tag)s'
+    @context.subst_method
+    def OOO_TOOLS_DIR (self):
+        # TODO: either make all ooo-tools (soltools: makedepend..., transex3: transex3 ...)
+        # self-hosting or compile them as Openoffice__tools package...
+        # Shortcut: use precompiled tools from user's system
+        return os.environ['OOO_TOOLS_DIR']
     def autoupdate (self):
         # Why is build.py:Build:patch() not doing this?
         map (self.apply_patch, self.__class__.patches)
@@ -114,7 +135,15 @@ ac_cv_icu_version_minor=${ac_cv_icu_version_minor=3.81}
 
 --cache-file=%(builddir)s/config.cache
 
+--with-tools-dir=%(OOO_TOOLS_DIR)s
+
 '''))
+
+# TODO:
+# --with-system-libwpd
+# --with-system-libwps
+# --with-system-libwpg
+
     def make_unpack (self):
         # FIXME: python detection is utterly broken, should use python-config
         self.system ('cd %(builddir)s && make unpack')
@@ -125,18 +154,61 @@ ac_cv_icu_version_minor=${ac_cv_icu_version_minor=3.81}
 cd %(builddir)s/build/%(cvs_tag)s && patch -p%(patch_strip_component)s < %(patchdir)s/%(name)s
 ''', locals ())
     def patch_upstream (self):
-        self.system ('cp -p %(builddir)s/build/%(cvs_tag)s/config_office/acinclude.m4.pristine %(builddir)s/build/%(cvs_tag)s/config_office/acinclude.m4 || cp -p %(builddir)s/build/%(cvs_tag)s/config_office/acinclude.m4 %(builddir)s/build/%(cvs_tag)s/config_office/acinclude.m4.pristine ')
-        self.system ('cp -p %(builddir)s/build/%(cvs_tag)s/config_office/configure.in.pristine %(builddir)s/build/%(cvs_tag)s/config_office/configure.in || cp -p %(builddir)s/build/%(cvs_tag)s/config_office/configure.in %(builddir)s/build/%(cvs_tag)s/config_office/configure.in.pristine')
+        base = '%(builddir)s/build/%(cvs_tag)s'
+        upstream_dir = self.upstream_dir ()
+        for file in (
+            'config_office/acinclude.m4',
+            'config_office/configure.in',
+            'config_office/bootstrap.1',
+            'config_office/set_soenv.in',
+            'solenv/inc/startup/startup.mk',
+            'solenv/inc/wntgcci6.mk',
+            'solenv/inc/unitools.mk',
+            'solenv/bin/build.pl',
+            'solenv/bin/deliver.pl',
+            'soltools/util/makefile.pmk',
+            'soltools/mkdepend/collectdircontent.cxx',
+            'sal/rtl/source/makefile.mk',
+            ):
+            self.system ('cp -p %(upstream_dir)s/%(file)s.pristine %(upstream_dir)s/%(file)s || cp -p %(upstream_dir)s/%(file)s %(upstream_dir)s/%(file)s.pristine' % locals ())
         map (self.apply_upstream_patch, self.__class__.upstream_patches)
+
         # FIXME: neutralize silly GNU make check
-        # self.system ('''sed -i -e "s@' 3[.]81'@'gpuhleez, we are not even building mozilla'@" %(builddir)s/build/%(cvs_tag)s/config_office/configure.in')
+        # self.system ('''sed -i -e "s@' 3[.]81'@'gpuhleez, we are not even building mozilla'@" %(upstream_dir)s/config_office/configure.in')
+
         # configure blindly adds /usr includes, even when not necessary
-        self.system ('sed -i -e "s@=/usr/include@=%(system_prefix)s/include@" %(builddir)s/build/%(cvs_tag)s/config_office/configure.in')
+        self.system ('sed -i -e "s@=/usr/include@=%(system_prefix)s/include@" %(upstream_dir)s/config_office/configure.in')
+
         # configure.in uses AC_CHECK_FILE, which simply assert-fails
         # when cross compiling slated for removal in ~2000
         # http://www.mail-archive.com/autoconf@gnu.org/msg02857.html
-        self.system ('sed -i -e "s@AC_CHECK_FILE(@AC_CHECK_FILE_CROSS(@" %(builddir)s/build/%(cvs_tag)s/config_office/configure.in')
+        self.system ('sed -i -e "s@AC_CHECK_FILE(@AC_CHECK_FILE_CROSS(@" %(upstream_dir)s/config_office/configure.in')
 
+        # avoid juggling of names for windows-nt
+        self.system ('sed -i -e "s@WINNT@WNT@" %(upstream_dir)s/config_office/configure.in')
+
+        # TODO: ASM is handled in individual solenv/inc/*mk
+        self.system (misc.join_lines ('''sed -i.guborig
+-e 's@\<ar\>@$(AR)@g'
+-e 's@\<dlltool\>@$(DLLTOOL)@g'
+-e 's@\<ld\>\([^-]\|$\)@$(LD)\\1@g'
+-e 's@\<nm\>@$(NM)@g'
+-e 's@\<ranlib\>@$(RANLIB)@g'
+%(upstream_dir)s/solenv/inc/*mk'''))
+
+        self.system ('chmod +x %(upstream_dir)s/solenv/bin/build.pl %(upstream_dir)s/solenv/bin/deliver.pl')
+    def makeflags (self):
+        return misc.join_lines ('''
+CC_FOR_BUILD=cc
+CXX_FOR_BUILD=c++
+LDFLAGS_FOR_BUILD=
+C_INCLUDE_PATH=
+LIBRARY_PATH=
+EXECPOST=
+''')
+##main configure barfs
+##CPPFLAGS=
+                
 class Openoffice__mingw (Openoffice):
     def configure_command (self):
         return (Openoffice.configure_command (self)
