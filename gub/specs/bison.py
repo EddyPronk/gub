fd3f31415049c688c433ff343fe0a2cbf4f49340
Jan Nieuwenhuizen <janneke@gnu.org>
2008-10-23 21:05:49 +0200

    Bison url thinko.

--- gub/specs/bison.py
+++ gub/specs/bison.py
@@ -1,4 +1,4 @@
 from gub import toolsbuild
 
 class Bison__tools (toolsbuild.ToolsBuild):
-    source = 'ftp://ftp.gnu.org/ftp/gnu/bison/bison-2.3.tar.gz'
+    source = 'ftp://ftp.gnu.org/pub/gnu/bison/bison-2.3.tar.gz'
