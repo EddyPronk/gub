767512d7f298e46dc00caad6fe969f533cd436f0
Jan Nieuwenhuizen <janneke@gnu.org>
2008-10-24 17:02:07 +0200

    mingw::gettext-0.17 now barfs on sed command; revert to 0.15.  Add patch.

--- gub/specs/gettext.py
+++ gub/specs/gettext.py
@@ -3,10 +3,8 @@ from gub import targetbuild
 from gub import toolsbuild
 
 class Gettext (targetbuild.TargetBuild):
-    source = mirrors.with_template (
-        # 0.16.1 makes gcc barf on ICE.
-        # name='gettext', version='0.16.1', mirror=mirrors.gnu, format='gz')
-        name='gettext', version='0.15', mirror=mirrors.gnu, format='gz')
+    # 0.16.1 makes gcc barf on ICE.
+    source = 'ftp://ftp.gnu.org/pub/gnu/gettext/gettext-0.15.tar.gz'
 
     def get_build_dependencies (self):
         return ['libtool']
@@ -37,11 +35,27 @@ class Gettext__freebsd (Gettext):
             return ['libgnugetopt'] + Gettext.get_build_dependencies (self)
         return Gettext.get_build_dependencies (self)
 
-class Gettext__mingw (Gettext):
-    def __init__ (self, settings, source):
-        Gettext.__init__ (self, settings, source)
+'''
+mingw: 0.17
+	sed_extract_major='/^[0-9]/{'${nl}'s/^\([0-9]*\).*/\1/p'${nl}q${nl}'}'${nl}'c\'${nl}0${nl}q; \
+	sed_extract_minor='/^[0-9][0-9]*[.][0-9]/{'${nl}'s/^[0-9]*[.]\([0-9]*\).*/\1/p'${nl}q${nl}'}'${nl}'c\'${nl}0${nl}q; \
+	sed_extract_subminor='/^[0-9][0-9]*[.][0-9][0-9]*[.][0-9]/{'${nl}'s/^[0-9]*[.][0-9]*[.]\([0-9]*\).*/\1/p'${nl}q${nl}'}'${nl}'c\'${nl}0${nl}q; \
+	i686-mingw32-windres \
+	  "-DPACKAGE_VERSION_STRING=\\\"0.17\\\"" \
+	  "-DPACKAGE_VERSION_MAJOR="`echo '0.17' | sed -n -e "$sed_extract_major"` \
+	  "-DPACKAGE_VERSION_MINOR="`echo '0.17' | sed -n -e "$sed_extract_minor"` \
+	  "-DPACKAGE_VERSION_SUBMINOR="`echo '0.17' | sed -n -e "$sed_extract_subminor"` \
+	  -i /home/janneke/vc/gub/target/mingw/src/gettext-0.17/gettext-runtime/intl/libintl.rc -o libintl.res --output-format=coff
+sed: expressie #1, teken 11: onbekende opdracht: '\'
+sed: expressie #1, teken 25: onbekende opdracht: '\'
+sed: expressie #1, teken 39: onbekende opdracht: '\'
+'''
 
-    source = mirrors.with_template (name='gettext', version='0.17', mirror=mirrors.gnu, format='gz')
+class Gettext__mingw (Gettext):
+
+    #source = mirrors.with_template (name='gettext', version='0.17', mirror=mirrors.gnu, format='gz')
+    source = Gettext.source
+    patches = ['gettext-0.15-mingw.patch']
  
     def config_cache_overrides (self, str):
         return (re.sub ('ac_cv_func_select=yes', 'ac_cv_func_select=no',
