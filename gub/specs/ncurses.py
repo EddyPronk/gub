567524f35c90d9b03bde007dd431fbdea1cd64ca
Jan Nieuwenhuizen <janneke@gnu.org>
2008-10-24 15:01:19 +0200

    Bugfix for ncurses.

--- gub/specs/ncurses.py
+++ gub/specs/ncurses.py
@@ -4,10 +4,10 @@ from gub import toolsbuild
 class Ncurses (targetbuild.TargetBuild):
     source = 'ftp://ftp.gnu.org/pub/gnu/ncurses/ncurses-5.5.tar.gz'
     def license_files (self):
-        return '%(srcdir)s/README'
+        return ['%(srcdir)s/README']
 
 class Ncurses__tools (toolsbuild.ToolsBuild):
     source = Ncurses.source
     def license_files (self):
-        return '%(srcdir)s/README'
+        return ['%(srcdir)s/README']
 
