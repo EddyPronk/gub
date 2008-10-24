e4497264649a1d6a17357386de985962fabc2c91
Jan Nieuwenhuizen <janneke@gnu.org>
2008-10-24 21:03:52 +0200

    Resurrect tcltk.py

--- gub/specs/tcltk.py
+++ gub/specs/tcltk.py
@@ -1,32 +1,24 @@
-from gub import mirrors
-from gub import misc
-from gub import targetbuild
 from gub import context
+from gub import targetbuild
  
 class Tcltk (targetbuild.TargetBuild):
-    source = mirrors.with_template (name='tcltk', 
-            mirrors.lilypondorg,
-            version='8.4.14')
+    source = 'http://lilypond.org/download/gub-sources/tcltk-8.4.14.tar.gz'
     def license_files (self):
         return ['%(srcdir)s/tcl/license.terms']
-    
     def force_sequential_build (self):
         return True
     def configure (self):
         self.system ('''cd %(srcdir)s/tcl &&  ./unix/configure --prefix=%(install_prefix)s
 cd %(srcdir)s/tk/ && ./unix/configure --prefix=%(install_prefix)s
 ''')
-        
     def compile (self):
         self.system ('cd %(builddir)s/tcl && make')
         self.system ('cd %(builddir)s/tk && make')
-                     
     def patch (self):
         self.system ("cd %(srcdir)s/tcl/ && patch -p1 < %(patchdir)s/tcl-8.4.14-dde.patch")
     def install (self):
         self.system ('cd %(builddir)s/tcl && make DESTDIR=%(install_root)s install')
         self.system ('cd %(builddir)s/tk && make DESTDIR=%(install_root)s install')
-
     def get_subpackage_definitions (self):
         s = targetbuild.TargetBuild.get_subpackage_definitions (self)
         s['doc'].append (self.settings.prefix_dir + '/lib/tk8.4/demos')
@@ -36,7 +28,6 @@ class Tcltk__mingw (Tcltk):
     @context.subst_method
     def RC (self):
         return  '%(cross_prefix)s/bin/%(target_architecture)s-windres'
-    
     def configure (self):
         self.system ('''mkdir -p %(builddir)s/tcl  %(builddir)s/tk ''')
         self.system ('''cd %(builddir)s/tcl &&  %(srcdir)s/tcl/win/configure --prefix=%(install_prefix)s
