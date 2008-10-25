568e0612dbd870475711b025894f3a4472dcd16e
Jan Nieuwenhuizen <janneke@gnu.org>
2008-10-25 10:24:33 +0200

    Typo in texinfo.

--- gub/specs/texinfo.py
+++ gub/specs/texinfo.py
@@ -4,6 +4,6 @@ class Texinfo__tools (toolsbuild.ToolsBuild):
     source = 'ftp://ftp.gnu.org/pub/gnu/texinfo/texinfo-4.11.tar.bz2'
     def patch (self):
         # for 4.11
-        self.system ('cd %(srcdir)s/buil-aux && chmod +x install-sh config.sub config.guess missing || :')
+        self.system ('cd %(srcdir)s/build-aux && chmod +x install-sh config.sub config.guess missing')
     def get_build_dependencies (self):
         return ['ncurses']
