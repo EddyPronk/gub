ac21f296a6a46422147400151dc7cdc4e8cf4906
Jan Nieuwenhuizen <janneke@gnu.org>
2008-10-25 23:00:23 +0200

    Some fruitless work on docbook-utils and jade.

---
+++ gub/specs/docbook-utils.py
@@ -0,0 +1,6 @@
+from gub import toolsbuild
+
+class Docbook_utils__tools (toolsbuild.ToolsBuild):
+    source = 'ftp://sources.redhat.com/pub/docbook-tools/new-trials/SOURCES/docbook-utils-0.6.14.tar.gz'
+    def get_build_dependencies (self):
+        return ['jade']
---
+++ gub/specs/jade.py
@@ -0,0 +1,28 @@
+from gub import build
+from gub import commands
+from gub import misc
+from gub import toolsbuild
+
+class Jade__tools (toolsbuild.ToolsBuild):
+    source = 'ftp://ftp.jclark.com/pub/jade/jade-1.2.1.tar.gz'
+    def get_build_dependencies (self):
+        return ['tools::autoconf', 'tools::libtool']
+    def autoupdate (self):
+        self.system ('cd %(srcdir)s && cp -f config/configure.in .')
+        self.system ('cd %(srcdir)s && libtoolize --force --copy || :')
+        self.runner._execute (commands.ForcedAutogenMagic (self))
+        self.shadow_tree ('%(srcdir)s', '%(builddir)s')
+    def configure_command (self):
+        return (build.UnixBuild.configure_command (self)
+                + misc.join_lines ('''
+CFLAGS=-I%(system_prefix)s/include
+LDFLAGS=-L%(system_prefix)s/lib
+LD_LIBRARY_PATH=%(system_prefix)s/lib
+'''))
+    def configure_binary (self):
+        return '%(builddir)s/configure'
+    def configure (self):
+        toolsbuild.ToolsBuild.configure (self)
+        self.system ('cd %(builddir)s; for i in $(ls -1dF * |grep /); do make -C $i -f ../Makefile.lib Makefile.lt; done || :')
+    def makeflags (self):
+        return 'top_builddir=%(builddir)s'
