87efbba3d26a4205cf827868a263814c07c0ee6b
Jan Nieuwenhuizen <janneke@gnu.org>
2008-10-26 08:33:57 +0100

    Mingw patch for rasqal.

--- gub/specs/rasqal.py
+++ gub/specs/rasqal.py
@@ -4,3 +4,6 @@ class Rasqal (targetbuild.TargetBuild):
     source = 'http://download.librdf.org/source/rasqal-0.9.16.tar.gz'
     def get_build_dependencies (self):
         return ['raptor-devel', 'libpcre-devel']
+
+class Rasqal__mingw (Rasqal):
+    patches = ['rasqal-0.9.16-mingw.patch']
