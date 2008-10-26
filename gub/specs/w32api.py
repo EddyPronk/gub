89c925489925da05f2efbcf9ca94c23bb356a44a
Jan Nieuwenhuizen <janneke@gnu.org>
2008-10-26 08:33:36 +0100

    Update w32api to latest 3.12.

--- gub/specs/w32api.py
+++ gub/specs/w32api.py
@@ -1,8 +1,9 @@
 from gub import build
-from gub import mirrors
 
 class W32api (build.BinaryBuild, build.SdkBuild):
-    source = mirrors.with_template (name='w32api', version='3.11', strip_components=0, mirror=mirrors.mingw)
+    #source = 'http://surfnet.dl.sourceforge.net/sourceforge/mingw/mingw-3.11.tar.gz'
+    #source = 'http://surfnet.dl.sourceforge.net/sourceforge/mingw/mingw-3.12.tar.gz'
+    source = 'http://surfnet.dl.sourceforge.net/sourceforge/mingw/w32api-3.12-mingw32-dev.tar.gz'
     def __init__ (self, settings, source):
         build.BinaryBuild.__init__ (self, settings, source)
         source.strip_components = 0
@@ -11,4 +12,3 @@ class W32api (build.BinaryBuild, build.SdkBuild):
         self.system ('''
 cd  %(srcdir)s/ && mkdir usr && mv include lib usr/
 ''')
-
