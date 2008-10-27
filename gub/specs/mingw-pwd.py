8622373adb580f1fd6a8d85e32b4d184dd85029e
Jan Nieuwenhuizen <janneke@gnu.org>
2008-10-27 11:06:57 +0100

    Refactor Mingw-pwd.

--- gub/specs/mingw-pwd.py
+++ gub/specs/mingw-pwd.py
@@ -2,11 +2,7 @@ from gub import build
 
 ##class Mingw_pwd (build.SdkBuild): nicer as sdk, but triggers full rebuild
 class Mingw_pwd (build.NullBuild):
-    source = 'http://mingw/mingw-pwd-1.0.tar.gz'
-    def stages (self):
-        return ['untar', 'install', 'package', 'clean']
-    def download (self):
-        pass
+    source = 'url://host/mingw-pwd-1.0.tar.gz'
     def untar (self):
         self.dump (r'''#! /bin/bash
 options=$(echo -n "$@" | tr -d W | sed 's/-\( \|$\)/\1/')
