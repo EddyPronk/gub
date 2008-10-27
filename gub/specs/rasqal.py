7dea0586354ee6e77d296b48f859762facbc6011
Jan Nieuwenhuizen <janneke@gnu.org>
2008-10-27 13:57:42 +0100

    Rasqal: install config script.

--- gub/specs/rasqal.py
+++ gub/specs/rasqal.py
@@ -4,6 +4,8 @@ class Rasqal (targetbuild.AutoBuild):
     source = 'http://download.librdf.org/source/rasqal-0.9.16.tar.gz'
     def get_build_dependencies (self):
         return ['raptor-devel', 'libpcre-devel']
+    def config_script (self):
+        return 'rasqal-config'
 
 class Rasqal__mingw (Rasqal):
     patches = ['rasqal-0.9.16-mingw.patch']
