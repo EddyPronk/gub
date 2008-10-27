cb0b86c0ed035ebdd285d5872b2d992c1a02b491
Jan Nieuwenhuizen <janneke@gnu.org>
2008-10-27 13:58:39 +0100

    Redland: oops, use right url and install config script.

--- gub/specs/redland.py
+++ gub/specs/redland.py
@@ -1,6 +1,8 @@
 from gub import targetbuild
 
 class Redland (targetbuild.AutoBuild):
-    source = 'http://download.librdf.org/source/rasqal-0.9.16.tar.gz'
+    source = 'http://download.librdf.org/source/redland-1.0.8.tar.gz'
     def get_build_dependencies (self):
         return ['rasqal-devel']
+    def config_script (self):
+        return 'redland-config'
