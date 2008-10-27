f76c12bcf39150e6196990598c02b4bf42fc322b
Jan Nieuwenhuizen <janneke@gnu.org>
2008-10-27 11:06:38 +0100

    Use MakeBuild for librestrict.

--- gub/specs/librestrict.py
+++ gub/specs/librestrict.py
@@ -1,11 +1,10 @@
 from gub import toolsbuild
 
-class Librestrict__tools (toolsbuild.AutoBuild):
+class Librestrict__tools (toolsbuild.MakeBuild):
     source = 'url://host/librestrict-1.0.tar.gz'
-    # ugh, download is not a true stage [yet]
     def untar (self):
         self.system ('rm -rf %(builddir)s')
-    def configure (self):
+    def shadow (self):
         self.shadow_tree ('%(gubdir)s/librestrict', '%(builddir)s')
     def makeflags (self):
         return 'prefix=%(system_prefix)s'
