696091cdddddaeb0205ee1d76acdea505b2a7c18
Jan Nieuwenhuizen <janneke@gnu.org>
2008-10-28 12:49:24 +0100

    Typo.

--- gub/specs/osx-lilypad.py
+++ gub/specs/osx-lilypad.py
@@ -7,4 +7,4 @@ class Osx_lilypad__darwin__ppc (build.NullBuild):
     source = 'http://lilypond.org/download/gub-sources/osx-lilypad-ppc-0.2.tar.gz'
 
 class Osx_lilypad__darwin__x86 (build.NullBuild):
-    source = 'http://lilypond.org/download/gub-sources/osx-lilypad-x86-0.2.tar.gz&name=osx-lilypad
+    source = 'http://lilypond.org/download/gub-sources/osx-lilypad-x86-0.2.tar.gz&name=osx-lilypad'
