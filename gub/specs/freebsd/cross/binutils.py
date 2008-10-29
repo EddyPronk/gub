bd375b74509445f868c239d3f4af35b27f5480a8
Jan Nieuwenhuizen <janneke@gnu.org>
2008-10-29 21:29:47 +0100

    Resurrect freebsd.

--- gub/specs/freebsd/cross/binutils.py
+++ gub/specs/freebsd/cross/binutils.py
@@ -1,10 +1,11 @@
 from gub.specs.cross import binutils
+from gub import misc
 
 class Binutils__freebsd (binutils.Binutils):
+    patches = binutils.Binutils.patches
     def configure_command (self):
         # Add --program-prefix, otherwise we get
         # i686-freebsd-FOO iso i686-freebsd4-FOO.
-        from gub import misc
         return (binutils.Binutils.configure_command (self)
             + misc.join_lines ('''
 --program-prefix=%(toolchain_prefix)s
