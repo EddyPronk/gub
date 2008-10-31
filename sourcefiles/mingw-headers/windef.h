9680733854cf08b9f09e0b8f7b682094eb6f6738
Jan Nieuwenhuizen <janneke@gnu.org>
2008-10-31 14:01:41 +0100

    Make mingw header hack work with tools/

--- sourcefiles/mingw-headers/windef.h
+++ sourcefiles/mingw-headers/windef.h
@@ -1,9 +1,10 @@
 #ifndef OOO_WINDEF_H
 #define OOO_WINDEF_H
 
-/* Hack arount tools/solar.h BOOL etc gdisaster */
+/* Hack arount tools/solar.h BOOL etc disaster */
 
-#ifdef __MINGW32__
+#if defined (__MINGW32__) && ! defined (BOOL)
+#define OOO_BOOL_HACK
 #define BOOL WINDEF_BOOL
 #define INT32 WINDEF_INT32
 #define UINT32 WINDEF_UINT32
@@ -11,10 +12,11 @@
 
 #include <../include/windef.h>
 
-#if 0 //def __MINGW32__
+#if 0 //def OOO_BOOL_HACK
 #undef BOOL
 #undef INT32
 #undef UINT32
-#endif /* __MINGW32__ */
+#undef OOO_BOOL_HACK
+#endif /* OOO_BOOL_HACK */
 
 #endif /* OOO_WINDEF_H */
