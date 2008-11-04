d359913fedba7af86efdf241cd44d46bf88ee9ad
Jan Nieuwenhuizen <janneke@gnu.org>
2008-11-04 16:23:56 +0100

    Build librestrict without using [GNU] make.

--- gub/specs/librestrict.py
+++ gub/specs/librestrict.py
@@ -9,3 +9,12 @@ class Librestrict__tools (tools.MakeBuild):
         return 'prefix=%(system_prefix)s'
     def LD_PRELOAD (self):
         return ''
+
+class Librestrict_nomake__tools (Librestrict__tools):
+    def compile_command (self):
+        return 'gcc -W -Wall -shared -fPIC -o librestrict.so restrict.c'
+    def install_command (self):
+        return ('mkdir -p %(install_root)s/%(system_prefix)s/lib'
+                ' && cp -p librestrict.so %(install_root)s/%(system_prefix)s/lib')
+
+Librestrict__tools = Librestrict_nomake__tools
