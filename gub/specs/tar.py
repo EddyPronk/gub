748bff7fe12359b3b0d7e29ea0f76b8aeb2c8faa
Jan Nieuwenhuizen <janneke@gnu.org>
2008-11-04 22:29:09 +0100

    FreeBSD compatibility: add tar package, using simple tar command.

---
+++ gub/specs/tar.py
@@ -0,0 +1,9 @@
+from gub import tools
+
+class Tar__tools (tools.AutoBuild):
+    source = 'ftp://ftp.gnu.org/pub/gnu/tar/tar-1.20.tar.gz'
+    def __init__ (self, settings, source):
+        tools.AutoBuild.__init__ (self, settings, source)
+        self.source._unpack = self.source._unpack_promise_well_behaved
+    def wrap_executables (self):
+        return
