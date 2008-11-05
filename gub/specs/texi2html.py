b17bcc27d1fceccc96bbf20cd6830e5c33dfbd46
Jan Nieuwenhuizen <janneke@gnu.org>
2008-11-05 09:14:38 +0100

    Remove another manual git dependency.

--- gub/specs/texi2html.py
+++ gub/specs/texi2html.py
@@ -2,5 +2,3 @@ from gub import tools
 
 class Texi2html__tools (tools.AutoBuild):
     source = 'http://lilypond.org/vc/texi2html.git&branch=master'
-    def get_build_dependencies (self):
-        return ['git']
