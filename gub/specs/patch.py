9f5bf0f6c0d61a31f657087c5f723e44878ac7e2
Jan Nieuwenhuizen <janneke@gnu.org>
2008-11-05 17:19:50 +0100

    Patch: hack for configure failure on FreeBSD.

--- gub/specs/patch.py
+++ gub/specs/patch.py
@@ -4,6 +4,11 @@ class Patch__tools (tools.AutoBuild):
     source = 'ftp://ftp.gnu.org/pub/gnu/patch/patch-2.5.4.tar.gz'
     def configure_variables (self):
         return ''
+    def configure (self):
+        tools.AutoBuild.configure (self)
+        if 'freebsd' in self.settings.build_architecture:
+            self.file_sub ([('^#define HAVE_SETMODE 1', '/* #undef HAVE_SETMODE */')],
+                           '%(builddir)s/config.h')
     def install_command (self):
         return self.broken_install_command ()
     def wrap_executables (self):
