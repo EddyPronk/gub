634f8c8f2900c824603eb7cd6d25cc3bc9ed45d5
Jan Nieuwenhuizen <janneke@gnu.org>
2008-11-05 21:35:52 +0100

    Remove subdir name from gub-sources urls.

--- gub/specs/freebsd-runtime.py
+++ gub/specs/freebsd-runtime.py
@@ -1,9 +1,9 @@
 from gub import build
 
 class Freebsd_runtime (build.BinaryBuild, build.SdkBuild):
-    source = 'http://lilypond.org/download/gub-sources/freebsd-runtime/freebsd-runtime-4.11-1.%(package_arch)s.tar.gz&strip=0'
+    source = 'http://lilypond.org/download/gub-sources/freebsd-runtime-4.11-1.%(package_arch)s.tar.gz&strip=0'
     def untar (self):
         build.BinaryBuild.untar (self)
 
 class Freebsd_runtime__freebsd__64 (Freebsd_runtime):
-    source = 'http://lilypond.org/download/gub-sources/freebsd-runtime/freebsd-runtime-6.2-2.%(package_arch)s.tar.gz&strip=0'
+    source = 'http://lilypond.org/download/gub-sources/freebsd-runtime-6.2-2.%(package_arch)s.tar.gz&strip=0'
