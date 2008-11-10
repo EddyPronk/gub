fa58befaab1c9bfd22c46e48da0757185c03398d
Jan Nieuwenhuizen <janneke@gnu.org>
2008-11-10 21:23:48 +0100

    Netpbm: really build without libxml2, add missing dependencies.

--- gub/specs/netpbm.py
+++ gub/specs/netpbm.py
@@ -1,44 +1,50 @@
+from gub import misc
 from gub import tools
 
 class Netpbm__tools (tools.AutoBuild):
     # source='svn:https://svn.sourceforge.net/svnroot/netpbm/stable&revision=172'
     source='http://lilypond.org/download/gub-sources/netpbm-patched-10.35.tar.bz2'
-
     def get_build_dependencies (self):
-        return ['libjpeg'] # libtiff-dev libx11-dev
-
+        return ['libjpeg', 'libpng', 'libtiff', 'zlib'] #libxml2? libx11-dev
     def configure (self):
         self.shadow ()
         self.dump ('\n'*3 + 'static\n' + '\n'*18, '%(builddir)s/answers')
         self.system ('cd %(builddir)s && %(srcdir)s/configure < answers')
-
+    def force_sequential_build (self):
+        return True
+    def makeflags (self):
         '''
 libpbm3.c:116: note: use -flax-vector-conversions to permit conversions between vectors with differing element types or numbers of subparts
 libpbm3.c:116: fout: incompatible type for argument 1 of __builtin_ia32_pcmpeqb
 '''
-    def compile (self):
-        self.system ('''cd %(builddir)s && make CC=gcc \
-        CFLAGS="-O2 -fPIC -flax-vector-conversions"  \
-        LDFLAGS="-L%(builddir)s/pbm -L%(builddir)s/pgm -L%(builddir)s/pnm -L%(builddir)s/ppm" \
-        LADD="-lm" \
-        LINUXSVGALIB="NONE" \
-        XML2LIBS="NONE" \
-        X11LIB="NONE"
+        return misc.join_lines ('''
+CC=gcc
+CFLAGS='-O2 -fPIC -flax-vector-conversions'
+LDFLAGS='%(rpath)s -L%(builddir)s/pbm -L%(builddir)s/pgm -L%(builddir)s/pnm -L%(builddir)s/ppm'
+LADD=-lm
+LINUXSVGALIB=NONE
+XML2LD=NONE
+XML2_LIBS=NONE
+XML2_CFLAGS=NONE
+X11LIB=NONE
 ''')
     def install (self):
-        self.system ('mkdir -p %(install_root)s/')
-        self.system ('cd %(builddir)s && make package pkgdir=%(install_prefix)s LINUXSVGALIB="NONE" XML2LIBS="NONE" X11LIB="NONE"')
-        self.system ('''rm -rf %(install_prefix)s/misc 
-rm -rf %(install_prefix)s/README
-rm -rf %(install_prefix)s/VERSION
-rm -rf %(install_prefix)s/link
-rm -rf %(install_prefix)s/misc
-rm -rf %(install_prefix)s/man
-rm -rf %(install_prefix)s/pkginfo
-rm -rf %(install_prefix)s/config_template
-''')
-    def packaging_suffix_dir (self):
-        return ''
+        # Great.  netpmb's install will not create any parent directories
+        self.system ('mkdir -p %(install_root)s%(system_prefix)s')
+        # but demands that the toplevel install directory does not yet exist.
+        # It's a feature! :-)
+        self.system ('rmdir %(install_root)s%(system_prefix)s')
 
+        self.system ('cd %(builddir)s && make package pkgdir=%(install_root)s%(system_prefix)s %(makeflags)s')
+        # Huh, we strip stuff in installer.py, no?  Hmm.
+        self.system ('''rm -rf %(install_prefix)s/misc 
+rm -rf %(install_root)s%(system_prefix)s/README
+rm -rf %(install_root)s%(system_prefix)s/VERSION
+rm -rf %(install_root)s%(system_prefix)s/link
+rm -rf %(install_root)s%(system_prefix)s/misc
+rm -rf %(install_root)s%(system_prefix)s/man
+rm -rf %(install_root)s%(system_prefix)s/pkginfo
+rm -rf %(install_root)s%(system_prefix)s/config_template
+''')
     def license_files (self):
         return '%(srcdir)s/README'
