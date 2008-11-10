0b14edf21aab313e373604bdcae386b61c9c1ec4
Jan Nieuwenhuizen <janneke@gnu.org>
2008-11-10 21:27:11 +0100

    Fontforge: fix picking-up of libraries from /.  Add dependencies.

--- gub/specs/fontforge.py
+++ gub/specs/fontforge.py
@@ -2,29 +2,9 @@ from gub import tools
 
 class Fontforge__tools (tools.AutoBuild):
     source = 'http://lilypond.org/download/gub-sources/fontforge_full-20080927.tar.bz2'
-
-    def configure_command (self):
-        return (tools.AutoBuild.configure_command (self)
-                + ' --without-freetype-src'
-                + ' --disable-libff '
-                # let's ignore python (and its dynamic link intracies
-                # for now).
-                + ' --without-python')
-
-    def configure (self):
-        self.shadow ()
-        tools.AutoBuild.configure (self)
-
+    patches = ['fontforge-20080927-noxml2.patch']
     def get_build_dependencies (self):
-        return ['freetype']
-
-    def srcdir (self):
-        return tools.AutoBuild.srcdir (self).replace ('_full', '')
-
-    def force_sequential_build (self):
-        return True
-    
-    # actions.
+        return ['freetype', 'libxml2']
     def patch (self):
         tools.AutoBuild.patch (self)
         for name in ['%(srcdir)s/fontforge/Makefile.dynamic.in',
@@ -39,3 +19,22 @@ class Fontforge__tools (tools.AutoBuild):
                 [(' -I$(top_srcdir)/inc',
                   ' -I$(top_srcdir)/inc -I$(top_builddir)/inc')],
                 name, use_re=False)
+        # URG, fontforge uses no *-config or *.pc files, but
+        # looks in /usr/include :-)
+        self.file_sub ([('([I" \(])/usr/include',
+                         r'\1%(system_prefix)s/include')],
+                       '%(srcdir)s/configure')
+    def configure_command (self):
+        return (tools.AutoBuild.configure_command (self)
+                + ' --without-freetype-src'
+                + ' --disable-libff '
+                # let's ignore python (and its dynamic link intracies
+                # for now).
+                + ' --without-python')
+    def configure (self):
+        self.shadow ()
+        tools.AutoBuild.configure (self)
+    def srcdir (self):
+        return tools.AutoBuild.srcdir (self).replace ('_full', '')
+    def force_sequential_build (self):
+        return True
