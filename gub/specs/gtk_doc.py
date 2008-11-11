1c0823cc5455f66f805fb5375d8a22ba993ff21b
Jan Nieuwenhuizen <janneke@gnu.org>
2008-11-11 12:54:24 +0100

    Ugh, have gtk-doc depend on libxml: gzopen64 trouble.

--- gub/specs/gtk_doc.py
+++ gub/specs/gtk_doc.py
@@ -2,3 +2,6 @@ from gub import tools
 
 class Gtk_doc__tools (tools.AutoBuild):
     source = 'ftp://ftp.gtk.org/pub/gtk-doc/gtk-doc-1.1.tar.gz'
+    def get_build_dependencies (self):
+        # Ugh, ugh: avoid [zlib: gzopen64] trouble
+        return ['libxml2']
