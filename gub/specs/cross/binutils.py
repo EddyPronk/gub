396274e6c4bf3a0c65fd32d9aa4c91b6787c7d62
Jan Nieuwenhuizen <janneke@gnu.org>
2008-11-14 13:48:26 +0100

    Binutils: robustification: override MULTIOSDIR instead of (re)moving libiberty blindly.
    This should fix http://lists.gnu.org/archive/html/lilypond-devel/2008-11/msg00163.html .

--- gub/specs/cross/binutils.py
+++ gub/specs/cross/binutils.py
@@ -11,7 +11,15 @@ class Binutils (cross.AutoBuild):
                 + misc.join_lines ('''
 --disable-werror
 '''))
-    def install (self):
+    def makeflags (self):
+        ##return 'toolexeclibdir=%(system_prefix)s/lib'
+        ##binutils' makefile uses:
+        ## MULTIOSDIR = `$(CC) $(LIBCFLAGS) -print-multi-os-directory`
+        ## which differs on each system.
+        ## so we MUST set it.
+        return 'MULTIOSDIR=../../lib'
+    def FIXME_breaks_on_some_linuxes_install (self):
+        # please document why this should be removed?
         cross.AutoBuild.install (self)
         self.system ('rm %(install_prefix)s/cross/lib/libiberty.a')
 
