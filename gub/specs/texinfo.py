98408212ef8c839c7c41482c6319d20fa3d5ccf3
Jan Nieuwenhuizen <janneke@gnu.org>
2009-01-17 09:23:52 +0000

    Texinfo: use tar.gz rather than bzip2, delays the need for tools::bzip2.

--- gub/specs/texinfo.py
+++ gub/specs/texinfo.py
@@ -1,7 +1,7 @@
 from gub import tools
 
 class Texinfo__tools (tools.AutoBuild):
-    source = 'ftp://ftp.gnu.org/pub/gnu/texinfo/texinfo-4.11.tar.bz2'
+    source = 'ftp://ftp.gnu.org/pub/gnu/texinfo/texinfo-4.11.tar.gz'
     def patch (self):
         # for 4.11
         self.system ('cd %(srcdir)s/build-aux && chmod +x install-sh config.sub config.guess missing')
