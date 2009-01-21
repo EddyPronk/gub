c74729c57c01594e6301bada0e40542e6ba7e7b8
Jan Nieuwenhuizen <janneke@gnu.org>
2009-01-21 21:54:54 +0100

    Oopslet.

--- gub/octal.py
+++ gub/octal.py
@@ -1,6 +1,6 @@
 # During python2 -> python3 transition, keep core code clear of octal
 # diff.  Diffs trigger checksum problems.
-if 1: sys.verions.startswith ('2'):
+if 1: #sys.verions.startswith ('2'):
     o644 = 0644
     o755 = 0755
 # does not parse
