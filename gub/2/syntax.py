d854c1073152aaeb510b4de090907a8c1b38c8bf
Jan Nieuwenhuizen <janneke@gnu.org>
2009-01-28 14:02:39 +0100

    Python3: Add gub/{2,3}/syntax.py with printf.

---
+++ gub/2/syntax.py
@@ -0,0 +1,5 @@
+
+def printf (*args):
+    for i in args:
+        print i,
+    print
---
+++ gub/specs/test-23.py
@@ -0,0 +1,39 @@
+import sys
+#
+from gub import tools
+
+from gub import octal
+assert (octal.o644 == 420)
+assert (octal.o755 == 493)
+
+from gub.syntax import printf
+
+sys.stdout.write ('\nEXPECT:2 3')
+sys.stdout.write ('\n>>>')
+printf (2,3)
+sys.stdout.write ('<<<\n')
+
+sys.stdout.write ('\nEXPECT: (2, 3)')
+sys.stdout.write ('\n>>>')
+printf ((2,3))
+sys.stdout.write ('<<<\n')
+
+class Test_23 (tools.NullBuild):
+    source = 'url://host/test-23-1.0.tar.gz'
+    def untar (self):
+        pass
+    def install (self):
+        self.system ('mkdir -p %(install_root)s%(system_prefix)s')
+
+sys.stdout.write ('''\nEXPECT: <class 'test-23.Test_23'>''')
+sys.stdout.write ('\n>>>')
+printf (Test_23)
+sys.stdout.write ('<<<\n')
+
+from gub import settings
+from gub import repository
+sys.stdout.write ('\nEXPECT:<test-23.Test_23 object at 0x00000>''')
+sys.stdout.write ('\n>>>')
+printf (Test_23 (settings.Settings (), repository.Version ('test-23')))
+sys.stdout.write ('<<<\n')
+
