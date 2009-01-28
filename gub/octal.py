17be162208f534552c2141f768eb4df6acb75b3e
Jan Nieuwenhuizen <janneke@gnu.org>
2009-01-28 13:59:58 +0100

    Python3: Move and split gub.octal.py into gub/2/octal.py and gub/3/octal.py.

--- gub/octal.py
+++
@@ -1,9 +0,0 @@
-# During python2 -> python3 transition, keep core code clear of octal
-# diff.  Diffs trigger checksum problems.
-if 1: #sys.verions.startswith ('2'):
-    o644 = 0644
-    o755 = 0755
-# does not parse
-#else:
-#    o644 = 0o644
-#    o755 = 0o755
