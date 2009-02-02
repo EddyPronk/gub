0b876b7b44a323ec24b6d0830ff9990c0586355e
Jan Nieuwenhuizen <janneke@gnu.org>
2009-02-02 22:13:53 +0100

    Add libtiff as target.

--- gub/specs/libtiff.py
+++ gub/specs/libtiff.py
@@ -1,6 +1,16 @@
+from gub import target
 from gub import tools
 
-class Libtiff__tools (tools.AutoBuild):
+class Libtiff (target.AutoBuild):
     source = 'http://dl.maptools.org/dl/libtiff/tiff-3.8.2.tar.gz'
     def get_build_dependencies (self):
-        return ['libtool']
+        return ['tools::libtool', 'libjpeg-devel']
+    def configure (self):
+        target.AutoBuild.configure (self)
+# libtool: install: error: cannot install `libtiffxx.la' to a directory not ending in /home/janneke/vc/gub/target/linux-64/build/libtiff-3.8.2/libtiff/.libs
+        self.update_libtool ()
+
+class Libtiff__tools (tools.AutoBuild):
+    source = Libtiff.source
+    def get_build_dependencies (self):
+        return ['libtool', 'libjpeg-devel']
