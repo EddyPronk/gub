94c2a304443d2046b34f3dd8f0666bf8aa4f8bd5
Jan Nieuwenhuizen <janneke@gnu.org>
2009-02-02 22:14:06 +0100

    Bump glibmm.

--- gub/specs/glibmm.py
+++ gub/specs/glibmm.py
@@ -1,10 +1,16 @@
 from gub import target
 
 class Glibmm (target.AutoBuild):
-    source = 'http://ftp.gnome.org/pub/GNOME/sources/glibmm/2.12/glibmm-2.12.10.tar.gz'
+#    source = 'http://ftp.gnome.org/pub/GNOME/sources/glibmm/2.12/glibmm-2.12.10.tar.gz'
+    source = 'http://ftp.gnome.org/pub/GNOME/sources/glibmm/2.18/glibmm-2.18.1.tar.gz'
     def _get_build_dependencies (self):
         return ['glib', 'libsig++']
     def get_build_dependencies (self):
         return self._get_build_dependencies ()
     def get_dependency_dict (self):
         return {'': self._get_build_dependencies ()}
+    ## FIXME: tools.configure () always updates libtool
+    ## why no in target.py // 18 packages do this??
+    def configure (self):
+        target.AutoBuild.configure (self)
+        self.update_libtool ()
