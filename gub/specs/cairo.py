c7b212d1447db6ac84115bf2d63618404af5bdfb
Jan Nieuwenhuizen <janneke@gnu.org>
2009-02-06 09:01:30 +0100

    Cairo: remove configure-generated from srcdir.
    Fixes cross build/--srcdir build.

--- gub/specs/cairo.py
+++ gub/specs/cairo.py
@@ -2,15 +2,20 @@ from gub import target
 
 class Cairo (target.AutoBuild):
     source = 'http://www.cairographics.org/releases/cairo-1.8.6.tar.gz'
+    def patch (self):
+        target.AutoBuild.patch (self)
+        self.system ('rm -f %(srcdir)s/src/cairo-features.h')
     def _get_build_dependencies (self):
         return ['tools::libtool', 'fontconfig-devel', 'libpng-devel', 'libx11-devel', 'libxrender-devel', 'pixman-devel']
 
 class Cairo__mingw (Cairo):
     source = Cairo.source
-    def get_build_dependencies (self):
-        return [x for x in Cairo.get_build_dependencies (self)
+    def configure_command (self):
+        return (Cairo.configure_command (self)
+                + ' --enable-win32=yes'
+                + ' --enable-win32-render=yes'
+                + ' --disable-xlib'
+                + ' --disable-xlib-render')
+    def _get_build_dependencies (self):
+        return [x for x in Cairo._get_build_dependencies (self)
                 if 'libx' not in x]
-    def get_dependency_dict (self):
-       return {'': [x.replace ('-devel', '')
-                    for x in self._get_build_dependencies ()
-                    if 'tools::' not in x and 'cross/' not in x and 'libx' not in x]}
