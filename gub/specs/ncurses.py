f2f434a8cddc48d0bdd28d086e743311632657cc
Jan Nieuwenhuizen <janneke@gnu.org>
2009-02-08 18:47:20 +0100

    ncurses: build shared.

--- gub/specs/ncurses.py
+++ gub/specs/ncurses.py
@@ -3,11 +3,16 @@ from gub import tools
 
 class Ncurses (target.AutoBuild):
     source = 'ftp://ftp.gnu.org/pub/gnu/ncurses/ncurses-5.5.tar.gz'
+    def configure_command (self):
+        return (tools.AutoBuild.configure_command (self)
+                + ' --without-normal --with-shared ')
     def license_files (self):
         return ['%(srcdir)s/README']
 
 class Ncurses__tools (tools.AutoBuild):
     source = Ncurses.source
+    def configure_command (self):
+        return (tools.AutoBuild.configure_command (self)
+                + ' --with-normal --with-shared ')
     def license_files (self):
         return ['%(srcdir)s/README']
-
