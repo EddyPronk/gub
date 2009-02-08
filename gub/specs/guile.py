fc5ef04bee79e21b7cdf86fda5a46686b9da6fc4
Jan Nieuwenhuizen <janneke@gnu.org>
2009-02-08 15:45:56 +0100

    Guile: all use patches.  Hmm, better automate this, like source?

--- gub/specs/guile.py
+++ gub/specs/guile.py
@@ -105,6 +105,7 @@ exit 0
 
 class Guile__mingw (Guile):
     source = Guile.source
+    patches = Guile.patches
     def __init__ (self, settings, source):
         Guile.__init__ (self, settings, source)
         # Configure (compile) without -mwindows for console
@@ -154,6 +155,7 @@ libltdl_cv_sys_search_path=${libltdl_cv_sys_search_path="%(system_prefix)s/lib"}
         self.system ('''mv %(install_prefix)s/lib/lib*[0-9].la %(install_prefix)s/bin''')
 
 class Guile__linux (Guile):
+    patches = Guile.patches
     def compile_command (self):
         # FIXME: when not x-building, guile runs guile without
         # setting the proper LD_LIBRARY_PATH.
@@ -161,10 +163,12 @@ class Guile__linux (Guile):
                 + Guile.compile_command (self))
 
 class Guile__linux__ppc (Guile__linux):
+    patches = Guile.patches
     def config_cache_overrides (self, str):
         return str + "\nguile_cv_have_libc_stack_end=no\n"
 
 class Guile__freebsd (Guile):
+    patches = Guile.patches
     def config_cache_settings (self):
         return (Guile.config_cache_settings (self)
                 + '''
@@ -177,8 +181,7 @@ guile_cv_use_csqrt="no"
                 + Guile.configure_variables (self))
 
 class Guile__darwin (Guile):
-    source = 'git://git.sv.gnu.org/guile.git&branch=branch_release-1-8&revision=release_1-8-4'
-
+    patches = Guile.patches
     def install (self):
         Guile.install (self)
 
@@ -193,14 +196,14 @@ class Guile__darwin (Guile):
                          'libguile-srfi*.dylib')
  
 class Guile__darwin__x86 (Guile__darwin):
-    source = 'git://git.sv.gnu.org/guile.git&branch=branch_release-1-8&revision=release_1-8-4'
-
+    patches = Guile.patches
     def configure (self):
         Guile__darwin.configure (self)
         self.file_sub ([('guile-readline', '')],
                        '%(builddir)s/Makefile')
         
 class Guile__cygwin (Guile):
+    patches = Guile.patches
     def category_dict (self):
         return {'': 'Interpreters'}
     # Using gub dependencies only would be nice, but
@@ -275,7 +278,7 @@ guile-tut').
 
 class Guile__tools (tools.AutoBuild, Guile):
     source = Guile.source
-    patches = ['guile-reloc-1.8.6.patch', 'guile-1.8.6-test-use-srfi.patch']
+    patches = Guile.patches
     def _get_build_dependencies (self):
         return (Guile._get_build_dependencies (self)
                 + ['autoconf', 'automake', 'gettext', 'flex', 'libtool'])
