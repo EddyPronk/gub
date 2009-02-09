202d3f56d66f07ef1ddf83a08035acd1ad538409
Jan Nieuwenhuizen <janneke@gnu.org>
2009-02-09 12:06:42 +0100

    libx11: disable xf86bigfont, as it may be picked-up from /usr.  Ugh.

--- gub/specs/libx11.py
+++ gub/specs/libx11.py
@@ -4,3 +4,6 @@ class Libx11 (target.AutoBuild):
     source = 'http://xorg.freedesktop.org/releases/X11R7.4/src/lib/libX11-1.1.5.tar.gz'
     def _get_build_dependencies (self):
         return ['libtool', 'inputproto-devel', 'kbproto-devel', 'libxcb-devel', 'xextproto-devel', 'xproto-devel', 'xtrans-devel']
+    def configure_command (self):
+        return (target.AutoBuild.configure_command (self)
+                + ' --disable-xf86bigfont')
