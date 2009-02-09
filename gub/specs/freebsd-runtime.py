b71debfdff61b201aed160df1a0a324797cd63db
Jan Nieuwenhuizen <janneke@gnu.org>
2009-02-09 16:46:20 +0100

    Oops.

--- gub/specs/freebsd-runtime.py
+++ gub/specs/freebsd-runtime.py
@@ -4,6 +4,10 @@ class Freebsd_runtime (build.BinaryBuild, build.SdkBuild):
     source = 'http://lilypond.org/download/gub-sources/freebsd-runtime-4.11-1.%(package_arch)s.tar.gz&strip=0'
     def untar (self):
         build.BinaryBuild.untar (self)
+
+class Freebsd_runtime__freebsd__x86 (Freebsd_runtime):
+    def untar (self):
+        Freebsd_runtime.untar (self)
         self.system ('''
 cp %(sourcefiledir)s/stdint-32.h %(srcdir)s%(prefix_dir)s/include/stdint.h
 ''')
