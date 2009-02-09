e8ccab648d027c90d55f03cc72adaa71abcbc56a
Jan Nieuwenhuizen <janneke@gnu.org>
2009-02-09 20:12:18 +0100

    libxext: x-build fix.

--- gub/specs/libxext.py
+++ gub/specs/libxext.py
@@ -4,3 +4,6 @@ class Libxext (target.AutoBuild):
     source = 'http://xorg.freedesktop.org/releases/X11R7.4/src/lib/libXext-1.0.4.tar.gz'
     def _get_build_dependencies (self):
         return ['libtool', 'libx11-devel']
+    def configure_command (self):
+        return (target.AutoBuild.configure_command (self)
+                + ' --disable-malloc0returnsnull')
