e0097b266de23eacbea867d3ffa61f5aa9aeeaf7
Jan Nieuwenhuizen <janneke@gnu.org>
2009-02-09 15:58:52 +0100

    freebsd::libxau: depend on libxconv.

--- gub/specs/libxau.py
+++ gub/specs/libxau.py
@@ -4,3 +4,7 @@ class Libxau (target.AutoBuild):
     source = 'http://xorg.freedesktop.org/releases/X11R7.4/src/everything/libXau-1.0.4.tar.gz'
     def _get_build_dependencies (self):
         return ['tools::libtool', 'xproto-devel']
+
+class Libxau__freebsd (Libxau):
+    def _get_build_dependencies (self):
+        return Libxau._get_build_dependencies (self) + ['libiconv-devel']
