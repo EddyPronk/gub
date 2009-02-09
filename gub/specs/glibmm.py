3c625cda5b2617caaf588e2ea5d699e2b5fc9269
Jan Nieuwenhuizen <janneke@gnu.org>
2009-02-09 16:44:16 +0100

    Remove examples from glibmm.

--- gub/specs/glibmm.py
+++ gub/specs/glibmm.py
@@ -4,3 +4,10 @@ class Glibmm (target.AutoBuild):
     source = 'http://ftp.gnome.org/pub/GNOME/sources/glibmm/2.18/glibmm-2.18.1.tar.gz'
     def _get_build_dependencies (self):
         return ['glib-devel', 'libsig++-devel']
+
+class Glibmm__freebsd__x86 (Glibmm):
+    #source = 'http://ftp.gnome.org/pub/GNOME/sources/glibmm/2.19/glibmm-2.19.2.tar.gz'
+    def patch (self):
+        Glibmm.patch (self)
+        self.file_sub ([('(SUBDIRS = .*) examples( .*)', r'\1\2')],
+                         '%(srcdir)s/Makefile.in')
