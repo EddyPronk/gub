440ab3b377da3cba84f120e0e0cad8516eb4e599
Jan Nieuwenhuizen <janneke@gnu.org>
2009-02-09 13:34:30 +0100

    freebsd[4]-x86: use -lc_r for pthreads.

--- gub/specs/libpthread-stubs.py
+++ gub/specs/libpthread-stubs.py
@@ -5,8 +5,12 @@ class Libpthread_stubs (target.AutoBuild):
     def _get_build_dependencies (self):
         return ['libtool']
 
+class Libpthread_stubs__freebsd__x86 (Libpthread_stubs):
+    def configure_command (self):
+        return (Libpthread_stubs.configure_command (self)
+                + ' LDFLAGS=-lc_r')
+    
 class Libpthread_stubs__mingw (Libpthread_stubs):
-    source = Libpthread_stubs.source
     def _get_build_dependencies (self):
         return (Libpthread_stubs._get_build_dependencies (self)
                 + ['pthreads-w32-devel'])
