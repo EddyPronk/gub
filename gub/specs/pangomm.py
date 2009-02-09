d773499cf444602953d9fd5a6085adc97a9310e8
Jan Nieuwenhuizen <janneke@gnu.org>
2009-02-09 16:32:24 +0100

    pangomm: add missing dependency on glibmm.

--- gub/specs/pangomm.py
+++ gub/specs/pangomm.py
@@ -3,7 +3,7 @@ from gub import target
 class Pangomm (target.AutoBuild):
     source = 'http://ftp.acc.umu.se/pub/GNOME/sources/pangomm/2.14/pangomm-2.14.1.tar.gz'
     def _get_build_dependencies (self):
-        return ['pangocairo-devel', 'libsig++-devel']
+        return ['glibmm-devel', 'libsig++-devel', 'pangocairo-devel']
     def configure_command (self):
         return (target.AutoBuild.configure_command (self)
                 + ' CFLAGS=-I%(system_prefix)s/include/freetype2'
