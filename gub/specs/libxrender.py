5360f1d647945784048af6bf6160e9e9c4d262f7
Jan Nieuwenhuizen <janneke@gnu.org>
2009-02-09 15:57:24 +0100

    libxrender: x-build fix.

--- gub/specs/libxrender.py
+++ gub/specs/libxrender.py
@@ -4,3 +4,6 @@ class Libxrender (target.AutoBuild):
     source = 'http://xorg.freedesktop.org/releases/X11R7.4/src/everything/libXrender-0.9.4.tar.gz'
     def _get_build_dependencies (self):
         return ['tools::libtool', 'libx11-devel', 'libxdmcp-devel', 'renderproto-devel']
+    def configure_command (self):
+        return (target.AutoBuild.configure_command (self)
+                + ' --disable-malloc0returnsnull')
