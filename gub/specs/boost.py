45f4f9f37ef54e789d22b7b76b61c159253ba51c
Jan Nieuwenhuizen <janneke@gnu.org>
2009-02-10 10:52:54 +0100

    boost: bump to 1.38, bjam v2 build support.  Fixes freebsd-64.

--- gub/specs/boost.py
+++ gub/specs/boost.py
@@ -18,8 +18,25 @@ class BjamBuild (target.MakeBuild):
     def CFLAGS (self):
         return ''
     def compile_command (self):
+        #without = ['python', 'test']
+        wyth = ['date_time',
+              'filesystem',
+              'function_types',
+                'graph',
+                'iostreams',
+                'math',
+                'program_options',
+                #'python',
+                'regex',
+                'serialization',
+                'signals',
+                'system',
+                #'test',
+                'thread',
+                'wave']
         return misc.join_lines ('''
 bjam
+-q
 '-sTOOLS=gcc'
 '-sGCC=%(toolchain_prefix)sgcc -fPIC -DBOOST_PLATFORM_CONFIG=\\"boost/config/platform/linux.hpp\\"'
 '-sGXX=%(toolchain_prefix)sg++ -fPIC -DBOOST_PLATFORM_CONFIG=\\"boost/config/platform/linux.hpp\\"'
@@ -28,15 +45,15 @@ bjam
 '-sBUILD=release <optimization>space <inlining>on <debug-symbols>off <runtime-link>static'
 '-sPYTHON_VERSION=2.4'
 '-scxxflags=-fPIC'
---without-libraries=python
---without-test
 --layout=system
 --builddir=%(builddir)s
 --prefix=%(prefix_dir)s
 --exec-prefix=%(prefix_dir)s
 --libdir=%(prefix_dir)s/lib
 --includedir=%(prefix_dir)s/include
-''')
+--verbose
+'''
+                                + ' --with-'.join ([''] + wyth))
     def install_command (self):
         return (self.compile_command ()
                 + ' install').replace ('=%(prefix_dir)s', '=%(install_prefix)s')
@@ -56,15 +73,22 @@ class Boost (BjamBuild):
     def license_files (self):
         return ['%(srcdir)s/LICENSE_1_0.txt']
     def compile_command (self):
-        return 'PATH=%(builddir)s:PATH ' + BjamBuild.compile_command (self)
+        return 'PATH=%(builddir)s:$PATH ' + BjamBuild.compile_command (self)
     def install (self):
         BjamBuild.install (self)
         # Bjam `installs' header files by using symlinks to the source dir?
 
         def add_plain_lib_names (logger, file):
-            base = os.path.basename (file).replace ('-s.a', '.a')
+            base = (os.path.basename (file)
+                    .replace ('-mt.a', '.a')
+                    .replace ('-s.a', '.a')
+                    .replace ('-mt.so', '.so')
+                    .replace ('-s.so', '.so'))
             misc.symlink_in_dir (file, base)
+        self.map_locate (add_plain_lib_names, '%(install_prefix)s/lib', 'libboost_*-mt.a')
+        self.map_locate (add_plain_lib_names, '%(install_prefix)s/lib', 'libboost_*-mt.so')
         self.map_locate (add_plain_lib_names, '%(install_prefix)s/lib', 'libboost_*-s.a')
+        self.map_locate (add_plain_lib_names, '%(install_prefix)s/lib', 'libboost_*-s.so')
 
         def replace_links (logger, file):
             if os.path.islink (file):
@@ -87,26 +111,39 @@ class Boost__mingw (Boost):
         return (Boost.compile_command (self)
                 .replace ('linux.hpp', 'win32.hpp'))
     
-class Boost__freebsd__64 (Boost):
-    source = 'http://surfnet.dl.sourceforge.net/sourceforge/boost/boost_1_34_1.tar.bz2'
-    #source = 'http://surfnet.dl.sourceforge.net/sourceforge/boost/boost_1_38_0.tar.bz2'
+class Boost_v2 (Boost):
+    source = 'http://surfnet.dl.sourceforge.net/sourceforge/boost/boost_1_38_0.tar.bz2'
     def build_bjam (self):
+        Boost.patch (self)
         # the separately available boost-jam is terribly broken wrt
         # building boost: build included bjam
         self.system ('cd %(builddir)s/tools/jam/src && CC=gcc sh build.sh gcc && mv bin.*/bjam %(builddir)s')
+    def patch (self):
+        '''http://goodliffe.blogspot.com/2008/05/cross-compiling-boost.html
+
+Now, here's the magic. Brace yourself. This is how you use a custom
+gcc compiler, you write some odd rule into some very well hidden
+config file:
+
+echo "using gcc : 4.2.2 : PATH_TO_DIR/arm-softfloat-linux-gnu-g++ ; "
+> tools/build/v2/user-config.jam
+'''
+        gcc_version = '' #don't care
+        self.dump ('''
+using gcc : %(gcc_version)s : %(system_prefix)s%(cross_dir)s/bin/%(CXX)s;
+''',
+                   '%(srcdir)s/tools/build/v2/user-config.jam',
+                   env=locals ())
     def compile_command (self):
-        return (Boost.configure_command (self)
+        return (Boost.compile_command (self)
+                .replace ('-sTOOLS', 'toolset')
+                .replace ('<runtime-link>static', '<runtime-link>dynamic')
+                + ' link=shared')
+
+class Boost__freebsd__64 (Boost_v2):
+    source = 'http://surfnet.dl.sourceforge.net/sourceforge/boost/boost_1_38_0.tar.bz2'
+    def compile_command (self):
+        return (Boost_v2.compile_command (self)
                 .replace ('linux.hpp', 'bsd.hpp')
-                # yay, let's make a file called ./configure, but barf
-                # on common configure options. yes -- good idea!
-                # we'll make a real complicated 'make' command instead
-                # that hold all the parameters!
-                .replace ('--config-cache', '')
-                .replace ('--build=%(build_architecture)s', '')
-                .replace ('--host=%(target_architecture)s', '')
-                .replace ('--target=%(target_architecture)s', '')
-                .replace ('--disable-static', '')
-                .replace ('--enable-shared', '')
-                .replace ('--sysconfdir=%(prefix_dir)s/etc', '')
-                .replace ('--infodir=%(prefix_dir)s/share/info', '')
-                .replace ('--mandir=%(prefix_dir)s/share/man', ''))
+                .replace ('--with-math', '')
+                + ' target-os=freebsd')
