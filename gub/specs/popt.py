16c6ef005a7bfc0c66d549a73e2b43ff7126db92
Jan Nieuwenhuizen <janneke@gnu.org>
2009-02-11 10:14:15 +0100

    popt: freebsd wchar fix.

--- gub/specs/popt.py
+++ gub/specs/popt.py
@@ -6,6 +6,9 @@ class Popt (target.AutoBuild):
         return (string
                 + '\nac_cv_va_copy=${ac_cv_va_copy=C99}\n')
 
+class Popt__freebsd__x86 (Popt):
+    patches = ['popt-no-wchar-hack.patch']
+
 class Popt__mingw (Popt):
     patches = ['popt-no-sys-ioctl.patch']
     def configure_command (self):
