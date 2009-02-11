e4be7905289b2b661789fe1413437716be212d7f
Jan Nieuwenhuizen <janneke@gnu.org>
2009-02-11 19:17:41 +0100

    linux-ppc: re-add pixman auxvec fix.

--- gub/specs/pixman.py
+++ gub/specs/pixman.py
@@ -3,4 +3,7 @@ from gub import target
 class Pixman (target.AutoBuild):
     source = 'http://www.cairographics.org/releases/pixman-0.13.2.tar.gz'
     def _get_build_dependencies (self):
-        return ['tools::libtool']
+        return ['libtool']
+
+class Pixman__linux__ppc (Pixman):
+    patches = ['pixman-0.13.2-auxvec.patch']
