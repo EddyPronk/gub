0f55c9dd6467a2b6edd477124450a6c7bf73f376
Jan Nieuwenhuizen <janneke@gnu.org>
2009-02-12 17:32:35 +0100

    Add execve restriction, commented-out.

--- librestrict/restrict.c
+++ librestrict/restrict.c
@@ -205,6 +205,31 @@ void initialize (void)
     }
 }
 
+#if 0
+/*
+  Using execve restriction works beautifully, but this would mean that
+  we'd need a full build root, eg, tools::busybox *and* a plain
+  tools::gcc, tools::g++, etc.
+
+*/
+static int
+sys_execve (char const *file_name, char *const argv[], char *const envp[])
+{
+  return syscall (SYS_execve, file_name, argv, envp);
+}
+
+int
+__execve (char const *file_name, char *const argv[], char *const envp[])
+{
+  if (getenv ("LIBRESTRICT_VERBOSE"))
+    fprintf (stderr, "%s: %s\n", __PRETTY_FUNCTION__, file_name);
+  if (!is_allowed (file_name, "execve"))
+    abort ();
+  return sys_execve (file_name, argv, envp);
+}
+
+int execve (char const *file_name, char *const argv[], char *const envp[])  __attribute__ ((alias ("__execve")));
+#endif
 static int
 sys_open (char const *file_name, int flags, int mode)
 {
