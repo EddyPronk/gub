f7bdd17d0f28483195aba3a2bc880cff9d7b6337
Jan Nieuwenhuizen <janneke@gnu.org>
2009-02-13 20:30:49 +0100

    tools::rsync: read from PREFIX/etc/popt, don't read $HOME.  Fixes stat-restrict.

--- gub/specs/rsync.py
+++ gub/specs/rsync.py
@@ -2,5 +2,10 @@ from gub import tools
 
 class Rsync__tools (tools.AutoBuild):
     source = 'ftp://ftp.samba.org/pub/rsync/src/rsync-3.0.4.tar.gz'
+    patches = ['rsync-popt-add-prefix.patch']
     # adding libpopt build depependency not really necessary, as rsync
     # bundles it.
+    def configure_command (self):
+        return (tools.AutoBuild.configure_command (self)
+                + ' --with-included-popt'
+                + r''' 'CFLAGS=-DPREFIX=\"%(system_prefix)s\"' ''')
