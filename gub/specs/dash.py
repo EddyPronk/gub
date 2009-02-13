ba912c2f9919ea11dc3af3a6b3fffd6778b1bf9b
Jan Nieuwenhuizen <janneke@gnu.org>
2009-02-13 07:34:55 +0100

    Build dash dynamically linked.

--- gub/specs/dash.py
+++ gub/specs/dash.py
@@ -2,5 +2,9 @@ from gub import tools
 
 class Dash__tools (tools.AutoBuild):
     source = 'http://gondor.apana.org.au/~herbert/dash/files/dash-0.5.5.1.tar.gz'
+    def configure_command (self):
+        return (tools.AutoBuild.configure_command (self)
+                # dash takes --enable-static to mean: --disable-shared
+                .replace ('--enable-static', ''))
     def wrap_executables (self):
         pass
