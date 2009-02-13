77ad66063dc7b9b0df8d84bf374e540be1810ce2
Jan Nieuwenhuizen <janneke@gnu.org>
2009-02-13 22:39:32 +0100

    texi2html: bump to 1.82 tarball.

--- gub/specs/texi2html.py
+++ gub/specs/texi2html.py
@@ -1,4 +1,5 @@
 from gub import tools
 
 class Texi2html__tools (tools.AutoBuild):
-    source = 'http://lilypond.org/vc/texi2html.git&branch=master'
+    #source = 'http://lilypond.org/vc/texi2html.git&branch=master'
+    source = 'http://www.very-clever.com/download/nongnu/texi2html/texi2html-1.82.tar.gz'
