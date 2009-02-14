04bf753148e4febcbf30c14ae9734ab8292f8fd8
Jan Nieuwenhuizen <janneke@gnu.org>
2009-02-14 21:15:46 +0100

    hunspell: new package.

---
+++ gub/specs/hunspell.py
@@ -0,0 +1,19 @@
+from gub import target
+
+class Hunspell (target.AutoBuild):
+    source = 'http://surfnet.dl.sourceforge.net/sourceforge/hunspell/hunspell-1.2.8.tar.gz'
+
+class Hunspell__mingw (Hunspell):
+    def configure_command (self):
+        return (Hunspell.configure_command (self)
+                + ' --disable-nls')
+    def patch (self):
+        Hunspell.patch (self)
+        self.file_sub ([('(chmorph_LDADD.*)',
+                         r'\1 ../hunspell/libhunspell-1.2.la')],
+                       '%(srcdir)s/src/tools/Makefile.am')
+        self.file_sub ([('(chmorph_LDADD.*)',
+                         r'\1 ../hunspell/libhunspell-1.2.la'),
+                        ('(	../parsers/libparsers.a .*@READLINELIB@)',
+                         r'\1 ../hunspell/libhunspell-1.2.la -liconv')],
+                       '%(srcdir)s/src/tools/Makefile.in')
