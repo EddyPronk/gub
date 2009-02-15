626d4674f3afb41bf97d881ff5fa3ab4f89f4c9c
Jan Nieuwenhuizen <janneke@gnu.org>
2009-02-15 21:27:40 +0100

    Python2.6 fix -- may print lots of warnings.

--- gub/repository.py
+++ gub/repository.py
@@ -540,8 +540,11 @@ class Git (Repository):
     def git_pipe (self, command, ignore_errors=False):
         return self.read_pipe (self.git_command () + command,
                                ignore_errors=ignore_errors,
-                               env=self.get_env (),
-                               logger=self.logger.harmless)
+                               env=self.get_env ())
+                               # ugh: suffer errors for now,
+                               # python2.6 and up cannot handle
+                               # overriding the logger here
+                               # logger=self.logger.harmless)
     def is_downloaded (self):
         if not os.path.isdir (os.path.join (self.dir, 'refs')):
             return False
