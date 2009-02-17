bf7668c3ed44415689f0a20e4b092fdbda2e3351
Jan Nieuwenhuizen <janneke@gnu.org>
2009-02-17 16:21:01 +0100

    Dash: make echo, test and [ external commands.
    This makes LIBRESTRICT=open:stat a whole lot more interesting.

--- gub/specs/dash.py
+++ gub/specs/dash.py
@@ -2,6 +2,12 @@ from gub import tools
 
 class Dash__tools (tools.AutoBuild):
     source = 'http://gondor.apana.org.au/~herbert/dash/files/dash-0.5.5.1.tar.gz'
+    def patch (self):
+        tools.AutoBuild.patch (self)
+        self.file_sub ([('testcmd		test [[]', ''),
+                        ('echocmd		echo', '')],
+                       '%(srcdir)s/src/builtins.def.in',
+                       must_succeed=True)
     def configure_command (self):
         return (tools.AutoBuild.configure_command (self)
                 # dash takes --enable-static to mean: --disable-shared
