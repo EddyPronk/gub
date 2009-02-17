038482d74d1939200863389d2336943e93543aea
Jan Nieuwenhuizen <janneke@gnu.org>
2009-02-17 16:21:18 +0100

    Expat: bump to 2.0.1.

--- gub/specs/expat.py
+++ gub/specs/expat.py
@@ -3,31 +3,27 @@ from gub import target
 from gub import tools
 
 class Expat (target.AutoBuild):
-    source = 'http://surfnet.dl.sourceforge.net/sourceforge/expat/expat-1.95.8.tar.gz'
-    patches = ['expat-1.95.8-mingw.patch']
-
+    source = 'http://surfnet.dl.sourceforge.net/sourceforge/expat/expat-2.0.1.tar.gz'
+    patches = ['expat-2.0.1-mingw.patch']
     def _get_build_dependencies (self):
         return ['libtool', 'tools::expat']
-
     def patch (self):
         target.AutoBuild.patch (self)
         #FIXME: should have configure.ac/in vs configure timestamp test
         self.system ("rm %(srcdir)s/configure")
         self.system ("touch %(srcdir)s/tests/xmltest.sh.in")
-
     def makeflags (self):
         return misc.join_lines ('''
 CFLAGS="-O2 -DHAVE_EXPAT_CONFIG_H"
 EXEEXT=
 RUN_FC_CACHE_TEST=false
 ''')
-    def compile_command (self):
-        return (target.AutoBuild.compile_command (self)
-            + self.makeflags ())
 
-    def install_command (self):
-        return (target.AutoBuild.install_command (self)
-                + self.makeflags ())
+class Expat__mingw (Expat):
+    def configure_command (self):
+        # mingw's expat libtool build breaks with DASH
+        return (Expat.configure_command (self)
+                .replace ('SHELL=', 'XSHELL='))
 
 class Expat__linux__arm__vfp (Expat):
     patches = []
