9921ce42e5e2a894a14750259fc10a3d8039b6ce
Patrick McCarty <pnorcks@gmail.com>
2009-06-29 23:45:41 -0700

    Bump t1utils, and fix build with glibc 2.10

--- gub/specs/t1utils.py
+++ gub/specs/t1utils.py
@@ -1,5 +1,6 @@
 from gub import tools
 
 class T1utils__tools (tools.AutoBuild):
-    source = 'http://www.lcdf.org/type/t1utils-1.33.tar.gz'
+    source = 'http://www.lcdf.org/type/t1utils-1.34.tar.gz'
+    patches = ['t1utils-1.34-glibc-compat.patch']
 
