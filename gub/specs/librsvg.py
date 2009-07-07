4aa94cf093d49362bef227606874b5c013ce8716
Jan Nieuwenhuizen <janneke@gnu.org>
2009-07-07 10:03:00 +0200

    librsvg: fix package names in dependency list.  Fixes build.

--- gub/specs/librsvg.py
+++ gub/specs/librsvg.py
@@ -3,4 +3,4 @@ from gub import target
 class Librsvg (target.AutoBuild):
     source = 'http://ftp.gnome.org/pub/GNOME/sources/librsvg/2.22/librsvg-2.22.3.tar.gz'
     def _get_build_dependencies (self):
-        return ['tools::libtool', 'cairo-devel', 'fontconfig-devel', 'libglib-devel', 'libgtk+-devel', 'pangocairo-devel', 'libxml2-devel']
+        return ['tools::libtool', 'cairo-devel', 'fontconfig-devel', 'glib-devel', 'gtk+-devel', 'pangocairo-devel', 'libxml2-devel']
