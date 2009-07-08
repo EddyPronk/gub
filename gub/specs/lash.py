362e7c0cb1a112c9940a08845ef9dc5f27971cf3
Jan Nieuwenhuizen <janneke@gnu.org>
2009-07-08 17:09:35 +0200

    lash: initial build fixes.

--- gub/specs/lash.py
+++ gub/specs/lash.py
@@ -2,5 +2,16 @@ from gub import target
 
 class Lash (target.AutoBuild):
     source = 'http://www.very-clever.com/download/nongnu/lash/lash-0.6.0~rc2.tar.bz2'
+    patches = ['lash-0.6.0.rc2.patch']
     def _get_build_dependencies (self):
-        return ['tools::automake', 'tools::pkg-config',]
+        return ['tools::automake', 'tools::pkg-config',
+                'e2fsprogs-devel',
+                'dbus-devel',
+                'jack-devel',
+                ]
+    def configure_command (self):
+        return (target.AutoBuild.configure_command (self)
+                + '--without-python')
+        # either that, or
+        # + 'CPPFLAGS="-I%(system_prefix)s/include `python-config --cflags`"'
+        # + 'LDFLAGS="`python-config --ldflags`"')
