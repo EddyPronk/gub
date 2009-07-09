6d2bc7a8d37cb31fbde1cf87b302070effc3f870
Jan Nieuwenhuizen <janneke@gnu.org>
2009-07-09 08:30:17 +0200

    Use custom option to build e2fsprogs shared.  Fixes lash linkage on linux-64.

--- gub/specs/e2fsprogs.py
+++ gub/specs/e2fsprogs.py
@@ -2,6 +2,9 @@ from gub import target
 
 class E2fsprogs (target.AutoBuild):
     source = 'http://surfnet.dl.sourceforge.net/sourceforge/e2fsprogs/e2fsprogs-1.38.tar.gz'
+    def configure_command (self):
+        return (target.AutoBuild.configure_command (self)
+                + '  --enable-elf-shlibs')
     def install_command (self):
         return (target.AutoBuild.install_command (self)
                 + ' install-libs')
