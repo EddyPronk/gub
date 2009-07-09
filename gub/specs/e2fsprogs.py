2faf4b51b9781a1600c9ab801e46391c371ee96c
Jan Nieuwenhuizen <janneke@gnu.org>
2009-07-09 11:35:44 +0200

    e2fsprogs: mention update possibility.

--- gub/specs/e2fsprogs.py
+++ gub/specs/e2fsprogs.py
@@ -1,5 +1,7 @@
 from gub import target
 
+# FIXME: updateme and dependants, package has finally been split
+# http://sourceforge.net/projects/e2fsprogs/files/
 class E2fsprogs (target.AutoBuild):
     source = 'http://surfnet.dl.sourceforge.net/sourceforge/e2fsprogs/e2fsprogs-1.38.tar.gz'
     def configure_command (self):
