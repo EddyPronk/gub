53f1b3f903c02c1d828414d7203e78db6672b7f8
Jan Nieuwenhuizen <janneke@gnu.org>
2009-07-09 12:33:51 +0200

    Update librsvg in the feeble hope to fix svg_loader.so segfault.
    It does not help.
    
    stat("/home/janneke/denemo/usr/lib/gtk-2.0/2.10.0/loaders/svg_loader.so", {st_mode=S_IFREG|0755, st_size=35572, ...}) = 0
    futex(0x7ffdfb67d0ec, FUTEX_WAKE_PRIVATE, 2147483647) = 0
    open("/home/janneke/denemo/usr/lib/gtk-2.0/2.10.0/loaders/svg_loader.so", O_RDONLY) = 5
    read(5, "\177ELF\2\1\1\0\0\0\0\0\0\0\0\0\3\0>\0\1\0\0\0000\f\0\0\0\0\0\0@"..., 832) = 832
    fstat(5, {st_mode=S_IFREG|0755, st_size=35572, ...}) = 0
    --- SIGSEGV (Segmentation fault) @ 0 (0) ---
    +++ killed by SIGSEGV +++
    12:32:20 janneke@peder:~/vc/gub
    $
    
    
    Program received signal SIGSEGV, Segmentation fault.
    [Switching to Thread 0x7f36b41e86f0 (LWP 29383)]
    0x00007f36ae30e63e in ?? () from /lib/libc.so.6
    (gdb) bt
    #0  0x00007f36ae30e63e in ?? () from /lib/libc.so.6
    #1  0x00007f36ae3108f1 in ?? () from /lib/libc.so.6
    #2  0x00007f36ae31253f in calloc () from /lib/libc.so.6
    #3  0x00007f36b4014146 in ?? () from /lib64/ld-linux-x86-64.so.2
    #4  0x00007f36b4010910 in ?? () from /lib64/ld-linux-x86-64.so.2
    #5  0x00007f36b4011c2b in ?? () from /lib64/ld-linux-x86-64.so.2
    #6  0x00007f36b401c4d5 in ?? () from /lib64/ld-linux-x86-64.so.2
    #7  0x00007f36b40178c6 in ?? () from /lib64/ld-linux-x86-64.so.2
    #8  0x00007f36b401bdbb in ?? () from /lib64/ld-linux-x86-64.so.2
    #9  0x00007f36aedaef5b in ?? () from /lib/libdl.so.2
    #10 0x00007f36b40178c6 in ?? () from /lib64/ld-linux-x86-64.so.2
    #11 0x00007f36aedaf2cc in ?? () from /lib/libdl.so.2
    #12 0x00007f36aedaeec1 in dlopen () from /lib/libdl.so.2
    #13 0x00007f36b0d398dc in g_module_open (file_name=0x1078ef0 "/home/janneke/denemo/usr/lib/gtk-2.0/2.10.0/loaders/svg_loader.so", flags=G_MODULE_BIND_MASK) at /home/janneke/vc/gub/target/linux-64/src/glib-2.20.4/gmodule/gmodule-dl.c:99
    #14 0x00007f36b3351e9c in _gdk_pixbuf_load_module (image_module=0x1078e10, error=0x7fffbc226628) at /home/janneke/vc/gub/target/linux-64/src/gtk+-2.16.4/gdk-pixbuf/gdk-pixbuf-io.c:776
    #15 0x00007f36b3352b7d in IA__gdk_pixbuf_new_from_file (filename=<value optimized out>, error=0x7fffbc226628) at /home/janneke/vc/gub/target/linux-64/src/gtk+-2.16.4/gdk-pixbuf/gdk-pixbuf-io.c:1075
    #16 0x00000000004352d0 in ?? ()
    #17 0x00000000004354d1 in ?? ()
    #18 0x00007f36ae2b45a6 in __libc_start_main () from /lib/libc.so.6
    #19 0x000000000041083a in ?? ()
    #20 0x00007fffbc226738 in ?? ()
    #21 0x000000000000001c in ?? ()
    #22 0x0000000000000001 in ?? ()
    #23 0x00007fffbc228811 in ?? ()
    #24 0x0000000000000000 in ?? ()
    (gdb)

--- gub/specs/librsvg.py
+++ gub/specs/librsvg.py
@@ -1,6 +1,12 @@
 from gub import target
 
 class Librsvg (target.AutoBuild):
-    source = 'http://ftp.gnome.org/pub/GNOME/sources/librsvg/2.22/librsvg-2.22.3.tar.gz'
+    source = 'http://ftp.gnome.org/pub/GNOME/sources/librsvg/2.26/librsvg-2.26.0.tar.gz'
     def _get_build_dependencies (self):
-        return ['tools::libtool', 'cairo-devel', 'fontconfig-devel', 'glib-devel', 'gtk+-devel', 'pangocairo-devel', 'libxml2-devel']
+        return ['tools::libtool',
+                'cairo-devel',
+                'fontconfig-devel',
+                'glib-devel',
+                'gtk+-devel',
+                'pangocairo-devel',
+                'libxml2-devel']
