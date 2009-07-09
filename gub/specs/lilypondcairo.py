5d08c2c98189a0c8174fcb107ec75f0dd8905209
Jan Nieuwenhuizen <janneke@gnu.org>
2009-07-09 15:11:27 +0200

    lilypondcairo: use tar ball.

--- gub/specs/lilypondcairo.py
+++ gub/specs/lilypondcairo.py
@@ -6,7 +6,7 @@ from gub.specs import lilypond
 # in.  Hmm.
 
 class Lilypondcairo (lilypond.Lilypond):
-    #source = 'http://lilypond.org/download/source/v2.13/2.13.3.tar.gz'
+    source = 'http://lilypond.org/download/source/v2.13/lilypond-2.13.3.tar.gz'
     def _get_build_dependencies (self):
         return [x.replace ('pango', 'pangocairo')
                 for x in lilypond.Lilypond._get_build_dependencies (self)]
@@ -14,6 +14,7 @@ class Lilypondcairo (lilypond.Lilypond):
         return {'': ['lilypond', 'lilypond-devel', 'lilypond-doc'], 'devel': ['lilypond', 'lilypond-devel', 'lilypond-doc'], 'doc': ['lilypond', 'lilypond-devel', 'lilypond-doc'], 'runtime': ['lilypond', 'lilypond-devel', 'lilypond-doc']}
 
 class Lilypondcairo__mingw (lilypond.Lilypond__mingw):
+    source = Lilypondcairo.source
     def _get_build_dependencies (self):
         return [x.replace ('pango', 'pangocairo')
                 for x in lilypond.Lilypond__mingw._get_build_dependencies (self)]
