d97b25573c049021ea1852df564bcddfddbdbb14
Jan Nieuwenhuizen <janneke@gnu.org>
2009-07-22 15:18:41 +0200

    gub/3/md5.py: update.

--- gub/3/md5.py
+++ gub/3/md5.py
@@ -2,3 +2,7 @@ import hashlib
 
 def md5 (string=''):
     return hashlib.md5 (string.encode ('utf8'))
+
+new = md5
+blocksize = 1        # legacy value (wrong in any useful sense)
+digest_size = 16
