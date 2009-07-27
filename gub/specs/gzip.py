178072dad7e1d8ad75c04757fb7e1dcfe11b6c71
Jan Nieuwenhuizen <janneke@gnu.org>
2009-07-27 21:37:23 +0200

    gzip: new package.

---
+++ gub/specs/gzip.py
@@ -0,0 +1,6 @@
+from gub import tools
+import os
+if 'BOOTSTRAP' in os.environ.keys (): from gub import target as tools
+
+class Gzip__tools (tools.AutoBuild):
+    source = 'http://ftp.gnu.org/pub/gnu/gzip/gzip-1.3.12.tar.gz'
