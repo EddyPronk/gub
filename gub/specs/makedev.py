4303eeb3db0171dcdc09ad72624ee0d23d496848
Jan Nieuwenhuizen <janneke@gnu.org>
2009-07-29 11:22:25 +0200

    makedev: /GUB chroot build fix

--- gub/specs/makedev.py
+++ gub/specs/makedev.py
@@ -2,6 +2,6 @@ from gub import tools
 
 class Makedev__tools (tools.MakeBuild):
     source = 'http://ftp.debian.nl/debian/pool/main/m/makedev/makedev_3.3.8.2.orig.tar.gz'
+    patches = ['makedev-fno-stack-protector.patch']
     def install_command (self):
-        return '''make %(makeflags)s DESTDIR=%(install_root)s/%(system_root)s install'''
-
+        return '''make %(makeflags)s DESTDIR=%(install_root)s install'''
