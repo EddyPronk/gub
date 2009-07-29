f75cfabeb47521b5f06361b65f9dfebc0bcab48d
Jan Nieuwenhuizen <janneke@gnu.org>
2009-07-29 11:22:06 +0200

    Fakeroot and fakechroot script fixes.

--- gub/specs/fakeroot.py
+++ gub/specs/fakeroot.py
@@ -9,8 +9,13 @@ class Fakeroot__tools (tools.AutoBuild):
             ]
     def configure_command (self):
         return (tools.AutoBuild.configure_command (self)
-                + ''' LDFLAGS='-L%(system_prefix)s/lib -ldl' '''
+                + ''' LDFLAGS='-L%(system_prefix)s/lib %(rpath)s -ldl' '''
                 + ' CC=%(system_prefix)s/bin/%(toolchain_prefix)sgcc'
                 + ' CCLD=%(system_prefix)s/bin/%(toolchain_prefix)sgcc'
                 + ' CXX=%(system_prefix)s/bin/%(toolchain_prefix)sg++'
                 )
+    def compile (self):
+        tools.AutoBuild.compile (self)
+        self.file_sub ([('BINDIR=.*', 'BINDIR=%(system_prefix)s/bin'),
+                        ('PATHS=', 'PATHS=%(system_prefix)s/lib:'),],
+                       '%(builddir)s/scripts/fakeroot')
