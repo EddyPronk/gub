1cf9022c049bff2984b46f2480008338d6b7b36e
Jan Nieuwenhuizen <janneke@gnu.org>
2009-08-19 10:24:03 +0200

    perl-extutils-makemaker: new package from CPAN.

---
+++ gub/specs/perl-extutils-makemaker.py
@@ -0,0 +1,5 @@
+from gub import tools
+
+class Perl_extutils_makemaker (tools.CpanBuild):
+    source = 'http://search.cpan.org/CPAN/authors/id/M/MS/MSCHWERN/ExtUtils-MakeMaker-6.55_02.tar.gz'
+    
