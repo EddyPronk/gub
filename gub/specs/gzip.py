4d31e3d0bd0d841e49967f9e95768f11d8c2fc14
Neil Puttock <n.puttock@gmail.com>
2009-08-21 00:37:08 +0100

    gzip: rename futimens () to prevent glibc conflict.

--- gub/specs/gzip.py
+++ gub/specs/gzip.py
@@ -4,3 +4,4 @@ if 'BOOTSTRAP' in os.environ.keys (): from gub import target as tools
 
 class Gzip__tools (tools.AutoBuild):
     source = 'http://ftp.gnu.org/pub/gnu/gzip/gzip-1.3.12.tar.gz'
+    patches = ['gzip-1.3.12-glibc-compat.patch']
