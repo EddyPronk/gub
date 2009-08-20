0a470ea86f6b262089d2d6c22229d1c72c7c918e
Jan Nieuwenhuizen <janneke@gnu.org>
2009-08-20 17:33:03 +0200

    pkg-config: depend on system::g++.  Fixes barf in deboostrap etch.

--- gub/specs/pkg-config.py
+++ gub/specs/pkg-config.py
@@ -3,3 +3,7 @@ from gub import tools
 class Pkg_config__tools (tools.AutoBuild):
     def _get_build_dependencies (self):
         return ['libtool']
+    def _get_build_dependencies (self):
+        return [
+            'system::g++'
+            ]
