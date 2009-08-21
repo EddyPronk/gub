b48faafb74384305862444ebd6dfcdece6108580
Jan Nieuwenhuizen <janneke@gnu.org>
2009-08-21 09:19:48 +0200

    gzip: add and enable no_patch option for bootstrap package.

--- gub/specs/gzip.py
+++ gub/specs/gzip.py
@@ -2,6 +2,13 @@ from gub import tools
 import os
 if 'BOOTSTRAP' in os.environ.keys (): from gub import target as tools
 
+no_patch = True
 class Gzip__tools (tools.AutoBuild):
     source = 'http://ftp.gnu.org/pub/gnu/gzip/gzip-1.3.12.tar.gz'
     patches = ['gzip-1.3.12-glibc-compat.patch']
+    def patch (self):
+        if no_patch:
+            for i in ('gzip.c', 'utimens.c', 'utimens.h'):
+                self.file_sub ([('(futimens)', r'gz_\1')], '%(srcdir)s/' + i)
+        else:
+            tools.AutoBuild.patch (self)
