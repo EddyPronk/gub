135e0303df0a4a83fc36aaede9f89b63b294723a
Jan Nieuwenhuizen <janneke@gnu.org>
2009-08-21 16:18:04 +0200

    tools::file: depend on libtool.

--- gub/specs/file.py
+++ gub/specs/file.py
@@ -6,9 +6,9 @@ class File__tools (tools.AutoBuild):
     source = 'ftp://ftp.astron.com/pub/file/file-5.03.tar.gz'
     def _get_build_dependencies (self):
         return [
+            'libtool',
             'zlib',
             ]
     def configure_command (self):
         return (tools.AutoBuild.configure_command (self)
                 + ''' LDFLAGS='-L%(system_prefix)s/lib %(rpath)s -Wl,-rpath -Wl,%(system_prefix)s/lib' ''')
-
