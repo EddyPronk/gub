1b01847bd65a5544756de39c250ec60e983e0f8e
Jan Nieuwenhuizen <janneke@gnu.org>
2009-08-21 17:21:59 +0200

    tools::curl: use rpath.  Fixes debootstrap build.

--- gub/specs/curl.py
+++ gub/specs/curl.py
@@ -6,6 +6,9 @@ class Curl (target.AutoBuild):
     source = 'http://curl.haxx.se/download/curl-7.19.0.tar.gz'
     def _get_build_dependencies (self):
         return ['tools::libtool']
+    def configure_command (self):
+        return (target.AutoBuild.configure_command (self)
+                + ''' LDFLAGS='%(rpath)s -Wl,-rpath -Wl,%(system_prefix)s/lib' ''')
     def install (self):
         target.AutoBuild.install (self)
         self.system ('mkdir -p %(install_prefix)s%(cross_dir)s/bin')
@@ -19,3 +22,6 @@ class Curl (target.AutoBuild):
 class Curl__tools (tools.AutoBuild, Curl):
     def _get_build_dependencies (self):
         return ['libtool']
+    def configure_command (self):
+        return (tools.AutoBuild.configure_command (self)
+                + ''' LDFLAGS='-L%(system_prefix)s/lib %(rpath)s -Wl,-rpath -Wl,%(system_prefix)s/lib' ''')
