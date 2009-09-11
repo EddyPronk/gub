fa7ba74ddd683a1f96c48d5ffdc5f1a42194644a
Jan Nieuwenhuizen <janneke@gnu.org>
2009-09-11 11:09:51 +0200

    intltool: depend on perl-xml-parser.

--- gub/specs/intltool.py
+++ gub/specs/intltool.py
@@ -2,3 +2,7 @@ from gub import tools
 
 class Intltool (tools.AutoBuild):
     source = 'ftp://ftp.gnome.org/pub/GNOME/sources/intltool/0.40/intltool-0.40.5.tar.gz'
+    def _get_build_dependencies (self):
+        return [
+            'tools::perl-xml-parser',
+            ]
