668134e8b27bae32728ab806eb412b38187e5180
Jan Nieuwenhuizen <janneke@gnu.org>
2009-09-14 16:00:59 +0200

    2/6/md5.py: python2.6 md5 compatibility-hack: revert using 3.0's code.
    Fixes
    
        UnicodeDecodeError: 'ascii' codec can't decode byte 0x80 in position 0: ordinal not in range(128)

--- gub/2/6/md5.py
+++ gub/2/6/md5.py
@@ -1,7 +1,7 @@
 import hashlib
 
 def md5 (string=''):
-    return hashlib.md5 (string.encode ('utf8'))
+    return hashlib.md5 (string)
 
 new = md5
 blocksize = 1        # legacy value (wrong in any useful sense)
