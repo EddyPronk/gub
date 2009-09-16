152ef366d81293924d21300e9289c9f04b70efc7
Graham Percival <gperciva@gperciva-desktop.(none)>
2009-09-16 11:00:24 +0100

    freebsd::cross/gcc: remove LDFLAGS. Fixes build on x86.

--- gub/specs/freebsd/cross/gcc.py
+++ gub/specs/freebsd/cross/gcc.py
@@ -1,4 +1,5 @@
 from gub.specs.cross import gcc as cross_gcc
+from gub import misc
 
 class Gcc__freebsd (cross_gcc.Gcc):
     #source = 'ftp://ftp.gnu.org/pub/gnu/gcc/gcc-4.1.2/gcc-4.1.2.tar.bz2'
@@ -6,3 +7,10 @@ class Gcc__freebsd (cross_gcc.Gcc):
     source = 'ftp://ftp.gnu.org/pub/gnu/gcc/gcc-4.3.2/gcc-4.3.2.tar.bz2'
     def _get_build_dependencies (self):
         return cross_gcc.Gcc._get_build_dependencies (self) + ['tools::mpfr']
+    def configure_command (self):
+        return (cross_gcc.Gcc.configure_command (self)
+                # PROMOTEME: if this work, try removing from cross.py
+                .replace ('LDFLAGS=-L%(system_prefix)s/lib', '')
+                + misc.join_lines ('''
+--program-prefix=%(toolchain_prefix)s
+'''))
