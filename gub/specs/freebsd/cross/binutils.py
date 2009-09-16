8d563326a4e0407efa10aad4330930ad29985809
Jan Nieuwenhuizen <janneke@gnu.org>
2009-09-16 08:50:55 +0200

    freebsd::cross/binutils: remove LDFLAGS.  Should fix build on x86.
    TODO: see if LDFLAGS can be removed from cross.py.  See
    
       http://lists.gnu.org/archive/html/lilypond-devel/2009-09/msg00380.html

--- gub/specs/freebsd/cross/binutils.py
+++ gub/specs/freebsd/cross/binutils.py
@@ -6,6 +6,8 @@ class Binutils__freebsd (binutils.Binutils):
         # Add --program-prefix, otherwise we get
         # i686-freebsd-FOO iso i686-freebsd4-FOO.
         return (binutils.Binutils.configure_command (self)
-            + misc.join_lines ('''
+                # PROMOTEME: if this work, try removing from cross.py
+                .replace ('LDFLAGS=-L%(system_prefix)s/lib', '')
+                + misc.join_lines ('''
 --program-prefix=%(toolchain_prefix)s
 '''))
