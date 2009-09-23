3b7f67d79d54625fd976cdf16e4463e19c5df62a
Jan Nieuwenhuizen <janneke@gnu.org>
2009-09-23 09:30:38 +0200

    pkg-config: merge build dependencies.

--- gub/specs/pkg-config.py
+++ gub/specs/pkg-config.py
@@ -2,8 +2,7 @@ from gub import tools
 
 class Pkg_config__tools (tools.AutoBuild):
     def _get_build_dependencies (self):
-        return ['libtool']
-    def _get_build_dependencies (self):
         return [
-            'system::g++'
+            'libtool',
+            'system::g++',
             ]
