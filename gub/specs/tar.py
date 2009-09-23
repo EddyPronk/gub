7179a94362448899a15e80c1da10e7f3fc9ec6ac
Jan Nieuwenhuizen <janneke@gnu.org>
2009-09-23 21:38:57 +0200

    Tar: git build fix.

--- gub/specs/tar.py
+++ gub/specs/tar.py
@@ -1,3 +1,4 @@
+from gub import repository
 from gub import tools
 import os
 if 'BOOTSTRAP' in os.environ.keys (): from gub import target as tools
@@ -6,4 +7,5 @@ class Tar__tools (tools.AutoBuild):
     source = 'http://ftp.gnu.org/pub/gnu/tar/tar-1.20.tar.gz'
     def __init__ (self, settings, source):
         tools.AutoBuild.__init__ (self, settings, source)
-        self.source._unpack = self.source._unpack_promise_well_behaved
+        if isinstance (self.source, repository.TarBall):
+            self.source._unpack = self.source._unpack_promise_well_behaved
