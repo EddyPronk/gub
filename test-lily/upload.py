203904f53bbaa4ee2d8fdd3df4f9f58d3bf41fa4
Jan Nieuwenhuizen <janneke@gnu.org>
2009-09-24 13:11:27 +0200

    test-lily/upload.py: remove hardcoded repo dir.  Fixes upload.

--- test-lily/upload.py
+++ test-lily/upload.py
@@ -181,8 +181,11 @@ def upload_binaries (repo, version, version_db):
     description = repo.git_pipe ('describe --abbrev=39 %s' % repo.get_ref ()).strip ()
     
     git_tag = 'release/%(version_str)s-%(build)d' % locals () 
-    git_tag_cmd = 'git --git-dir downloads/lilypond tag -m "" -a %(git_tag)s %(dirred_branch)s' % locals ()
-    git_push_cmd = 'git --git-dir downloads/lilypond push ssh+git://'+maintainer_git_username+'@git.sv.gnu.org/srv/git/lilypond.git/ refs/tags/%(git_tag)s:refs/tags/%(git_tag)s' % locals ()
+    git_dir = repo.dir
+    git_tag_cmd = 'git --git-dir %(git_dir)s tag -m "" -a %(git_tag)s %(dirred_branch)s' % locals ()
+    # Oops, ugh, does this work with shallow clone?  better set
+    # self.source.shallow = False in lilypond.py...
+    git_push_cmd = 'git --git-dir %(git_dir)s push ssh+git://'+maintainer_git_username+'@git.sv.gnu.org/srv/git/lilypond.git/ refs/tags/%(git_tag)s:refs/tags/%(git_tag)s' % locals ()
     gub_tag_cmd = 'git tag -m "release of lilypond %(description)s (%(version_str)s-%(build)d)"  "gub-release-lilypond-%(version_str)s-%(build)d"' % locals ()
 
     cmds.append (git_tag_cmd)
