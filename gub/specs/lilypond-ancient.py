61cb98071b035cbde3c037895a6913ffdeccc53d
Jan Nieuwenhuizen <janneke@gnu.org>
2009-10-13 15:17:04 +0200

    lilypond-ancient: guile and lex build hacks.

--- gub/specs/lilypond-ancient.py
+++ gub/specs/lilypond-ancient.py
@@ -19,8 +19,8 @@ class LilyPond (lilypond.LilyPond__simple):
     make_flags = (lilypond.LilyPond__simple.make_flags
                   + ' builddir=%(builddir)s'
                   + ' config=%(builddir)s/config.make')
-    configure_command = (''' CFLAGS='-I%(system_prefix)s/include -DGUILE_MINOR_VERSION=SCM_MINOR_VERSION' '''
-                         + ' LDFLAGS=-L%(system_prefix)s/lib '
+    configure_command = (''' CFLAGS="-I%(system_prefix)s/include -DSCM_DEBUG_TYPING_STRICTNESS=0 -Dyy_current_buffer='(yy_buffer_stack!=0?yy_buffer_stack[yy_buffer_stack_top]:0)'" '''
+                         + 'LDFLAGS=-L%(system_prefix)s/lib '
                          + lilypond.LilyPond__simple.configure_command)
     def __init__ (self, settings, source):
         lilypond.LilyPond__simple.__init__ (self, settings, source)
@@ -39,6 +39,9 @@ class LilyPond (lilypond.LilyPond__simple):
                 ('(^STEPMAKE_BISON\()REQUIRED', r'\1OPTIONAL'),
                 #], '%(srcdir)s/configure.in')
                 ], '%(srcdir)s/configure')
+        self.file_sub ([
+                ('GUILE_MINOR', 'SCM_MINOR'),
+                ], '%(srcdir)s/lily/include/lily-guile.hh')
         # FIXME: PROMOTME to texlive.
         self.file_sub ([('^(#include <kpathsea/getopt.h>)', r'//\1'),],
                        '%(system_prefix)s/include/kpathsea/kpathsea.h')
