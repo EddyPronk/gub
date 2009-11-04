c242bd59cdbbf2ffda5e72f4a6489970ab5344cf
Jan Nieuwenhuizen <janneke@gnu.org>
2009-11-04 13:41:28 +0100

    Use ftp.gnome.org instead of ftp.acc.umu.se.  Thanks, Graham.

--- gub/specs/libsig++.py
+++ gub/specs/libsig++.py
@@ -9,4 +9,4 @@ In file included from /home/janneke/vc/gub/target/freebsd-x86/src/libsig++-2.0.1
 /home/janneke/vc/gub/target/freebsd-x86/src/libsig++-2.0.17/sigc++/signal.h:168: error: changes meaning of 'slot_list' from 'struct sigc::slot_list<sigc::slot<T_return, sigc::nil, sigc::nil, sigc::nil, sigc::nil, sigc::nil, sigc::nil, sigc::nil> >'
 '''
 class Libsig_xx___freebsd (Libsig_xx_):
-    source = 'http://ftp.acc.umu.se/pub/GNOME/sources/libsigc++/2.2/libsigc++-2.2.3.tar.gz'
+    source = ''http://ftp.gnome.org/pub/GNOME/sources/libsigc++/2.2/libsigc++-2.2.3.tar.gz'
